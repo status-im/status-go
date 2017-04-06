9bf74818b9b6dca47f8333b04142a8b8eea771c3
teslapatrick <639261121@qq.com>
2017-04-06 19:51:57 +0800

    update

--- geth/testdata/test.sol
+++ geth/testdata/test.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.9;
 
 contract Test {
-    function double(int a) constant returns(int) {
-        return 2*a;
-    }
+function double(int a) constant returns(int) {
+return 2*a;
+}
 }
