cba45af33a764ad8dc6e1edc8c44eb10b62c0897
Alexander Ewetumo <trinoxf@gmail.com>
2017-07-21 10:59:25 +0100

    Added tests for rpc proxy

--- geth/proxy/rpc_test.go
+++ geth/proxy/rpc_test.go
@@ -253,5 +253,271 @@ func (s *RPCRouterTestSuite) TestSendTransaction() {
 	require.IsType((json.RawMessage)(nil), rawJSON)
 
 	require.Equal(string(rawJSON), "\"3434=done\"")
-
 }
+
+// func (s *RPCRouterTestSuite) TestMainnetAcceptance() {
+// 	require := s.Require()
+// 	require.NotNil(s.NodeManager)
+//
+// 	nodeConfig, err := MakeTestNodeConfig(params.MainNetworkID)
+// 	require.NoError(err)
+//
+// 	// validate default state of UpstreamConfig.Enable.
+// 	require.NotEmpty(nodeConfig.UpstreamConfig.URL)
+// 	require.NotEqual(nodeConfig.UpstreamConfig.Enabled, true)
+// 	require.Equal(nodeConfig.UpstreamConfig.URL, params.UpstreamMainNetEthereumNetworkURL)
+//
+// 	nodeConfig.UpstreamConfig.Enabled = true
+//
+// 	require.Equal(nodeConfig.UpstreamConfig.Enabled, true)
+//
+// 	started, err := s.NodeManager.StartNode(nodeConfig)
+// 	require.NoError(err)
+//
+// 	defer s.NodeManager.StopNode()
+//
+// 	// Attempt to find out if we started well.
+// 	select {
+// 	case <-started:
+// 		break
+// 	case <-time.After(1 * time.Second):
+// 		s.T().Fatal("Failed to start node manager")
+// 		break
+// 	}
+//
+// 	rpcNodeManager, ok := s.NodeManager.(common.RPCNodeManager)
+// 	require.Equal(ok, true)
+//
+// 	accountManager := rpcNodeManager.Account()
+// 	require.NotNil(accountManager)
+//
+// 	accountPassword := "fieldMarshal"
+// 	address, _, _, err := accountManager.CreateAccount(accountPassword)
+// 	require.NoError(err)
+//
+// 	selectErr := accountManager.SelectAccount(address, accountPassword)
+// 	require.NoError(selectErr)
+//
+// 	odFunc := otto.FunctionCall{
+// 		Otto: otto.New(),
+// 		This: otto.NullValue(),
+// 	}
+//
+// 	// create a new client and issue a request.
+// 	client, err := rpcNodeManager.RPCClient()
+// 	require.NoError(err)
+// 	require.NotNil(client)
+//
+// 	request := common.RPCCall{
+// 		ID:     65454545334343,
+// 		Method: "eth_sendTransaction",
+// 		Params: []interface{}{
+// 			map[string]interface{}{
+// 				"from":     address,
+// 				"to":       "0xe410006cad020e3690c8ba21ed8b0f065dde2453",
+// 				"value":    "0x2",
+// 				"nonce":    "0x1",
+// 				"data":     "Will-power",
+// 				"gasPrice": "0x4a817c800",
+// 				"gasLimit": "0x5208",
+// 				"chainId":  3391,
+// 			},
+// 		},
+// 	}
+//
+// 	res, err := rpcNodeManager.Exec(request, odFunc)
+// 	require.NoError(err)
+//
+// 	result, err := res.Get("result")
+// 	require.NoError(err)
+// 	require.NotNil(result)
+//
+// 	hash, err := res.Get("hash")
+// 	require.NoError(err)
+// 	require.NotNil(result)
+//
+// 	exported, err := result.Export()
+// 	require.NoError(err)
+//
+// 	fmt.Printf("Res: %+q\n", result)
+// 	fmt.Printf("ResHash: %+q\n", hash)
+// 	fmt.Printf("ResExported: %+q\n", exported)
+// }
+//
+// func (s *RPCRouterTestSuite) TestRopstenAcceptance() {
+// 	require := s.Require()
+// 	require.NotNil(s.NodeManager)
+//
+// 	nodeConfig, err := MakeTestNodeConfig(params.RopstenNetworkID)
+// 	require.NoError(err)
+//
+// 	// validate default state of UpstreamConfig.Enable.
+// 	require.NotEmpty(nodeConfig.UpstreamConfig.URL)
+// 	require.NotEqual(nodeConfig.UpstreamConfig.Enabled, true)
+// 	require.Equal(nodeConfig.UpstreamConfig.URL, params.UpstreamRopstenEthereumNetworkURL)
+//
+// 	nodeConfig.UpstreamConfig.Enabled = true
+//
+// 	require.Equal(nodeConfig.UpstreamConfig.Enabled, true)
+//
+// 	started, err := s.NodeManager.StartNode(nodeConfig)
+// 	require.NoError(err)
+//
+// 	defer s.NodeManager.StopNode()
+//
+// 	// Attempt to find out if we started well.
+// 	select {
+// 	case <-started:
+// 		break
+// 	case <-time.After(1 * time.Second):
+// 		s.T().Fatal("Failed to start node manager")
+// 		break
+// 	}
+//
+// 	rpcNodeManager, ok := s.NodeManager.(common.RPCNodeManager)
+// 	require.Equal(ok, true)
+//
+// 	accountManager := rpcNodeManager.Account()
+// 	require.NotNil(accountManager)
+//
+// 	accountPassword := "fieldMarshal"
+// 	address, _, _, err := accountManager.CreateAccount(accountPassword)
+// 	require.NoError(err)
+//
+// 	selectErr := accountManager.SelectAccount(address, accountPassword)
+// 	require.NoError(selectErr)
+//
+// 	odFunc := otto.FunctionCall{
+// 		Otto: otto.New(),
+// 		This: otto.NullValue(),
+// 	}
+//
+// 	// create a new client and issue a request.
+// 	client, err := rpcNodeManager.RPCClient()
+// 	require.NoError(err)
+// 	require.NotNil(client)
+//
+// 	request := common.RPCCall{
+// 		ID:     65454545334343,
+// 		Method: "eth_sendTransaction",
+// 		Params: []interface{}{
+// 			map[string]interface{}{
+// 				"from":     address,
+// 				"to":       "0xe410006cad020e3690c8ba21ed8b0f065dde2453",
+// 				"value":    "0x2",
+// 				"nonce":    "0x1",
+// 				"data":     "Will-power",
+// 				"gasPrice": "0x4a817c800",
+// 				"gasLimit": "0x5208",
+// 				"chainId":  3391,
+// 			},
+// 		},
+// 	}
+//
+// 	res, err := rpcNodeManager.Exec(request, odFunc)
+// 	require.NoError(err)
+//
+// 	result, err := res.Get("result")
+// 	require.NoError(err)
+// 	require.NotNil(result)
+//
+// 	hash, err := res.Get("hash")
+// 	require.NoError(err)
+// 	require.NotNil(result)
+//
+// 	exported, err := result.Export()
+// 	require.NoError(err)
+//
+// 	fmt.Printf("Res: %+q\n", result)
+// 	fmt.Printf("ResHash: %+q\n", hash)
+// 	fmt.Printf("ResExported: %+q\n", exported)
+// }
+//
+// func (s *RPCRouterTestSuite) TestRinkebyAcceptance() {
+// 	require := s.Require()
+// 	require.NotNil(s.NodeManager)
+//
+// 	nodeConfig, err := MakeTestNodeConfig(params.RinkebyNetworkID)
+// 	require.NoError(err)
+//
+// 	// validate default state of UpstreamConfig.Enable.
+// 	require.NotEmpty(nodeConfig.UpstreamConfig.URL)
+// 	require.NotEqual(nodeConfig.UpstreamConfig.Enabled, true)
+// 	require.Equal(nodeConfig.UpstreamConfig.URL, params.UpstreamRinkebyEthereumNetworkURL)
+//
+// 	nodeConfig.UpstreamConfig.Enabled = true
+//
+// 	require.Equal(nodeConfig.UpstreamConfig.Enabled, true)
+//
+// 	started, err := s.NodeManager.StartNode(nodeConfig)
+// 	require.NoError(err)
+//
+// 	defer s.NodeManager.StopNode()
+//
+// 	// Attempt to find out if we started well.
+// 	select {
+// 	case <-started:
+// 		break
+// 	case <-time.After(1 * time.Second):
+// 		s.T().Fatal("Failed to start node manager")
+// 		break
+// 	}
+//
+// 	rpcNodeManager, ok := s.NodeManager.(common.RPCNodeManager)
+// 	require.Equal(ok, true)
+//
+// 	accountManager := rpcNodeManager.Account()
+// 	require.NotNil(accountManager)
+//
+// 	accountPassword := "fieldMarshal"
+// 	address, _, _, err := accountManager.CreateAccount(accountPassword)
+// 	require.NoError(err)
+//
+// 	selectErr := accountManager.SelectAccount(address, accountPassword)
+// 	require.NoError(selectErr)
+//
+// 	odFunc := otto.FunctionCall{
+// 		Otto: otto.New(),
+// 		This: otto.NullValue(),
+// 	}
+//
+// 	// create a new client and issue a request.
+// 	client, err := rpcNodeManager.RPCClient()
+// 	require.NoError(err)
+// 	require.NotNil(client)
+//
+// 	request := common.RPCCall{
+// 		ID:     65454545334343,
+// 		Method: "eth_sendTransaction",
+// 		Params: []interface{}{
+// 			map[string]interface{}{
+// 				"from":     address,
+// 				"to":       "0xe410006cad020e3690c8ba21ed8b0f065dde2453",
+// 				"value":    "0x2",
+// 				"nonce":    "0x1",
+// 				"data":     "Will-power",
+// 				"gasPrice": "0x4a817c800",
+// 				"gasLimit": "0x5208",
+// 				"chainId":  3391,
+// 			},
+// 		},
+// 	}
+//
+// 	res, err := rpcNodeManager.Exec(request, odFunc)
+// 	require.NoError(err)
+//
+// 	result, err := res.Get("result")
+// 	require.NoError(err)
+// 	require.NotNil(result)
+//
+// 	hash, err := res.Get("hash")
+// 	require.NoError(err)
+// 	require.NotNil(result)
+//
+// 	exported, err := result.Export()
+// 	require.NoError(err)
+//
+// 	fmt.Printf("Res: %+q\n", result)
+// 	fmt.Printf("ResHash: %+q\n", hash)
+// 	fmt.Printf("ResExported: %+q\n", exported)
+// }
