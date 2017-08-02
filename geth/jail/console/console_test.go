d66729709d2aa9f961a1e9a01dd6958340e0b1b9
Alexander Ewetumo <trinoxf@gmail.com>
2017-08-02 15:14:16 +0100

    Refactor console and simply extension registration

--- geth/jail/console/console_test.go
+++ geth/jail/console/console_test.go
@@ -1,4 +1,4 @@
-package extensions_test
+package console_test
 
 import (
 	"bytes"
@@ -8,36 +8,46 @@ import (
 	"testing"
 
 	"github.com/robertkrimen/otto"
-	"github.com/status-im/status-go/geth/jail/extensions"
-	"github.com/status-im/status-go/geth/jail/extensions/console"
+	"github.com/status-im/status-go/geth/jail/console"
 	"github.com/status-im/status-go/geth/node"
 	"github.com/stretchr/testify/suite"
 )
 
-// TestExtensions validates the behaviour of giving conole extensions.
-func TestExtensions(t *testing.T) {
-	suite.Run(t, new(ExtensionsTestSuite))
+// TestConsole validates the behaviour of giving conole extensions.
+func TestConsole(t *testing.T) {
+	suite.Run(t, new(ConsoleTestSuite))
 }
 
-type ExtensionsTestSuite struct {
+type ConsoleTestSuite struct {
 	suite.Suite
 	vm *otto.Otto
 }
 
+func (s *ConsoleTestSuite) SetupTest() {
+	vm := otto.New()
+	require := s.Require()
+
+	require.NotNil(vm)
+	require.IsType(&otto.Otto{}, vm)
+
+	s.vm = vm
+}
+
 // TestConsoleLog will validate the operations of the console.log extension
 // for the otto vm.
-func (s *ExtensionsTestSuite) TestConsoleLog() {
+func (s *ConsoleTestSuite) TestConsoleLog() {
 	require := s.Require()
 	written := "Bob Marley"
 
 	var customWriter bytes.Buffer
 
-	console.Stdout = &customWriter
+	// register console handler.
+	err := s.vm.Set("console", map[string]interface{}{
+		"log": console.Extension(&customWriter, console.Log),
+	})
+	require.NoError(err)
 
-	_, err := s.vm.Run(fmt.Sprintf(`
-		console.log(%q);
-	`, written))
-
+	_, err = s.vm.Run(fmt.Sprintf(`console.log(%q);`, written))
 	require.NoError(err)
 
 	require.NotEmpty(&customWriter)
@@ -46,9 +56,8 @@ func (s *ExtensionsTestSuite) TestConsoleLog() {
 
 // TestObjectLogging will validate the operations of the console.log extension
 // when capturing objects declared from javascript.
-func (s *ExtensionsTestSuite) TestObjectLogging() {
+func (s *ConsoleTestSuite) TestObjectLogging() {
 	require := s.Require()
-	var customWriter bytes.Buffer
 
 	node.SetDefaultNodeNotificationHandler(func(event string) {
 
@@ -71,27 +80,18 @@ func (s *ExtensionsTestSuite) TestObjectLogging() {
 		require.Equal(objectReceived.Name, "bob")
 	})
 
-	console.Stdout = &customWriter
+	var customWriter bytes.Buffer
 
-	_, err := s.vm.Run(`
+	// register console handler.
+	err := s.vm.Set("console", map[string]interface{}{
+		"log": console.Extension(&customWriter, console.Log),
+	})
+	require.NoError(err)
+
+	_, err = s.vm.Run(`
 		var person = {name:"bob", age:24}
 		console.log(person);
 	`)
-
 	require.NoError(err)
 	require.NotEmpty(&customWriter)
-
-}
-
-func (s *ExtensionsTestSuite) SetupTest() {
-	vm := otto.New()
-	require := s.Require()
-
-	require.NotNil(vm)
-	require.IsType(&otto.Otto{}, vm)
-
-	err := extensions.ActivateExtensions(vm)
-	require.NoError(err)
-
-	s.vm = vm
 }
--- geth/jail/handlers.go
+++ geth/jail/handlers.go
@@ -3,6 +3,7 @@ package jail
 import (
 	"github.com/robertkrimen/otto"
 	"github.com/status-im/status-go/geth/node"
+	"github.com/status-im/status-go/geth/jail/console"
 )
 
 // signals
@@ -20,6 +21,11 @@ func registerHandlers(jail *Jail, vm *otto.Otto, chatID string) error {
 		return err
 	}
 	registerHandler := jeth.Object().Set
+
+	// register console handler.
+	if err = registerHandler("console", map[string]interface{}{
+		"log": console.Extension(os.Stdout, console.Log),
+	})
 
 	// register send handler
 	if err = registerHandler("send", makeSendHandler(jail)); err != nil {
