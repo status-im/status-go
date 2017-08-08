54f2a57bf57fcf98eac294782633822451793c21
Marin <marin.basic02@gmail.com>
2017-08-08 11:17:22 +0200

    Modify our log package to be thin - no config needed

--- geth/log/log.go
+++ geth/log/log.go
@@ -1,136 +1,75 @@
 package log
 
 import (
-	"os"
-	"path/filepath"
-	"sync"
-
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/status-im/status-go/geth/params"
 )
 
-// Logger is wrapper for custom logging
+// Logger is wrapper for go-ethereum log
 type Logger struct {
-	origHandler log.Handler
-	handler     log.Handler
-	config      *params.NodeConfig
+	output log.Logger
 }
 
-var (
-	onceInitNodeLogger sync.Once
-	nodeLoggerInstance *Logger
-	logger             log.Logger
-)
-
-func init() {
-	logger = log.New("geth", "StatusIM")
-}
-
-// SetupLogger configs logger using parameters in config
-func SetupLogger(config *params.NodeConfig) (*Logger, error) {
-	if !config.LogEnabled {
-		return nil, nil
-	}
-
-	onceInitNodeLogger.Do(func() {
-		nodeLoggerInstance = &Logger{
-			config:      config,
-			origHandler: log.Root().GetHandler(),
-		}
-		nodeLoggerInstance.handler = nodeLoggerInstance.makeLogHandler(parseLogLevel(config.LogLevel))
-	})
-
-	if err := nodeLoggerInstance.Start(); err != nil {
-		return nil, err
-	}
-
-	return nodeLoggerInstance, nil
-}
-
-// SetV allows to dynamically change log level of messages being written
-func (l *Logger) SetV(logLevel string) {
-	log.Root().SetHandler(l.makeLogHandler(parseLogLevel(logLevel)))
-}
-
-// Start installs logger handler
-func (l *Logger) Start() error {
-	log.Root().SetHandler(l.handler)
-	return nil
-}
-
-// Stop replaces our handler back to the original log handler
-func (l *Logger) Stop() error {
-	log.Root().SetHandler(l.origHandler)
-	return nil
-}
-
-// Show only Status logs
-func (l *Logger) DisableGeth() error {
-	logger.SetHandler(l.handler)
-	return nil
-}
+// Instance to a logger struct
+var loggerInstance *Logger
 
 // Trace is a convenient alias for Root().Trace
 func Trace(msg string, ctx ...interface{}) {
-	logger.Trace(msg, ctx...)
+	printLog(log.LvlTrace, msg, ctx...)
 }
 
 // Debug is a convenient alias for Root().Debug
 func Debug(msg string, ctx ...interface{}) {
-	logger.Debug(msg, ctx...)
+	printLog(log.LvlDebug, msg, ctx...)
 }
 
 // Info is a convenient alias for Root().Info
 func Info(msg string, ctx ...interface{}) {
-	logger.Info(msg, ctx...)
+	printLog(log.LvlInfo, msg, ctx...)
 }
 
 // Warn is a convenient alias for Root().Warn
 func Warn(msg string, ctx ...interface{}) {
-	logger.Warn(msg, ctx...)
+	printLog(log.LvlWarn, msg, ctx...)
 }
 
 // Error is a convenient alias for Root().Error
 func Error(msg string, ctx ...interface{}) {
-	logger.Error(msg, ctx...)
+	printLog(log.LvlError, msg, ctx...)
 }
 
 // Crit is a convenient alias for Root().Crit
 func Crit(msg string, ctx ...interface{}) {
-	logger.Crit(msg, ctx...)
+	printLog(log.LvlCrit, msg, ctx...)
 }
 
-// makeLogHandler creates a log handler for a given level and node configuration
-func (l *Logger) makeLogHandler(lvl log.Lvl) log.Handler {
-	var handler log.Handler
-	logFilePath := filepath.Join(l.config.DataDir, l.config.LogFile)
-	fileHandler := log.Must.FileHandler(logFilePath, log.LogfmtFormat())
-	stderrHandler := log.StreamHandler(os.Stderr, log.TerminalFormat(true))
-	if l.config.LogToStderr {
-		handler = log.MultiHandler(
-			log.LvlFilterHandler(lvl, log.CallerFileHandler(log.CallerFuncHandler(stderrHandler))),
-			log.LvlFilterHandler(lvl, fileHandler))
-	} else {
-		handler = log.LvlFilterHandler(lvl, fileHandler)
+// outputs the log to a given log config level
+func printLog(lvl log.Lvl, msg string, ctx ...interface{}) {
+	if loggerInstance == nil {
+		loggerInstance = &Logger{
+			output: log.New("geth", "StatusIM"),
+		}
+		loggerInstance.output.SetHandler(log.StdoutHandler)
 	}
 
-	return handler
-}
+	switch lvl {
 
-// parseLogLevel parses string and returns logger.* constant
-func parseLogLevel(logLevel string) log.Lvl {
-	switch logLevel {
-	case "ERROR":
-		return log.LvlError
-	case "WARNING":
-		return log.LvlWarn
-	case "INFO":
-		return log.LvlInfo
-	case "DEBUG":
-		return log.LvlDebug
-	case "TRACE":
-		return log.LvlTrace
+	case log.LvlError:
+		loggerInstance.output.Error(msg, ctx...)
+		break
+	case log.LvlWarn:
+		loggerInstance.output.Warn(msg, ctx...)
+		break
+	case log.LvlInfo:
+		loggerInstance.output.Info(msg, ctx...)
+		break
+	case log.LvlDebug:
+		loggerInstance.output.Debug(msg, ctx...)
+		break
+	case log.LvlTrace:
+		loggerInstance.output.Trace(msg, ctx...)
+		break
+	default:
+		loggerInstance.output.Info(msg, ctx...)
+
 	}
-
-	return log.LvlInfo
 }
