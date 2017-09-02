343200359334c9db74c6b9857caad31e2bf68996
Ivan Danyliuk <ivan.daniluk@gmail.com>
2017-09-02 17:40:58 +0200

    Resolve merge conflicts

--- geth/jail/jail_cell.go
+++ geth/jail/jail_cell.go
@@ -1,12 +1,11 @@
 package jail
 
 import (
-	"sync"
-
 	"github.com/robertkrimen/otto"
 	"github.com/status-im/ottoext/fetch"
 	"github.com/status-im/ottoext/loop"
 	"github.com/status-im/ottoext/timers"
+	"github.com/status-im/status-go/geth/jail/vm"
 )
 
 // Cell represents a single jail cell, which is basically a JavaScript VM.
@@ -14,67 +13,40 @@ type Cell struct {
 	sync.Mutex
 
 	id string
-	vm *otto.Otto
+	*vm.VM
 }
 
 // newCell encapsulates what we need to create a new jailCell from the
 // provided vm and eventloop instance.
 func newCell(id string, vm *otto.Otto) (*Cell, error) {
-	// create new event loop for the new cell.
-	// this loop is handling 'setTimeout/setInterval'
-	// calls and is running endlessly in a separate goroutine
-	lo := loop.New(vm)
+	cellVM := vm.New(ottoVM)
 
-	// register handlers for setTimeout/setInterval
-	// functions
-	if err := timers.Define(vm, lo); err != nil {
-		return nil, err
-	}
+	lo := loop.New(cellVM)
 
-	// register handlers for FetchAPI functions
-	if err := fetch.Define(vm, lo); err != nil {
-		return nil, err
-	}
+	registerVMHandlers(cellVM, lo)
 
-	// finally, start loop in a goroutine
+	// start loop in a goroutine
 	// Cell is currently immortal, so the loop
 	go lo.Run()
 
 	return &Cell{
 		id: id,
-		vm: vm,
+		VM: cellVM,
 	}, nil
 }
 
-// Set sets the value to be keyed by the provided keyname.
-func (cell *Cell) Set(key string, val interface{}) error {
-	cell.Lock()
-	defer cell.Unlock()
+// registerHandlers register variuous functions and handlers
+// to the Otto VM, such as Fetch API callbacks or promises.
+func registerVMHandlers(v *vm.VM, lo *loop.Loop) error {
+	// setTimeout/setInterval functions
+	if err := timers.Define(v, lo); err != nil {
+		return err
+	}
 
-	return cell.vm.Set(key, val)
-}
+	// FetchAPI functions
+	if err := fetch.Define(v, lo); err != nil {
+		return err
+	}
 
-// Get returns the giving key's otto.Value from the underline otto vm.
-func (cell *Cell) Get(key string) (otto.Value, error) {
-	cell.Lock()
-	defer cell.Unlock()
-
-	return cell.vm.Get(key)
-}
-
-// Call attempts to call the internal call function for the giving response associated with the
-// proper values.
-func (cell *Cell) Call(item string, this interface{}, args ...interface{}) (otto.Value, error) {
-	cell.Lock()
-	defer cell.Unlock()
-
-	return cell.vm.Call(item, this, args...)
-}
-
-// Run evaluates the giving js string on the associated vm llop.
-func (cell *Cell) Run(val string) (otto.Value, error) {
-	cell.Lock()
-	defer cell.Unlock()
-
-	return cell.vm.Run(val)
+	return nil
 }
