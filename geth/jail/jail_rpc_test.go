2af2cd941db49ba8e76ff81ce2c5911878a5a9e4
Alexander Ewetumo <trinoxf@gmail.com>
2017-09-04 17:42:34 +0100

    Fix JailRPCTestSuite.TestSendTransaction

--- geth/jail/jail_rpc_test.go
+++ geth/jail/jail_rpc_test.go
@@ -12,9 +12,9 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/robertkrimen/otto"
+	"github.com/status-im/status-go/geth/api"
 	"github.com/status-im/status-go/geth/common"
 	"github.com/status-im/status-go/geth/jail"
-	"github.com/status-im/status-go/geth/node"
 	"github.com/status-im/status-go/geth/params"
 	. "github.com/status-im/status-go/geth/testing"
 	"github.com/stretchr/testify/suite"
@@ -43,26 +43,33 @@ func TestJailRPCTestSuite(t *testing.T) {
 
 type JailRPCTestSuite struct {
 	BaseTestSuite
+	Backend        *api.StatusBackend
+	TxQueueManager common.TxQueueManager
 	Account        common.AccountManager
 	Policy         *jail.ExecutionPolicy
-	TxQueueManager common.TxQueueManager
 }
 
 func (s *JailRPCTestSuite) SetupTest() {
 	require := s.Require()
 
-	nodeManager := node.NewNodeManager()
+	backend := api.NewStatusBackend()
+	require.NotNil(backend)
 
-	acctman := node.NewAccountManager(nodeManager)
+	nodeManager := backend.NodeManager()
+	require.NotNil(nodeManager)
+
+	acctman := backend.AccountManager()
 	require.NotNil(acctman)
 
-	txQueueManager := node.NewTxQueueManager(nodeManager, acctman)
+	txQueueManager := backend.TxQueueManager()
+	require.NotNil(txQueueManager)
 
 	policy := jail.NewExecutionPolicy(nodeManager, acctman, txQueueManager)
 	require.NotNil(policy)
 
 	s.Policy = policy
 	s.Account = acctman
+	s.Backend = backend
 	s.NodeManager = nodeManager
 	s.TxQueueManager = txQueueManager
 }
@@ -100,8 +107,6 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 
 		err := json.NewDecoder(r.Body).Decode(&txReq)
 		require.NoError(err)
-
-		fmt.Printf("Incoming request: %+q\n", txReq)
 
 		switch txReq.Method {
 		case "eth_getTransactionCount":
@@ -141,8 +146,8 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 
 	// httpRPCServer will serve as an upstream server accepting transactions.
 	httpRPCServer := httptest.NewServer(rpcService)
-	s.StartTestNode(params.RopstenNetworkID, WithUpstream(httpRPCServer.URL))
-	defer s.StopTestNode()
+	s.StartTestBackend(params.RopstenNetworkID, WithUpstream(httpRPCServer.URL))
+	defer s.StopTestBackend()
 
 	client, err := s.NodeManager.RPCClient()
 	require.NoError(err)
@@ -151,15 +156,15 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 	selectErr := s.Account.SelectAccount(TestConfig.Account1.Address, TestConfig.Account1.Password)
 	require.NoError(selectErr)
 
-	res, err := s.Policy.Execute(request, odFunc)
-	require.NoError(err)
-
 	// TODO(influx6): How do we move this into ExecutionPolicy.executeSendTransaction since
 	// we also need the account's password.
 	// TransactionQueueHandler is required to enqueue a transaction.
 	s.TxQueueManager.SetTransactionQueueHandler(func(queuedTx *common.QueuedTx) {
 		s.TxQueueManager.CompleteTransaction(queuedTx.ID, TestConfig.Account1.Password)
 	})
+
+	res, err := s.Policy.Execute(request, odFunc)
+	require.NoError(err)
 
 	result, err := res.Get("result")
 	require.NoError(err)
@@ -168,14 +173,48 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 	exported, err := result.Export()
 	require.NoError(err)
 
-	rawJSON, ok := exported.(json.RawMessage)
-	require.True(ok, "Expected raw json payload")
-	require.Equal(string(rawJSON), "\"3434=done\"")
+	fmt.Printf("Incoming response: %+q -> %+q\n", result, exported)
+
+	rawJSON, ok := exported.(string)
+	require.True(ok, "Expected Transaction Hash")
+	require.NotEmpty(rawJSON)
 }
 
-// func (s *JailRPCTestSuite) TestMainnetAcceptance() {
+func (s *JailRPCTestSuite) StartTestBackend(networkID int, ops ...TestNodeOption) {
+	require := s.Require()
+	require.NotNil(s.Backend)
+
+	nodeConfig, err := MakeTestNodeConfig(networkID)
+	require.NoError(err)
+
+	for _, op := range ops {
+		op(nodeConfig)
+	}
+
+	// import account keys
+	require.NoError(common.ImportTestAccount(nodeConfig.KeyStoreDir, "test-account1.pk"))
+	require.NoError(common.ImportTestAccount(nodeConfig.KeyStoreDir, "test-account2.pk"))
+
+	require.False(s.Backend.IsNodeRunning())
+	nodeStarted, err := s.Backend.StartNode(nodeConfig)
+	require.NoError(err)
+	<-nodeStarted
+	require.True(s.Backend.IsNodeRunning())
+}
+
+func (s *JailRPCTestSuite) StopTestBackend() {
+	require := s.Require()
+	require.NotNil(s.Backend)
+	require.True(s.Backend.IsNodeRunning())
+
+	backendStopped, err := s.Backend.StopNode()
+	require.NoError(err)
+	<-backendStopped
+	require.False(s.Backend.IsNodeRunning())
+}
+
+// func (s *JailRPCTestSuite) TestMainnetSendTransaction() {
 // 	require := s.Require()
-// 	require.NotNil(s.NodeManager)
 
 // 	odFunc := otto.FunctionCall{
 // 		Otto: otto.New(),
@@ -199,24 +238,55 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 // 		},
 // 	}
 
-// 	nodeConfig, err := MakeTestNodeConfig(params.MainNetworkID)
-// 	require.NoError(err)
+// 	rpcService := new(service)
+// 	rpcService.Handler = func(w http.ResponseWriter, r *http.Request) {
+// 		defer r.Body.Close()
 
-// 	nodeConfig.UpstreamConfig.Enabled = true
+// 		var txReq txRequest
 
-// 	// Start NodeManagers Node
-// 	started, err := s.NodeManager.StartNode(nodeConfig)
-// 	require.NoError(err)
+// 		err := json.NewDecoder(r.Body).Decode(&txReq)
+// 		require.NoError(err)
 
-// 	select {
-// 	case <-started:
-// 		break
-// 	case <-time.After(1 * time.Second):
-// 		require.Fail("Failed to start NodeManager")
-// 		break
+// 		switch txReq.Method {
+// 		case "eth_getTransactionCount":
+// 			w.WriteHeader(http.StatusOK)
+// 			w.Write([]byte(`{"jsonrpc": "2.0", "status":200, "result": "0x434"}`))
+// 			return
+// 		}
+
+// 		payload := ([]byte)(txReq.Payload)
+
+// 		var bu []interface{}
+
+// 		jserr := json.Unmarshal(payload, &bu)
+// 		require.NoError(jserr)
+// 		require.NotNil(bu)
+// 		require.Len(bu, 1)
+
+// 		buElem, ok := bu[0].(string)
+// 		require.Equal(ok, true)
+
+// 		decoded, err := hexutil.Decode(buElem)
+// 		require.NoError(err)
+// 		require.NotNil(decoded)
+
+// 		var tx types.Transaction
+// 		decodeErr := rlp.DecodeBytes(decoded, &tx)
+// 		require.NoError(decodeErr)
+
+// 		// Validate we are receiving transaction from the proper network chain.
+// 		c, err := s.NodeManager.NodeConfig()
+// 		require.NoError(err)
+// 		require.Equal(tx.ChainId().Uint64(), c.NetworkID)
+
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(`{"jsonrpc": "2.0", "status":200, "result": "3434=done"}`))
 // 	}
 
-// 	defer s.NodeManager.StopNode()
+// 	// httpRPCServer will serve as an upstream server accepting transactions.
+// 	httpRPCServer := httptest.NewServer(rpcService)
+// 	s.StartTestBackend(params.MainNetworkID, WithUpstream(httpRPCServer.URL))
+// 	defer s.StopTestBackend()
 
 // 	client, err := s.NodeManager.RPCClient()
 // 	require.NoError(err)
@@ -225,16 +295,32 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 // 	selectErr := s.Account.SelectAccount(TestConfig.Account1.Address, TestConfig.Account1.Password)
 // 	require.NoError(selectErr)
 
+// 	// TODO(influx6): How do we move this into ExecutionPolicy.executeSendTransaction since
+// 	// we also need the account's password.
+// 	// TransactionQueueHandler is required to enqueue a transaction.
+// 	s.TxQueueManager.SetTransactionQueueHandler(func(queuedTx *common.QueuedTx) {
+// 		s.TxQueueManager.CompleteTransaction(queuedTx.ID, TestConfig.Account1.Password)
+// 	})
+
 // 	res, err := s.Policy.Execute(request, odFunc)
 // 	require.NoError(err)
 
-// 	_, err = res.Get("hash")
+// 	result, err := res.Get("result")
 // 	require.NoError(err)
+// 	require.NotNil(result)
+
+// 	exported, err := result.Export()
+// 	require.NoError(err)
+
+// 	fmt.Printf("Incoming response: %+q -> %+q\n", result, exported)
+
+// 	rawJSON, ok := exported.(string)
+// 	require.True(ok, "Expected Transaction Hash")
+// 	require.NotEmpty(rawJSON)
 // }
 
-// func (s *JailRPCTestSuite) TestRobstenAcceptance() {
+// func (s *JailRPCTestSuite) TestRinkebySendTransaction() {
 // 	require := s.Require()
-// 	require.NotNil(s.NodeManager)
 
 // 	odFunc := otto.FunctionCall{
 // 		Otto: otto.New(),
@@ -258,24 +344,55 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 // 		},
 // 	}
 
-// 	nodeConfig, err := MakeTestNodeConfig(params.RopstenNetworkID)
-// 	require.NoError(err)
+// 	rpcService := new(service)
+// 	rpcService.Handler = func(w http.ResponseWriter, r *http.Request) {
+// 		defer r.Body.Close()
 
-// 	nodeConfig.UpstreamConfig.Enabled = true
+// 		var txReq txRequest
 
-// 	// Start NodeManagers Node
-// 	started, err := s.NodeManager.StartNode(nodeConfig)
-// 	require.NoError(err)
+// 		err := json.NewDecoder(r.Body).Decode(&txReq)
+// 		require.NoError(err)
 
-// 	select {
-// 	case <-started:
-// 		break
-// 	case <-time.After(1 * time.Second):
-// 		require.Fail("Failed to start NodeManager")
-// 		break
+// 		switch txReq.Method {
+// 		case "eth_getTransactionCount":
+// 			w.WriteHeader(http.StatusOK)
+// 			w.Write([]byte(`{"jsonrpc": "2.0", "status":200, "result": "0x434"}`))
+// 			return
+// 		}
+
+// 		payload := ([]byte)(txReq.Payload)
+
+// 		var bu []interface{}
+
+// 		jserr := json.Unmarshal(payload, &bu)
+// 		require.NoError(jserr)
+// 		require.NotNil(bu)
+// 		require.Len(bu, 1)
+
+// 		buElem, ok := bu[0].(string)
+// 		require.Equal(ok, true)
+
+// 		decoded, err := hexutil.Decode(buElem)
+// 		require.NoError(err)
+// 		require.NotNil(decoded)
+
+// 		var tx types.Transaction
+// 		decodeErr := rlp.DecodeBytes(decoded, &tx)
+// 		require.NoError(decodeErr)
+
+// 		// Validate we are receiving transaction from the proper network chain.
+// 		c, err := s.NodeManager.NodeConfig()
+// 		require.NoError(err)
+// 		require.Equal(tx.ChainId().Uint64(), c.NetworkID)
+
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(`{"jsonrpc": "2.0", "status":200, "result": "3434=done"}`))
 // 	}
 
-// 	defer s.NodeManager.StopNode()
+// 	// httpRPCServer will serve as an upstream server accepting transactions.
+// 	httpRPCServer := httptest.NewServer(rpcService)
+// 	s.StartTestBackend(params.RinkebyNetworkID, WithUpstream(httpRPCServer.URL))
+// 	defer s.StopTestBackend()
 
 // 	client, err := s.NodeManager.RPCClient()
 // 	require.NoError(err)
@@ -284,68 +401,26 @@ func (s *JailRPCTestSuite) TestSendTransaction() {
 // 	selectErr := s.Account.SelectAccount(TestConfig.Account1.Address, TestConfig.Account1.Password)
 // 	require.NoError(selectErr)
 
-// 	res, err := s.Policy.Execute(request, odFunc)
-// 	require.NoError(err)
-
-// 	_, err = res.Get("hash")
-// 	require.NoError(err)
-// }
-
-// func (s *JailRPCTestSuite) TestRinkebyAcceptance() {
-// 	require := s.Require()
-// 	require.NotNil(s.NodeManager)
-
-// 	odFunc := otto.FunctionCall{
-// 		Otto: otto.New(),
-// 		This: otto.NullValue(),
-// 	}
-
-// 	request := common.RPCCall{
-// 		ID:     65454545334343,
-// 		Method: "eth_sendTransaction",
-// 		Params: []interface{}{
-// 			map[string]interface{}{
-// 				"from":     TestConfig.Account1.Address,
-// 				"to":       "0xe410006cad020e3690c8ba21ed8b0f065dde2453",
-// 				"value":    "0x2",
-// 				"nonce":    "0x1",
-// 				"data":     "Will-power",
-// 				"gasPrice": "0x4a817c800",
-// 				"gasLimit": "0x5208",
-// 				"chainId":  3391,
-// 			},
-// 		},
-// 	}
-
-// 	nodeConfig, err := MakeTestNodeConfig(params.RinkebyNetworkID)
-// 	require.NoError(err)
-
-// 	nodeConfig.UpstreamConfig.Enabled = true
-
-// 	// Start NodeManagers Node
-// 	started, err := s.NodeManager.StartNode(nodeConfig)
-// 	require.NoError(err)
-
-// 	select {
-// 	case <-started:
-// 		break
-// 	case <-time.After(1 * time.Second):
-// 		require.Fail("Failed to start NodeManager")
-// 		break
-// 	}
-
-// 	defer s.NodeManager.StopNode()
-
-// 	client, err := s.NodeManager.RPCClient()
-// 	require.NoError(err)
-// 	require.NotNil(client)
-
-// 	selectErr := s.Account.SelectAccount(TestConfig.Account1.Address, TestConfig.Account1.Password)
-// 	require.NoError(selectErr)
+// 	// TODO(influx6): How do we move this into ExecutionPolicy.executeSendTransaction since
+// 	// we also need the account's password.
+// 	// TransactionQueueHandler is required to enqueue a transaction.
+// 	s.TxQueueManager.SetTransactionQueueHandler(func(queuedTx *common.QueuedTx) {
+// 		s.TxQueueManager.CompleteTransaction(queuedTx.ID, TestConfig.Account1.Password)
+// 	})
 
 // 	res, err := s.Policy.Execute(request, odFunc)
 // 	require.NoError(err)
 
-// 	_, err = res.Get("hash")
+// 	result, err := res.Get("result")
 // 	require.NoError(err)
+// 	require.NotNil(result)
+
+// 	exported, err := result.Export()
+// 	require.NoError(err)
+
+// 	fmt.Printf("Incoming response: %+q -> %+q\n", result, exported)
+
+// 	rawJSON, ok := exported.(string)
+// 	require.True(ok, "Expected Transaction Hash")
+// 	require.NotEmpty(rawJSON)
 // }
