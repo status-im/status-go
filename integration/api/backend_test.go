1898abdb09113894d028db936f652f0996e1e843
Adam Babik <a.babik@designfortress.com>
2017-10-03 10:55:19 +0200

    fix integration/api

--- integration/api/backend_test.go
+++ integration/api/backend_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/status-im/status-go/geth/node"
 	"github.com/status-im/status-go/geth/params"
 	"github.com/status-im/status-go/integration"
-	. "github.com/status-im/status-go/testing"
 	"github.com/stretchr/testify/suite"
 )
 
