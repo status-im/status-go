acd1c1527cb9a41f3987b7f95e15be4682cb31df
Eugene <6655321@bk.ru>
2017-10-09 19:23:09 +0300

    Basic unit testing for notification service

--- geth/notification/fcm_client_test.go
+++ geth/notification/fcm_client_test.go
@@ -1,1 +1,23 @@
 package notification
+
+import (
+	"testing"
+
+	"github.com/NaySoftware/go-fcm"
+	t "github.com/status-im/status-go/geth/testing"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestFCMClientTestSuite(t *testing.T) {
+	suite.Run(t, new(FCMClientTestSuite))
+}
+
+type FCMClientTestSuite struct {
+	t.BaseTestSuite
+}
+
+func (s *FCMClientTestSuite) TestNewFCMClient() {
+	fcmClient := NewFCMClient()
+	s.Require().NotNil(fcmClient)
+	s.Require().IsType(&fcm.FcmClient{}, fcmClient)
+}
--- geth/notification/notification_test.go
+++ geth/notification/notification_test.go
@@ -1,1 +1,57 @@
 package notification
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/status-im/status-go/geth/common"
+	t "github.com/status-im/status-go/geth/testing"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestNotificationTestSuite(t *testing.T) {
+	suite.Run(t, new(NotificationTestSuite))
+}
+
+type NotificationTestSuite struct {
+	t.BaseTestSuite
+	messagingMock     *common.MockMessaging
+	messagingMockCtrl *gomock.Controller
+}
+
+func (s *NotificationTestSuite) SetupTest() {
+	s.messagingMockCtrl = gomock.NewController(s.T())
+	s.messagingMock = common.NewMockMessaging(s.messagingMockCtrl)
+}
+
+func (s *NotificationTestSuite) TearDownTest() {
+	s.messagingMockCtrl.Finish()
+}
+
+func (s *NotificationTestSuite) TestNewNotification() {
+	manager := New(nil)
+	s.Require().NotNil(manager)
+	s.Require().IsType(&Manager{}, manager)
+}
+
+func (s *NotificationTestSuite) TestNotify() {
+	token := "test"
+	s.messagingMock.EXPECT().NewFcmRegIdsMsg([]string{token}, map[string]string{
+		"msg": "Hello World1",
+		"sum": "Happy Day",
+	})
+
+	manager := New(s.messagingMock)
+	res := manager.Notify(token)
+
+	s.Require().Equal(token, res)
+}
+
+func (s *NotificationTestSuite) TestSend() {
+	s.messagingMock.EXPECT().Send().Times(1).Return(nil)
+
+	manager := New(s.messagingMock)
+	err := manager.Send()
+
+	s.Require().NoError(err)
+}
