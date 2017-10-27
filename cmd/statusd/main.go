f02d6372d6a172161b88154b51b83e81ef78876c
Ivan Danyliuk <ivan.daniluk@gmail.com>
2017-10-28 01:02:42 +0200

    Update node config code

--- cmd/statusd/main.go
+++ cmd/statusd/main.go
@@ -18,7 +18,7 @@ var (
 )
 
 var (
-	profMode       = flag.Bool("production", false, "Whether production settings should be loaded")
+	prodMode       = flag.Bool("production", false, "Whether production settings should be loaded")
 	nodeKeyFile    = flag.String("nodekey", "", "P2P node key file (private key)")
 	dataDir        = flag.String("datadir", params.DataDir, "Data directory for the databases and keystore")
 	networkID      = flag.Int("networkid", params.RopstenNetworkID, "Network identifier (integer, 1=Homestead, 3=Ropsten, 4=Rinkeby)")
@@ -46,30 +46,29 @@ func version(gitCommit string) string {
 }
 
 // makeNodeConfig parses incoming CLI options and returns node configuration object
-func makeNodeConfig(ctx *cli.Context) (*params.NodeConfig, error) {
-	nodeConfig, err := params.NewNodeConfig(
-		ctx.GlobalString(DataDirFlag.Name),
-		ctx.GlobalUint64(NetworkIDFlag.Name),
-		!ctx.GlobalBool(ProdModeFlag.Name))
+func makeNodeConfig(dataDir string, networkID int, devMode bool) (*params.NodeConfig, error) {
+	nodeConfig, err := params.NewNodeConfig(dataDir, networkID, devMode)
 	if err != nil {
 		return nil, err
 	}
 
-	nodeConfig.NodeKeyFile = ctx.GlobalString(NodeKeyFileFlag.Name)
-
-	if logLevel := ctx.GlobalString(LogLevelFlag.Name); logLevel != "" {
-		nodeConfig.LogLevel = logLevel
+	if *nodeKeyFile != "" {
+		nodeConfig.NodeKeyFile = *nodeKeyFile
 	}
-	if logFile := ctx.GlobalString(LogFileFlag.Name); logFile != "" {
-		nodeConfig.LogFile = logFile
+
+	if *logLevel != "" {
+		nodeConfig.LogLevel = *logLevel
+	}
+	if *logFile != "" {
+		nodeConfig.LogFile = *logFile
 	}
 
 	return nodeConfig, nil
 }
 
 // printNodeConfig prints node config
-func printNodeConfig(ctx *cli.Context) {
-	nodeConfig, err := makeNodeConfig(ctx)
+func printNodeConfig() {
+	nodeConfig, err := makeNodeConfig(*datadir, *networkID, *!prodMode)
 	if err != nil {
 		fmt.Printf("Loaded Config: failed (err: %v)", err)
 		return
