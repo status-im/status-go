67388066df1ce39dffdcdee4b9f90592188656b9
Ivan Danyliuk <ivan.daniluk@gmail.com>
2017-10-28 14:49:51 +0200

    Add status-wnode command

--- cmd/status-wnode/main.go
+++ cmd/status-wnode/main.go
@@ -4,33 +4,33 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"runtime"
-	"strings"
 
 	"github.com/status-im/status-go/geth/api"
 	"github.com/status-im/status-go/geth/params"
 )
 
 var (
-	gitCommit  = "N/A" // rely on linker: -ldflags -X main.GitCommit"
-	buildStamp = "N/A" // rely on linker: -ldflags -X main.buildStamp"
-	statusAPI  = api.NewStatusAPI()
-)
+	prodMode    = flag.Bool("production", false, "Whether production settings should be loaded")
+	nodeKeyFile = flag.String("nodekey", "", "P2P node key file (private key)")
+	dataDir     = flag.String("datadir", params.DataDir, "Data directory for the databases and keystore")
+	networkID   = flag.Int("networkid", params.RopstenNetworkID, "Network identifier (integer, 1=Homestead, 3=Ropsten, 4=Rinkeby)")
+	httpEnabled = flag.Bool("http", false, "HTTP RPC enpoint enabled (default: false)")
+	httpPort    = flag.Int("httpport", params.HTTPPort, "HTTP RPC server's listening port")
+	ipcEnabled  = flag.Bool("ipc", false, "IPC RPC enpoint enabled")
 
-var (
-	prodMode       = flag.Bool("production", false, "Whether production settings should be loaded")
-	nodeKeyFile    = flag.String("nodekey", "", "P2P node key file (private key)")
-	dataDir        = flag.String("datadir", params.DataDir, "Data directory for the databases and keystore")
-	networkID      = flag.Int("networkid", params.RopstenNetworkID, "Network identifier (integer, 1=Homestead, 3=Ropsten, 4=Rinkeby)")
-	whisperEnabled = flag.Bool("shh", false, "SHH protocol enabled")
-	swarmEnabled   = flag.Bool("swarm", false, "Swarm protocol enabled")
-	httpEnabled    = flag.Bool("http", false, "HTTP RPC enpoint enabled (default: false)")
-	httpPort       = flag.Int("httpport", params.HTTPPort, "HTTP RPC server's listening port")
-	ipcEnabled     = flag.Bool("ipc", false, "IPC RPC enpoint enabled")
-	logLevel       = flag.String("log", "", `Log level, one of: "ERROR", "WARN", "INFO", "DEBUG", and "TRACE"`)
-	logFile        = flag.String("logfile", "", "Path to the log file")
-	version        = flag.Bool("version", false, "Print version")
+	// wnode specific flags
+	echo           = flag.Bool("echo", true, "Echo mode, prints some arguments for diagnostics")
+	bootstrap      = flag.Bool("bootstrap", true, "Don't actively connect to peers, wait for incoming connections")
+	notify         = flag.Bool("notify", false, "Node is capable of sending Push Notifications")
+	forward        = flag.Bool("forward", false, "Only forward messages, neither send nor decrypt messages")
+	mailserver     = flag.Bool("mailserver", false, "Delivers expired messages on demand")
+	identity       = flag.String("identity", "", "Protocol identity file (private key used for asymmetric encryption)")
+	password       = flag.String("password", "", "Password file (password is used for symmetric encryption)")
+	port           = flag.Int("port", params.WhisperPort, "Whisper node's listening port")
+	pow            = flag.Float64("pow", params.WhisperMinimumPoW, "PoW for messages to be added to queue, in float format")
+	ttl            = flag.Int("ttl", params.WhisperTTL, "Time to live for messages, in seconds")
+	injectAccounts = flag.Bool("injectaccounts", true, "Whether test account should be injected or not")
+	firebaseAuth   = flag.String("firebaseauth", "", "FCM Authorization Key used for sending Push Notifications")
 )
 
 func main() {
@@ -42,18 +42,28 @@ func main() {
 		return
 	}
 
-	if *version {
-		printVersion(config, gitCommit, buildStamp)
-		return
+	printHeader(config)
+
+	if *injectAccounts {
+		LoadTestAccounts(config.DataDir)
 	}
 
-	if err = statusAPI.StartNode(config); err != nil {
+	backend := api.NewStatusBackend()
+	started, err := backend.StartNode(config)
+	if err != nil {
 		log.Fatalf("Node start failed: %v", err)
 		return
 	}
 
+	// wait till node is started
+	<-started
+
+	if *injectAccounts {
+		InjectTestAccounts(backend.NodeManager())
+	}
+
 	// wait till node has been stopped
-	node, err := statusAPI.NodeManager().Node()
+	node, err := backend.NodeManager().Node()
 	if err != nil {
 		log.Fatalf("Getting node failed: %v", err)
 		return
@@ -62,63 +72,10 @@ func main() {
 	node.Wait()
 }
 
-// makeNodeConfig parses incoming CLI options and returns node configuration object
-func makeNodeConfig() (*params.NodeConfig, error) {
-	devMode := !*prodMode
-	nodeConfig, err := params.NewNodeConfig(*dataDir, uint64(*networkID), devMode)
-	if err != nil {
-		return nil, err
+// printHeader prints command header
+func printHeader(config *params.NodeConfig) {
+	fmt.Println("Starting Whisper/5 node..")
+	if config.WhisperConfig.EchoMode {
+		fmt.Printf("Whisper Config: %s\n", config.WhisperConfig)
 	}
-
-	// TODO(divan): move this logic into params package
-	if *nodeKeyFile != "" {
-		nodeConfig.NodeKeyFile = *nodeKeyFile
-	}
-
-	if *logLevel != "" {
-		nodeConfig.LogLevel = *logLevel
-	}
-	if *logFile != "" {
-		nodeConfig.LogFile = *logFile
-	}
-
-	nodeConfig.LightEthConfig.Enabled = true
-	nodeConfig.RPCEnabled = *httpEnabled
-	nodeConfig.WhisperConfig.Enabled = *whisperEnabled
-	nodeConfig.SwarmConfig.Enabled = *swarmEnabled
-
-	// RPC configuration
-	if !*httpEnabled {
-		nodeConfig.HTTPHost = "" // HTTP RPC is disabled
-	}
-	nodeConfig.HTTPPort = *httpPort
-	nodeConfig.IPCEnabled = *ipcEnabled
-
-	return nodeConfig, nil
-}
-
-// printVersion prints verbose output about version and config.
-func printVersion(config *params.NodeConfig, gitCommit, buildStamp string) {
-	version := params.Version
-	if gitCommit != "" && len(gitCommit) > 8 {
-		version += "-" + gitCommit[:8]
-	}
-
-	fmt.Println(strings.Title(params.ClientIdentifier))
-	fmt.Println("Version:", params.Version)
-	if gitCommit != "" {
-		fmt.Println("Git Commit:", gitCommit)
-	}
-	if buildStamp != "" {
-		fmt.Println("Build Stamp:", buildStamp)
-	}
-
-	fmt.Println("Network Id:", config.NetworkID)
-	fmt.Println("Go Version:", runtime.Version())
-	fmt.Println("OS:", runtime.GOOS)
-	fmt.Printf("GOPATH=%s\n", os.Getenv("GOPATH"))
-	fmt.Printf("GOROOT=%s\n", runtime.GOROOT())
-
-	config.LightEthConfig.Genesis = "SKIP"
-	fmt.Println("Loaded Config: ", config)
 }
