4f62b67f54cb9a49ec0fa57cd4a25d9d4b5c6d7a
Frank Mueller <frank@status.im>
2017-11-09 17:45:05 +0100

    Using testify for API test

--- cmd/api/api_test.go
+++ cmd/api/api_test.go
@@ -2,62 +2,86 @@ package api_test
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/status-im/status-go/cmd/api"
+	"github.com/status-im/status-go/geth/params"
+	"github.com/stretchr/testify/suite"
 )
+
+// TestAPI runs the whole API test suite.
+func TestAPI(t *testing.T) {
+	suite.Run(t, new(APITestSuite))
+}
+
+// APITestSuite contains all tests of the exposed API.
+type APITestSuite struct {
+	suite.Suite
+}
 
 // TestStartStopServer tests starting the server without any client
 // connection. It is actively killed by using a cancel context.
-func TestStartStopServer(t *testing.T) {
+func (s *APITestSuite) TestStartStopServer() {
 	ctx, cancel := context.WithCancel(context.Background())
-	s, err := api.NewServer(ctx, "localhost", "12345")
-	if err != nil {
-		t.Errorf("cannot create server: %v", err)
-	}
-	if s == nil {
-		t.Errorf("no server returned")
-	}
+	srv, err := api.NewServer(ctx, "localhost", "12345")
+	s.NoError(err)
+	s.NotNil(srv)
+	s.NoError(srv.Err())
 
-	if s.Err() != nil {
-		t.Errorf("server has returned error: %v", s.Err())
-	}
-
+	// Terminate and wait so that background goroutine can end.
 	cancel()
-
-	// Sadly have to wait until cancel() has terminated server.
 	time.Sleep(1 * time.Millisecond)
 
-	if s.Err() != context.Canceled {
-		t.Errorf("server has returned illegl termination reason: %v", s.Err())
-	}
+	s.Equal(srv.Err(), context.Canceled)
 }
 
 // TestConnectClient test starting the server and connecting it
 // with a client.
-func TestConnectClient(t *testing.T) {
+func (s *APITestSuite) TestConnectClient() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	s, err := api.NewServer(ctx, "[::1]", "12345")
-	if err != nil {
-		t.Errorf("cannot create server: %v", err)
-	}
+	srv, err := api.NewServer(ctx, "[::1]", "12345")
+	s.NoError(err)
 
-	c, err := api.NewClient("[::1]", "12345")
-	if err != nil {
-		t.Errorf("cannot create client: %v", err)
-	}
+	clnt, err := api.NewClient("[::1]", "12345")
+	s.NoError(err)
 
-	addrs, err := c.AdminGetAddresses()
-	if err != nil {
-		t.Errorf("cannot retrieve addresses: %v", err)
-	}
-	if len(addrs) == 0 {
-		t.Errorf("retrieved no addresses")
-	}
+	addrs, err := clnt.AdminGetAddresses()
+	s.NoError(err)
+	s.True(len(addrs) != 0)
+	s.NoError(srv.Err())
+}
 
-	if s.Err() != nil {
-		t.Errorf("server didn't survived request: %v", s.Err())
-	}
+// TestStartNode tests starting a node on the server by a
+// client command.
+func (s *APITestSuite) TestStartStopNode() {
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "status-start-stop-node")
+	s.NoError(err)
+	defer os.RemoveAll(tmpDir) //nolint: errcheck
+
+	configJSON := `{
+		"NetworkId": ` + strconv.Itoa(params.RopstenNetworkID) + `,
+		"DataDir": "` + tmpDir + `",
+		"LogLevel": "INFO",
+		"RPCEnabled": true
+	}`
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	srv, err := api.NewServer(ctx, "[::1]", "12345")
+	s.NoError(err)
+
+	clnt, err := api.NewClient("[::1]", "12345")
+	s.NoError(err)
+
+	err = clnt.StatusStartNode(configJSON)
+	s.NoError(err)
+	s.NoError(srv.Err())
+
+	err = clnt.StatusStopNode()
+	s.NoError(err)
 }
