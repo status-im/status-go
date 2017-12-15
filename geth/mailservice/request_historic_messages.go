a99cc97833ee3debd4a79ba888a3de6bf0f98565
Adam Babik <a.babik@designfortress.com>
2017-12-15 14:27:09 +0100

    revert request_historic_messages.go

--- geth/mailservice/request_historic_messages.go
+++ geth/mailservice/request_historic_messages.go
@@ -4,37 +4,37 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"encoding/binary"
-	"errors"
 	"fmt"
-	"math"
 	"time"
 
 	"github.com/ethereum/go-ethereum/p2p/discover"
 	"github.com/ethereum/go-ethereum/whisper/whisperv5"
 	"github.com/status-im/status-go/geth/common"
-	"github.com/status-im/status-go/geth/log"
 	"github.com/status-im/status-go/geth/rpc"
 )
 
 var (
-	errInvalidNumberOfParams = errors.New("invalid number of parameters, expected 1 parameter")
-	errInvalidParams         = errors.New("invalid parameter data type")
-	errMissingTopic          = errors.New("topic value is required")
-	errMissingSymKeyID       = errors.New("symKeyID value is required")
-	errInvalidSymKeyID       = errors.New("symKeyID must be a string")
-	errMissingEnode          = errors.New("enode value is missing")
-	errInvalidEnode          = errors.New("enode must be a string and have a valid format")
-	errInvalidTimeRange      = errors.New("invalid TimeLow and TimeUp values")
+	//ErrInvalidNumberOfArgs - error invalid aruments in request
+	ErrInvalidNumberOfArgs = fmt.Errorf("invalid number of arguments, expected 1")
+	//ErrInvalidArgs - error invalid request format
+	ErrInvalidArgs = fmt.Errorf("invalid args")
+	//ErrTopicNotExist - error topic field doesn't exist in request
+	ErrTopicNotExist = fmt.Errorf("topic value does not exist")
+	//ErrTopicNotString - error topic is not string type
+	ErrTopicNotString = fmt.Errorf("topic value is not string")
+	//ErrMailboxSymkeyIDNotExist - error symKeyID field doesn't exist in request
+	ErrMailboxSymkeyIDNotExist = fmt.Errorf("symKeyID does not exist")
+	//ErrMailboxSymkeyIDNotString - error symKeyID is not string type
+	ErrMailboxSymkeyIDNotString = fmt.Errorf("symKeyID is not string")
+	//ErrPeerNotExist - error peer field doesn't exist in request
+	ErrPeerOrEnode = fmt.Errorf("enode or peer field should be not empty")
 )
 
 const defaultWorkTime = 5
 
-// RequestHistoricMessagesHandler returns an RPC handler
-// which sends a p2p request for historic messages.
+//RequestHistoricMessagesHandler returns an RPC handler which sends a p2p request for historic messages.
 func RequestHistoricMessagesHandler(nodeManager common.NodeManager) rpc.Handler {
 	return func(ctx context.Context, args ...interface{}) (interface{}, error) {
-		log.Info("RequestHistoricMessagesHandler start")
-
 		whisper, err := nodeManager.WhisperService()
 		if err != nil {
 			return nil, err
@@ -45,26 +45,22 @@ func RequestHistoricMessagesHandler(nodeManager common.NodeManager) rpc.Handler 
 			return nil, err
 		}
 
-		r, err := parseArgs(args)
+		r, err := parseArgs(args...)
 		if err != nil {
 			return nil, err
 		}
 
+		symkey, err := whisper.GetSymKey(r.SymkeyID)
+		if err != nil {
+			return nil, err
+		}
 		r.PoW = whisper.MinPow()
-
-		log.Info("RequestHistoricMessagesHandler parsed request", "request", r)
-
-		symKey, err := whisper.GetSymKey(r.SymKeyID)
+		env, err := makeEnvelop(r, symkey, node.Server().PrivateKey)
 		if err != nil {
 			return nil, err
 		}
 
-		envelope, err := makeEnvelop(r, symKey, node.Server().PrivateKey)
-		if err != nil {
-			return nil, err
-		}
-
-		err = whisper.RequestHistoricMessages(r.Peer, envelope)
+		err = whisper.RequestHistoricMessages(r.Peer, env)
 		if err != nil {
 			return nil, err
 		}
@@ -74,143 +70,118 @@ func RequestHistoricMessagesHandler(nodeManager common.NodeManager) rpc.Handler 
 }
 
 type historicMessagesRequest struct {
-	// MailServer enode address.
-	Peer []byte
-
-	// TimeLow is a lower bound of time range (optional).
-	// Default is 24 hours back from now.
-	TimeLow uint32
-
-	// TimeUp is a upper bound of time range (optional).
-	// Default is now.
-	TimeUp uint32
-
-	// Topic is a regular Whisper topic.
-	Topic whisperv5.TopicType
-
-	// SymKeyID is an ID of a symmetric key used to authenticate to MailServer.
-	// It's derived from MailServer password.
-	SymKeyID string
-
-	// PoW is Whisper Proof of Work value.
-	PoW float64
+	Peer     []byte              //mailbox peer
+	TimeLow  uint32              //resend messages from
+	TimeUp   uint32              //resend messages to
+	Topic    whisperv5.TopicType //resend messages by topic
+	SymkeyID string              //Mailbox symmetric key id
+	PoW      float64             //whisper proof of work
 }
 
-func parseArgs(args []interface{}) (r historicMessagesRequest, err error) { //nolint: gocyclo
+func parseArgs(args ...interface{}) (historicMessagesRequest, error) {
+	var (
+		r = historicMessagesRequest{
+			TimeLow: uint32(time.Now().Add(-24 * time.Hour).Unix()),
+			TimeUp:  uint32(time.Now().Unix()),
+		}
+	)
+
 	if len(args) != 1 {
-		return r, errInvalidNumberOfParams
+		return historicMessagesRequest{}, ErrInvalidNumberOfArgs
 	}
 
-	param, ok := args[0].(map[string]interface{})
+	historicMessagesArgs, ok := args[0].(map[string]interface{})
 	if !ok {
-		return r, errInvalidParams
+		return historicMessagesRequest{}, ErrInvalidArgs
 	}
 
-	enodeInterface, ok := param["enode"]
-	if !ok {
-		return r, errMissingEnode
+	if t, ok := historicMessagesArgs["from"]; ok {
+		if parsed, ok := t.(uint32); ok {
+			r.TimeLow = parsed
+		}
 	}
-	enode, ok := enodeInterface.(string)
-	if !ok {
-		return r, errInvalidEnode
+	if t, ok := historicMessagesArgs["to"]; ok {
+		if parsed, ok := t.(uint32); ok {
+			r.TimeUp = parsed
+		}
 	}
-	nodeInfo, err := discover.ParseNode(enode)
+	topicInterfaceValue, ok := historicMessagesArgs["topic"]
+	if !ok {
+		return historicMessagesRequest{}, ErrTopicNotExist
+	}
+
+	topicStringValue, ok := topicInterfaceValue.(string)
+	if !ok {
+		return historicMessagesRequest{}, ErrTopicNotString
+	}
+
+	if err := r.Topic.UnmarshalText([]byte(topicStringValue)); err != nil {
+		return historicMessagesRequest{}, nil
+	}
+
+	symkeyIDInterfaceValue, ok := historicMessagesArgs["symKeyID"]
+	if !ok {
+		return historicMessagesRequest{}, ErrMailboxSymkeyIDNotExist
+	}
+	r.SymkeyID, ok = symkeyIDInterfaceValue.(string)
+	if !ok {
+		return historicMessagesRequest{}, ErrMailboxSymkeyIDNotString
+	}
+
+	peer, err := getPeerID(historicMessagesArgs)
 	if err != nil {
-		return r, fmt.Errorf("%v: %v", errInvalidEnode, err)
+		return historicMessagesRequest{}, err
 	}
-	r.Peer = nodeInfo.ID[:]
-
-	topicInterface, ok := param["topic"]
-	if !ok {
-		return r, errMissingTopic
-	}
-	switch t := topicInterface.(type) {
-	case string:
-		r.Topic = whisperv5.BytesToTopic([]byte(t))
-	case []byte:
-		r.Topic = whisperv5.BytesToTopic(t)
-	case whisperv5.TopicType:
-		r.Topic = t
-	}
-
-	symKeyIDInterface, ok := param["symKeyID"]
-	if !ok {
-		return r, errMissingSymKeyID
-	}
-	r.SymKeyID, ok = symKeyIDInterface.(string)
-	if !ok {
-		return r, errInvalidSymKeyID
-	}
-
-	if t, ok := param["from"]; ok {
-		if value, ok := parseToUint32(t); ok {
-			r.TimeLow = value
-		} else {
-			return r, fmt.Errorf("from value must be unix time in seconds, got: %T", t)
-		}
-	} else {
-		r.TimeLow = uint32(time.Now().UTC().Add(-24 * time.Hour).Unix())
-	}
-	if t, ok := param["to"]; ok {
-		if value, ok := parseToUint32(t); ok {
-			r.TimeUp = value
-		} else {
-			return r, fmt.Errorf("to value must be unix time in seconds, got: %T", t)
-		}
-	} else {
-		r.TimeUp = uint32(time.Now().UTC().Unix())
-	}
-	if r.TimeLow > r.TimeUp {
-		return r, errInvalidTimeRange
-	}
-
+	r.Peer = peer
 	return r, nil
 }
 
-func parseToUint32(val interface{}) (uint32, bool) {
-	switch t := val.(type) {
-	case float64:
-		if t >= 0 && t < math.MaxUint32 {
-			return uint32(t), true
-		}
-	case int:
-		if t >= 0 && t < math.MaxUint32 {
-			return uint32(t), true
-		}
-	case int64:
-		if t >= 0 && t < math.MaxUint32 {
-			return uint32(t), true
-		}
-	}
+//makeEnvelop make envelop for request histtoric messages. symmetric key to authenticate to MailServer node and pk is the current node ID.
+func makeEnvelop(r historicMessagesRequest, symkey []byte, pk *ecdsa.PrivateKey) (*whisperv5.Envelope, error) {
+	var params whisperv5.MessageParams
+	params.PoW = r.PoW
+	params.Payload = makePayloadData(r)
+	params.KeySym = symkey
+	params.WorkTime = defaultWorkTime
+	params.Src = pk
 
-	return 0, false
-}
-
-// makeEnvelop makes an envelop for a historic messages request.
-// Symmetric key is used to authenticate to MailServer.
-// PK is the current node ID.
-func makeEnvelop(r historicMessagesRequest, symKey []byte, pk *ecdsa.PrivateKey) (*whisperv5.Envelope, error) {
-	params := whisperv5.MessageParams{
-		PoW:      r.PoW,
-		Payload:  makePayload(r),
-		KeySym:   symKey,
-		WorkTime: defaultWorkTime,
-		Src:      pk,
-	}
 	message, err := whisperv5.NewSentMessage(&params)
 	if err != nil {
 		return nil, err
 	}
-
 	return message.Wrap(&params)
 }
 
-// makePayload makes a specific payload for MailServer to request historic messages.
-func makePayload(r historicMessagesRequest) []byte {
-	// first 8 bytes are lowed and upper bounds as uint32
+//makePayloadData make specific payload for mailserver
+func makePayloadData(r historicMessagesRequest) []byte {
 	data := make([]byte, 8+whisperv5.TopicLength)
 	binary.BigEndian.PutUint32(data, r.TimeLow)
 	binary.BigEndian.PutUint32(data[4:], r.TimeUp)
 	copy(data[8:], r.Topic[:])
 	return data
+}
+
+//getPeerID is used to get peerID from string values of peerID or enode
+func getPeerID(m map[string]interface{}) ([]byte, error) {
+	peerInterfaceValue, okPeer := m["peer"]
+	enodeInterfaceValue, okEnode := m["enode"]
+
+	//only if existing peer or enode(!xor)
+	if okPeer == okEnode {
+		return nil, ErrPeerOrEnode
+	}
+	var peerOrEnode string
+	if p, ok := peerInterfaceValue.(string); ok && okPeer {
+		peerOrEnode = p
+	} else if str, ok := enodeInterfaceValue.(string); ok && okEnode {
+		peerOrEnode = str
+	} else {
+		return nil, ErrPeerOrEnode
+	}
+
+	n, err := discover.ParseNode(peerOrEnode)
+	if err != nil {
+		return nil, err
+	}
+	return n.ID[:], nil
 }
