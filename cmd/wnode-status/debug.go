c51e95cd90d1cb311fd00a6820ab9f79899e4b0a
Adam Babik <a.babik@designfortress.com>
2017-12-26 15:53:49 +0100

    expose node_info

--- cmd/wnode-status/debug.go
+++ cmd/wnode-status/debug.go
@@ -4,6 +4,8 @@ import (
 	"expvar"
 	"net/http"
 
+	"github.com/ethereum/go-ethereum/p2p"
+
 	"github.com/status-im/status-go/geth/api"
 	"github.com/status-im/status-go/geth/log"
 )
@@ -16,7 +18,7 @@ func newMetrics(b *api.StatusBackend) *metrics {
 	return &metrics{b}
 }
 
-func (m *metrics) peersInfo() interface{} {
+func (m *metrics) server() *p2p.Server {
 	node, err := m.backend.NodeManager().Node()
 	if err != nil {
 		log.Warn("Failed to get a node", "err", err.Error())
@@ -29,10 +31,27 @@ func (m *metrics) peersInfo() interface{} {
 		return nil
 	}
 
-	return server.PeersInfo()
+	return server
+}
+
+func (m *metrics) nodeInfo() interface{} {
+	if server := m.server(); server != nil {
+		return server.NodeInfo()
+	}
+
+	return nil
+}
+
+func (m *metrics) peersInfo() interface{} {
+	if server := m.server(); server != nil {
+		return server.PeersInfo()
+	}
+
+	return nil
 }
 
 func startDebugServer(addr string, m *metrics) error {
+	expvar.Publish("node_info", expvar.Func(m.nodeInfo))
 	expvar.Publish("peers_info", expvar.Func(m.peersInfo))
 
 	return http.ListenAndServe(addr, nil)
