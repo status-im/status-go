36e60c57e46ccc1dc41fc097df7548057906f58c
Adam Babik <a.babik@designfortress.com>
2017-12-26 12:51:33 +0100

    move peers_info to be collected on-demand

---
+++ cmd/wnode-status/debug.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"expvar"
+	"net/http"
+
+	"github.com/status-im/status-go/geth/api"
+	"github.com/status-im/status-go/geth/log"
+)
+
+type metrics struct {
+	backend *api.StatusBackend
+}
+
+func newMetrics(b *api.StatusBackend) *metrics {
+	return &metrics{b}
+}
+
+func (m *metrics) peersInfo() interface{} {
+	node, err := m.backend.NodeManager().Node()
+	if err != nil {
+		log.Warn("Failed to get node", "err", err.Error())
+		return nil
+	}
+
+	server := node.Server()
+	if server == nil {
+		log.Warn("Failed to get a server")
+		return nil
+	}
+
+	return server.PeersInfo()
+}
+
+func startDebugServer(addr string, m *metrics) error {
+	expvar.Publish("peers_info", expvar.Func(m.peersInfo))
+
+	return http.ListenAndServe(addr, nil)
+}
--- cmd/wnode-status/main.go
+++ cmd/wnode-status/main.go
@@ -1,37 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"expvar"
 	"flag"
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/status-im/status-go/geth/api"
 	"github.com/status-im/status-go/geth/log"
 	"github.com/status-im/status-go/geth/params"
 )
 
-var (
-	peersCount = expvar.NewInt("peers_count")
-	peersInfo  = expvar.NewString("peers_info")
-)
-
 func main() {
 	flag.Parse()
-
-	if *statsEnabled {
-		log.Info("Stats enabled", "addr", *statsAddr)
-
-		go func() {
-			if err := http.ListenAndServe(*statsAddr, nil); err != nil {
-				log.Error("Failed to start metrics server", "err", err.Error())
-			}
-		}()
-	}
 
 	config, err := makeNodeConfig()
 	if err != nil {
@@ -65,17 +45,22 @@ func main() {
 		}
 	}
 
+	// start debug server and collecting metrics
+	if *statsEnabled {
+		log.Info("Stats enabled", "addr", *statsAddr)
+
+		go func() {
+			metrics := newMetrics(backend)
+			if err := startDebugServer(*statsAddr, metrics); err != nil {
+				log.Error("Failed to start metrics server", "err", err.Error())
+			}
+		}()
+	}
+
 	ethNode, err := backend.NodeManager().Node()
 	if err != nil {
 		log.Crit("Getting node failed", "err", err.Error())
 		os.Exit(1)
-	}
-
-	if *statsEnabled {
-		if err := monitorPeers(ethNode); err != nil {
-			log.Crit("Failed to monitor peers", "err", err.Error())
-			os.Exit(1)
-		}
 	}
 
 	// wait till node has been stopped
@@ -86,40 +71,4 @@ func main() {
 func printHeader(config *params.NodeConfig) {
 	fmt.Println("Starting Whisper V5 node...")
 	fmt.Printf("Config: %s\n", config.WhisperConfig)
-}
-
-func monitorPeers(n *node.Node) error {
-	server := n.Server()
-	if server == nil {
-		return fmt.Errorf("failed to get server")
-	}
-
-	peerEvents := make(chan *p2p.PeerEvent)
-	subscription := server.SubscribeEvents(peerEvents)
-	go func() {
-		for {
-			select {
-			case ev := <-peerEvents:
-				log.Info("Received an event", "type", ev.Type, "peerID", ev.Peer.String())
-
-				// Update expvar only if a peer is added or dropped.
-				// Other events are: message sent and message received.
-				if ev.Type == p2p.PeerEventTypeAdd || ev.Type == p2p.PeerEventTypeDrop {
-					peersCount.Set(int64(server.PeerCount()))
-
-					data, err := json.Marshal(server.PeersInfo())
-					if err != nil {
-						log.Warn("Failed to marshal peers info", "err", err.Error())
-					}
-					peersInfo.Set(string(data))
-				}
-			case err := <-subscription.Err():
-				log.Error("Subscription failed", "err", err.Error())
-				subscription.Unsubscribe()
-				return
-			}
-		}
-	}()
-
-	return nil
 }
