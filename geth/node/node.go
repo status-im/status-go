420fe4c4c3ffb8841b70a3a0b2b96663e1ff6ea6
alex <aok5326@psu.edu>
2018-02-18 12:51:31 -0500

    Remove DefaultMinimumPow patches in favor of using whisper config

--- geth/node/node.go
+++ geth/node/node.go
@@ -161,9 +161,13 @@ func activateShhService(stack *node.Node, config *params.NodeConfig) error {
 	}
 
 	serviceConstructor := func(*node.ServiceContext) (node.Service, error) {
-		whisperConfig := config.WhisperConfig
-		whisperService := whisper.New(nil)
+		whisperServiceConfig := &whisper.Config{
+			MaxMessageSize:     whisper.DefaultMaxMessageSize,
+			MinimumAcceptedPOW: 0.001,
+		}
+		whisperService := whisper.New(whisperServiceConfig)
 
+		whisperConfig := config.WhisperConfig
 		// enable metrics
 		whisperService.RegisterEnvelopeTracer(&shhmetrics.EnvelopeTracer{})
 
