ce41943e9805aedcd0d28e71f2f7495898ef6d2f
Alex Kohler <akohler@lutron.com>
2018-02-20 12:15:00 -0500

    Add "and rerun" to echo

--- _assets/ci/update-geth.sh
+++ _assets/ci/update-geth.sh
@@ -12,7 +12,7 @@ if [ ! -z "$GETH_BRANCH" ]; then
 	sed -i 'N;N;s@\(\[\[constraint]]\n  name = "github.com\/ethereum\/go-ethereum"\n  branch =\)\(.*\)@\1 '"\"${GETH_BRANCH}\""'@g' Gopkg.toml
 fi
 dep ensure -v -update github.com/ethereum/go-ethereum
-make dep-ensure || (echo "Please fix patches. (dep-ensure failed)" && exit 1)
+make dep-ensure || (echo "Please fix patches and rerun. (dep-ensure failed)" && exit 1)
 
 git add Gopkg.lock Gopkg.toml vendor/ _assets/patches/
 if $(git diff --cached --quiet); then
