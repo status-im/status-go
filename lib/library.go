a1e31200c59b8dac332311b7ba7ebeb76c2905b2
Frank Mueller <frank@mweb.de>
2018-03-16 09:45:53 +0100

    Missing argument for config

--- lib/library.go
+++ lib/library.go
@@ -17,7 +17,7 @@ import (
 //GenerateConfig for status node
 //export GenerateConfig
 func GenerateConfig(datadir *C.char, networkID C.int, devMode C.int) *C.char {
-	config, err := params.NewNodeConfig(C.GoString(datadir), uint64(networkID), devMode == 1)
+	config, err := params.NewNodeConfig(C.GoString(datadir), "", uint64(networkID), devMode == 1)
 	if err != nil {
 		return makeJSONResponse(err)
 	}
