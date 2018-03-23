9bb732d9816a0de6f7e5d1a8de50406b0d23f7fe
Frank Mueller <mue@tideland.biz>
2018-03-23 14:58:40 +0100

    Add profiling with pprof (#754)

---
+++ profiling/profiler.go
@@ -0,0 +1,40 @@
+package profiling
+
+import (
+	"fmt"
+	"net/http"
+	hpprof "net/http/pprof"
+
+	"github.com/ethereum/go-ethereum/log"
+)
+
+// Profiler runs and controls a HTTP pprof interface.
+type Profiler struct {
+	server *http.Server
+}
+
+// NewProfiler creates an instance of the profiler with
+// the given port.
+func NewProfiler(port int) *Profiler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/pprof/", hpprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", hpprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", hpprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", hpprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", hpprof.Trace)
+	p := Profiler{
+		server: &http.Server{
+			Addr:    fmt.Sprintf(":%d", port),
+			Handler: mux,
+		},
+	}
+	return &p
+}
+
+// Go starts the HTTP pprof in the background.
+func (p *Profiler) Go() {
+	go func() {
+		log.Info("debug server stopped", "err", p.server.ListenAndServe())
+	}()
+	log.Info("debug server started")
+}
