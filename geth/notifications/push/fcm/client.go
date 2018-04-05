e9c2292776bd84624226853ead9d9fcc8cdd05f1
Adam Babik <dreame4@gmail.com>
2018-04-05 15:14:47 +0200

    Switch mockgen to reflect mode (#800)
    Switch mockgen to use reflect mode as source mode does not support vendored packages.

--- geth/notifications/push/fcm/client.go
+++ geth/notifications/push/fcm/client.go
@@ -2,8 +2,8 @@ package fcm
 
 import "github.com/NaySoftware/go-fcm"
 
-// firebaseClient is a copy of "go-fcm" client methods.
-type firebaseClient interface {
+// FirebaseClient is a copy of "go-fcm" client methods.
+type FirebaseClient interface {
 	NewFcmRegIdsMsg(tokens []string, body interface{}) *fcm.FcmClient
 	Send() (*fcm.FcmResponseStatus, error)
 	SetNotificationPayload(payload *fcm.NotificationPayload) *fcm.FcmClient
