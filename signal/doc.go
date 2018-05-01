40582ace98509afd9db0fc1b373ab86b80c40375
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-05-01 16:22:06 +0200

    Add doc to signal package to make it godoc friendly

---
+++ signal/doc.go
@@ -0,0 +1,5 @@
+// Package signal implements event-based signalling interface between status-go
+// and externally linked codebases like status-react or status-desktop.
+// Events are send asynchronously using OS-specific linking mechanisms. See sources
+// for implementation details.
+package signal
