3e742e221c5b2b73a63e94868b79005c1d0f0fa5
Adam Babik <a.babik@designfortress.com>
2018-05-13 21:01:21 +0200

    update PeerPool constructor

--- geth/node/status_node.go
+++ geth/node/status_node.go
@@ -165,13 +165,13 @@ func (n *StatusNode) setupRPCClient() (err error) {
 
 func (n *StatusNode) startPeerPool() error {
 	n.register = peers.NewRegister(n.config.RegisterTopics...)
+	options := peers.NewDefaultOptions()
 	// TODO(dshulyak) consider adding a flag to define this behaviour
-	allowStop := len(n.config.RegisterTopics) == 0
-	n.peerPool = peers.NewPeerPool(n.config.RequireTopics,
-		peers.DefaultFastSync,
-		peers.DefaultSlowSync,
+	options.AllowStop = len(n.config.RegisterTopics) == 0
+	n.peerPool = peers.NewPeerPool(
+		n.config.RequireTopics,
 		peers.NewCache(n.db),
-		allowStop,
+		options,
 	)
 	if err := n.register.Start(n.gethNode.Server()); err != nil {
 		return err
