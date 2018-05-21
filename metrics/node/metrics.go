f5085d0db108b37de1563b4e0d78fcb19e916c69
Adam Babik <a.babik@designfortress.com>
2018-05-21 12:55:46 +0200

    fix handling events in updateNodeMetrics()

--- metrics/node/metrics.go
+++ metrics/node/metrics.go
@@ -1,28 +1,44 @@
 package node
 
 import (
+	"errors"
+	"flag"
+
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/p2p"
 )
 
 var (
-	nodePeersCounter  = metrics.NewRegisteredCounter("p2p/Peers", nil)
-	nodeMaxPeersGauge = metrics.NewRegisteredGauge("p2p/MaxPeers", nil)
+	nodePeersCounter  metrics.Counter
+	nodeMaxPeersGauge metrics.Gauge
 )
 
-func updateNodeMetrics(node *node.Node, evType p2p.PeerEventType) {
+func init() {
+	// When running tests, we want metrics to be enabled.
+	// Having init() in metrics_test does not work because
+	// this init() is executed first.
+	if flag.Lookup("test.v") != nil {
+		metrics.Enabled = true
+	}
+
+	nodePeersCounter = metrics.NewRegisteredCounter("p2p/Peers", nil)
+	nodeMaxPeersGauge = metrics.NewRegisteredGauge("p2p/MaxPeers", nil)
+}
+
+func updateNodeMetrics(node *node.Node, evType p2p.PeerEventType) error {
 	server := node.Server()
 	if server == nil {
-		logger.Error("server not available")
-		return
+		return errors.New("p2p server is unavailable")
 	}
 
 	if evType == p2p.PeerEventTypeAdd {
 		nodePeersCounter.Inc(1)
-	} else {
+	} else if evType == p2p.PeerEventTypeDrop {
 		nodePeersCounter.Dec(1)
 	}
 
 	nodeMaxPeersGauge.Update(int64(server.MaxPeers))
+
+	return nil
 }
