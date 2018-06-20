d6ec16bc7b0cbed794d3df50e311aacfee7f8f3f
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-06-20 17:41:22 +0200

    Fix return type of RequestMessages (hash instead of bool)

--- t/benchmarks/mailserver_test.go
+++ t/benchmarks/mailserver_test.go
@@ -79,13 +79,13 @@ func testMailserverPeer(t *testing.T) {
 	ok, err := shhAPI.MarkTrustedPeer(context.TODO(), *peerURL)
 	require.NoError(t, err)
 	require.True(t, ok)
-	ok, err = shhextAPI.RequestMessages(context.TODO(), shhext.MessagesRequest{
+	hash, err := shhextAPI.RequestMessages(context.TODO(), shhext.MessagesRequest{
 		MailServerPeer: *peerURL,
 		SymKeyID:       symKeyID,
 		Topic:          topic,
 	})
 	require.NoError(t, err)
-	require.True(t, ok)
+	require.NotNil(t, hash)
 	// wait for all messages
 	require.NoError(t, waitForMessages(t, *msgCount, shhAPI, filterID))
 }
