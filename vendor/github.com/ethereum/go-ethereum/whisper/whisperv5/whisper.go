9686795d69dcd316205fb829f26920f7df713094
Adrià Cidre <adria.cidre@gmail.com>
2018-06-22 11:21:19 +0200

    [#1040] Patch go-ethereum reducing from Warn to Info "message loop" logs

--- vendor/github.com/ethereum/go-ethereum/whisper/whisperv5/whisper.go
+++ vendor/github.com/ethereum/go-ethereum/whisper/whisperv5/whisper.go
@@ -499,7 +499,7 @@ func (wh *Whisper) runMessageLoop(p *Peer, rw p2p.MsgReadWriter) error {
 		// fetch the next packet
 		packet, err := rw.ReadMsg()
 		if err != nil {
-			log.Warn("message loop", "peer", p.peer.ID(), "err", err)
+			log.Info("message loop", "peer", p.peer.ID(), "err", err)
 			return err
 		}
 		if packet.Size > wh.MaxMessageSize() {
