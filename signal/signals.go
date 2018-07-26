0661cbf8783c4a3d47b478e798103f72f4a272fd
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-07-26 19:29:53 +0200

    Add handlers code back to make tests work before refactoring

--- signal/signals.go
+++ signal/signals.go
@@ -2,27 +2,77 @@
 
 package signal
 
-import "unsafe"
+import (
+	"encoding/json"
 
-// send is noop for normal server build
-func send(typ string, event interface{}) {}
+	"sync"
+
+	"github.com/ethereum/go-ethereum/log"
+)
+
+// All general log messages in this package should be routed through this logger.
+var logger = log.New("package", "status-go/signal")
+
+// Envelope is a general signal sent upward from node to RN app
+type Envelope struct {
+	Type  string      `json:"type"`
+	Event interface{} `json:"event"`
+}
+
+// NewEnvelope creates new envlope of given type and event payload.
+func NewEnvelope(typ string, event interface{}) *Envelope {
+	return &Envelope{
+		Type:  typ,
+		Event: event,
+	}
+}
+
+// send sends application signal (in JSON) upwards to application (via default notification handler)
+func send(typ string, event interface{}) {
+	signal := NewEnvelope(typ, event)
+	data, err := json.Marshal(&signal)
+	if err != nil {
+		logger.Error("Marshalling signal envelope", "error", err)
+		return
+	}
+
+	notificationHandlerMutex.Lock()
+	notificationHandler(string(data))
+	notificationHandlerMutex.Unlock()
+}
 
 // NodeNotificationHandler defines a handler able to process incoming node events.
 // Events are encoded as JSON strings.
 type NodeNotificationHandler func(jsonEvent string)
 
+var notificationHandler NodeNotificationHandler = TriggerDefaultNodeNotificationHandler
+
+// notificationHandlerMutex guards notificationHandler for concurrent calls
+var notificationHandlerMutex sync.RWMutex
+
 // SetDefaultNodeNotificationHandler sets notification handler to invoke on Send
 func SetDefaultNodeNotificationHandler(fn NodeNotificationHandler) {
+	notificationHandlerMutex.Lock()
+	notificationHandler = fn
+	notificationHandlerMutex.Unlock()
 }
 
 // ResetDefaultNodeNotificationHandler sets notification handler to default one
 func ResetDefaultNodeNotificationHandler() {
+	notificationHandlerMutex.Lock()
+	notificationHandler = TriggerDefaultNodeNotificationHandler
+	notificationHandlerMutex.Unlock()
 }
 
 // TriggerDefaultNodeNotificationHandler triggers default notification handler (helpful in tests)
 func TriggerDefaultNodeNotificationHandler(jsonEvent string) {
+	logger.Trace("Notification received", "event", jsonEvent)
 }
 
-// SetSignalEventCallback set callback
-func SetSignalEventCallback(cb unsafe.Pointer) {
+//nolint: golint
+func TriggerTestSignal() {
+	str := `{"answer": 42}`
+	notificationHandlerMutex.Lock()
+	notificationHandler(str)
+	notificationHandlerMutex.Unlock()
 }
