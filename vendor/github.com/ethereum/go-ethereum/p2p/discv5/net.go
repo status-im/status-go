6c6fb4d0b324e39ce168fd65ce359f3a128d7918
Dmitry <yashulyak@gmail.com>
2018-08-14 09:46:11 +0300

    Remove patch 0031

--- vendor/github.com/ethereum/go-ethereum/p2p/discv5/net.go
+++ vendor/github.com/ethereum/go-ethereum/p2p/discv5/net.go
@@ -641,18 +641,7 @@ loop:
 				}()
 			} else {
 				refreshDone = make(chan struct{})
-
-				done := make(chan struct{})
-				net.refresh(done)
-				<-done
-
-				// Refresh again only if there are no seeds.
-				// Also, sleep for some time to prevent from
-				// executing too often.
-				go func() {
-					time.Sleep(time.Millisecond * 100)
-					close(refreshDone)
-				}()
+				net.refresh(refreshDone)
 			}
 		}
 	}
