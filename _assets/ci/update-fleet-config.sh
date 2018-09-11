9add4bbf1d2f6d9ac01a1efe250834a1c0663fb6
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-09-11 18:40:17 +0200

    Add script to update fleets static JSON. Closes #1188

---
+++ _assets/ci/update-fleet-config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DIR="$(cd $(dirname "$0")/../../config/cli; pwd)"
+
+echo "Downloading https://fleets.status.im/"
+json=$(curl --silent https://fleets.status.im/)
+fleets=('eth.beta'
+        'eth.staging'
+        'eth.test')
+for fleet in ${fleets[@]}; do 
+    echo "Processing $fleet fleet..."
+    fleetJSON=$(echo $json | jq ".fleets.\"$fleet\"")
+    boot=$(echo $fleetJSON | jq ".boot | map(.)" -r)
+    mail=$(echo $fleetJSON | jq ".mail | map(.)" -r)
+    whisper=$(echo $fleetJSON | jq ".whisper | map(.)" -r)
+    git checkout $DIR/fleet-$fleet.json \
+        && jq ".ClusterConfig.BootNodes = $boot | .ClusterConfig.TrustedMailServers = $mail | .ClusterConfig.StaticNodes = $whisper" $DIR/fleet-$fleet.json \
+        | tee "$DIR/tmp.json" >/dev/null \
+        && mv $DIR/tmp.json $DIR/fleet-$fleet.json
+done
