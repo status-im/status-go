57dac1ab07bca2e1753ea7a3515694e9dddc4b9e
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-09-14 10:42:43 +0200

    Use in-memory DB for test.

--- node/status_node_test.go
+++ node/status_node_test.go
@@ -24,7 +24,7 @@ import (
 )
 
 func TestStatusNodeStart(t *testing.T) {
-	config, err := utils.MakeTestNodeConfig(params.StatusChainNetworkID)
+	config, err := utils.MakeTestNodeConfigWithDataDir("", "", params.FleetUndefined, params.StatusChainNetworkID)
 	require.NoError(t, err)
 	n := New()
 
