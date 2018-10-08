cf8e2953759d9eef17a2fb449281b8f3bfa54ca7
Andrea Franz <andrea@gravityblast.com>
2018-10-08 16:45:35 +0200

    add libpcsclite-dev in a before_install hook on CI

--- _assets/scripts/install_deps.sh
+++ _assets/scripts/install_deps.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -z "$CI_SUDO_REQUIRED" ]; then
-  sudo apt install -y protobuf-compiler libpcsclite-dev && exit
-fi
-
 if [ -x "$(command -v apt)" ]; then
   apt install -y protobuf-compiler libpcsclite-dev
 fi
