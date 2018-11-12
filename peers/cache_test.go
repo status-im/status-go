57cbfbae3a9043bd215b9e22114b344db4b59dac
Adam Babik <a.babik@designfortress.com>
2018-11-12 22:25:44 +0100

    refactor peers.cache_test

--- peers/cache_test.go
+++ peers/cache_test.go
@@ -4,14 +4,68 @@ import (
 	"net"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/p2p/enode"
+
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/p2p/discv5"
-	"github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/storage"
 )
+
+func TestPeersRange(t *testing.T) {
+	peersDB, err := newInMemoryCache()
+	require.NoError(t, err)
+	topic := discv5.Topic("test")
+
+	peerCount := 3
+	peers, err := createDiscv5Peers(peerCount)
+	require.NoError(t, err)
+
+	addedPeerIDs := make(map[string]struct{})
+	for _, peer := range peers {
+		assert.NoError(t, peersDB.AddPeer(peer, topic))
+		addedPeerIDs[peer.String()] = struct{}{}
+	}
+
+	// check if all peers are cached
+	cachedNodes := peersDB.GetPeersRange(topic, peerCount)
+	require.Len(t, cachedNodes, peerCount)
+	// and returned peers are these we just created
+	for _, node := range cachedNodes {
+		_, ok := addedPeerIDs[node.String()]
+		assert.True(t, ok)
+	}
+
+	// test removing peers
+	pk, err := peers[0].ID.Pubkey()
+	require.NoError(t, err)
+	require.NoError(t, peersDB.RemovePeer(enode.PubkeyToIDV4(pk), topic))
+	cachedNodes = peersDB.GetPeersRange(topic, peerCount)
+	require.Len(t, cachedNodes, peerCount-1)
+}
+
+func TestMultipleTopics(t *testing.T) {
+	peersDB, err := newInMemoryCache()
+	require.NoError(t, err)
+	topics := []discv5.Topic{discv5.Topic("first"), discv5.Topic("second")}
+
+	peerCount := 3
+	peers, err := createDiscv5Peers(peerCount)
+	require.NoError(t, err)
+
+	for i := range topics {
+		for _, peer := range peers {
+			assert.NoError(t, peersDB.AddPeer(peer, topics[i]))
+		}
+	}
+
+	for i := range topics {
+		nodes := peersDB.GetPeersRange(topics[i], peerCount+1)
+		assert.Len(t, nodes, peerCount)
+	}
+}
 
 // newInMemoryCache creates a cache for tests
 func newInMemoryCache() (*Cache, error) {
@@ -22,61 +76,22 @@ func newInMemoryCache() (*Cache, error) {
 	return NewCache(memdb), nil
 }
 
-func TestPeersRange(t *testing.T) {
-	peersDB, err := newInMemoryCache()
-	require.NoError(t, err)
-	topic := discv5.Topic("test")
+func createDiscv5Peers(count int) ([]*discv5.Node, error) {
+	nodes := make([]*discv5.Node, count)
 
-	var (
-		peers    [3]*discv5.Node
-		nodeIDs  [3]enode.ID
-		expected = make(map[string]struct{})
-	)
-	for i := 0; i < len(peers); i++ {
-		identity, err := crypto.GenerateKey()
-		require.NoError(t, err)
-		peers[i] = discv5.NewNode(
-			discv5.PubkeyID(&identity.PublicKey),
-			net.IPv4(100, 100, 0, byte(i)),
-			32311,
-			32311)
-		nodeIDs[i] = enode.PubkeyToIDV4(&identity.PublicKey)
-		expected[peers[i].String()] = struct{}{}
-	}
-	for _, peer := range peers {
-		assert.NoError(t, peersDB.AddPeer(peer, topic))
-	}
-
-	nodes := peersDB.GetPeersRange(topic, 3)
-	require.Len(t, nodes, 3)
-
-	for _, node := range nodes {
-		_, ok := expected[node.String()]
-		assert.True(t, ok)
-	}
-
-	assert.NoError(t, peersDB.RemovePeer(nodeIDs[0], topic))
-	require.Len(t, peersDB.GetPeersRange(topic, 3), 2)
-}
-
-func TestMultipleTopics(t *testing.T) {
-	peersDB, err := newInMemoryCache()
-	require.NoError(t, err)
-	topics := []discv5.Topic{discv5.Topic("first"), discv5.Topic("second")}
-	for i := range topics {
-		for j := 0; j < 3; j++ {
-			identity, err := crypto.GenerateKey()
-			require.NoError(t, err)
-			peer := discv5.NewNode(
-				discv5.PubkeyID(&identity.PublicKey),
-				net.IPv4(100, 100, 0, byte(i)),
-				32311,
-				32311)
-			assert.NoError(t, peersDB.AddPeer(peer, topics[i]))
+	for i := 0; i < count; i++ {
+		id, err := crypto.GenerateKey()
+		if err != nil {
+			return nil, err
 		}
+
+		nodes[i] = discv5.NewNode(
+			discv5.PubkeyID(&id.PublicKey),
+			net.IPv4(10, 10, 0, byte(i)),
+			32311,
+			32311,
+		)
 	}
-	for i := range topics {
-		nodes := peersDB.GetPeersRange(topics[i], 10)
-		assert.Len(t, nodes, 3)
-	}
+
+	return nodes, nil
 }
