cddf2e1c6cdbdea77307eadac9f25af874e3d723
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-11-20 19:02:54 +0100

    Change `NotifyUsers` to allow only data JSON element. Part of status-im/status-react#6772

--- notifications/push/fcm/notification_test.go
+++ notifications/push/fcm/notification_test.go
@@ -1,10 +1,10 @@
 package fcm
 
 import (
+	"encoding/json"
 	"errors"
 	"testing"
 
-	"github.com/NaySoftware/go-fcm"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
 )
@@ -29,43 +29,52 @@ func (s *NotifierTestSuite) TearDownTest() {
 	s.fcmClientMockCtrl.Finish()
 }
 
-func (s *NotifierTestSuite) TestNotifySuccess() {
-	fcmPayload := getPayload()
+func (s *NotifierTestSuite) TestSendSuccess() {
 	ids := []string{"1"}
-	payload := fcmPayload
-	msg := make(map[string]string)
-	body := "body1"
-	msg["msg"] = body
+	dataPayload := make(map[string]string)
+	dataPayload["from"] = "a"
+	dataPayload["to"] = "b"
+	dataPayloadByteArray, err := json.Marshal(dataPayload)
+	s.Require().NoError(err)
+	dataPayloadJSON := string(dataPayloadByteArray)
 
-	s.fcmClientMock.EXPECT().SetNotificationPayload(&fcmPayload).Times(1)
-	s.fcmClientMock.EXPECT().NewFcmRegIdsMsg(ids, msg).Times(1)
+	s.fcmClientMock.EXPECT().NewFcmRegIdsMsg(ids, dataPayload).Times(1)
 	s.fcmClientMock.EXPECT().Send().Return(nil, nil).Times(1)
 	fcmClient := Notification{s.fcmClientMock}
 
-	err := fcmClient.Send(body, payload, ids...)
+	err = fcmClient.Send(dataPayloadJSON, ids...)
 
 	s.NoError(err)
 }
 
-func (s *NotifierTestSuite) TestNotifyError() {
+func (s *NotifierTestSuite) TestSendError() {
 	expectedError := errors.New("error")
-	fcmPayload := getPayload()
-	ids := []string{"1"}
-	payload := fcmPayload
-	msg := make(map[string]string)
-	body := "body2"
-	msg["msg"] = body
+	ids := []string{"2"}
+	dataPayload := make(map[string]string)
+	dataPayload["from"] = "c"
+	dataPayload["to"] = "d"
+	dataPayloadByteArray, err := json.Marshal(dataPayload)
+	s.Require().NoError(err)
+	dataPayloadJSON := string(dataPayloadByteArray)
 
-	s.fcmClientMock.EXPECT().SetNotificationPayload(&fcmPayload).Times(1)
-	s.fcmClientMock.EXPECT().NewFcmRegIdsMsg(ids, msg).Times(1)
+	s.fcmClientMock.EXPECT().NewFcmRegIdsMsg(ids, dataPayload).Times(1)
 	s.fcmClientMock.EXPECT().Send().Return(nil, expectedError).Times(1)
 	fcmClient := Notification{s.fcmClientMock}
 
-	err := fcmClient.Send(body, payload, ids...)
+	err = fcmClient.Send(dataPayloadJSON, ids...)
 
 	s.Equal(expectedError, err)
 }
 
-func getPayload() fcm.NotificationPayload {
-	return fcm.NotificationPayload{Title: "Status - new message", Body: "sum"}
+func (s *NotifierTestSuite) TestSendWithInvalidJSON() {
+	ids := []string{"3"}
+	dataPayloadJSON := "{a=b}"
+
+	fcmClient := Notification{s.fcmClientMock}
+
+	err := fcmClient.Send(dataPayloadJSON, ids...)
+	s.Require().Error(err)
+
+	_, ok := err.(*json.SyntaxError)
+	s.True(ok)
 }
