c166e869c02c6f4f08c7dfb3af5d1d91c19f34b8
Adam Babik <a.babik@designfortress.com>
2018-12-11 09:26:12 +0100

    validate request in SyncMail

--- mailserver/mailserver.go
+++ mailserver/mailserver.go
@@ -384,10 +384,9 @@ func (s *WMailServer) SyncMail(peer *whisper.Peer, request whisper.SyncMailReque
 		return fmt.Errorf("requests per seconds limit exceeded")
 	}
 
-	// TODO(adam): uncomment when a new Whisper version is ready
-	// if err := request.Validate(); err != nil {
-	// 	return fmt.Errorf("request is invalid: %v", err)
-	// }
+	if err := request.Validate(); err != nil {
+		return fmt.Errorf("request is invalid: %v", err)
+	}
 
 	iter := s.createIterator(request.Lower, request.Upper, request.Cursor)
 	defer iter.Release()
