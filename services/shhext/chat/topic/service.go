f310d7a9866f19cc5b3023d8c7c6dfe942eb2957
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2019-05-20 11:54:31 +0200

    Set up topic on sending

--- services/shhext/chat/topic/service.go
+++ services/shhext/chat/topic/service.go
@@ -17,9 +17,8 @@ func NewService(persistence PersistenceService) *Service {
 	return &Service{persistence: persistence}
 }
 
-// Receive will generate a shared secret for a given identity, and return it
-func (s *Service) Receive(myPrivateKey *ecdsa.PrivateKey, theirPublicKey *ecdsa.PublicKey, installationID string) ([]byte, error) {
-	log.Info("Receive called for", "installationID", installationID)
+func (s *Service) setupTopic(myPrivateKey *ecdsa.PrivateKey, theirPublicKey *ecdsa.PublicKey, installationID string) ([]byte, error) {
+	log.Info("Setup topic called for", "installationID", installationID)
 	sharedKey, err := ecies.ImportECDSA(myPrivateKey).GenerateShared(
 		ecies.ImportECDSAPublic(theirPublicKey),
 		sskLen,
@@ -37,22 +36,31 @@ func (s *Service) Receive(myPrivateKey *ecdsa.PrivateKey, theirPublicKey *ecdsa.
 	return sharedKey, err
 }
 
-// Send returns a shared key if we have received a message from all the installationIDs
-func (s *Service) Send(theirPublicKey *ecdsa.PublicKey, installationIDs []string) ([]byte, error) {
-	theirIdentity := crypto.CompressPubkey(theirPublicKey)
+// Receive will generate a shared secret for a given identity, and return it
+func (s *Service) Receive(myPrivateKey *ecdsa.PrivateKey, theirPublicKey *ecdsa.PublicKey, installationID string) ([]byte, error) {
+	return s.setupTopic(myPrivateKey, theirPublicKey, installationID)
+}
 
-	response, err := s.persistence.Get(theirIdentity, installationIDs)
+// Send returns a shared key and whether it has been acknowledged from all the installationIDs
+func (s *Service) Send(myPrivateKey *ecdsa.PrivateKey, myInstallationID string, theirPublicKey *ecdsa.PublicKey, theirInstallationIDs []string) ([]byte, bool, error) {
+	sharedKey, err := s.setupTopic(myPrivateKey, theirPublicKey, myInstallationID)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	for _, installationID := range installationIDs {
+	theirIdentity := crypto.CompressPubkey(theirPublicKey)
+	response, err := s.persistence.Get(theirIdentity, theirInstallationIDs)
+	if err != nil {
+		return nil, false, err
+	}
+
+	for _, installationID := range theirInstallationIDs {
 		if !response.installationIDs[installationID] {
-			return nil, nil
+			return sharedKey, false, nil
 		}
 	}
 
-	return response.secret, nil
+	return response.secret, true, nil
 }
 
 func (s *Service) All() ([][]byte, error) {
