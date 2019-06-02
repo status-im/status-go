0349f4ca999b1fc37431e8eb2391197cc6d86213
Dmitry <yashulyak@gmail.com>
2019-06-02 09:19:53 +0300

    Accept hex encoding in API and handle not specified start value

--- services/wallet/api.go
+++ services/wallet/api.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
 )
 
@@ -16,12 +17,15 @@ type API struct {
 // GetTransfers returns transfers in range of blocks. If `end` is nil all transfers from `start` will be returned.
 // TODO(dshulyak) benchmark loading many transfers from database. We can avoid json unmarshal/marshal if we will
 // read header, tx and receipt as a raw json.
-func (api *API) GetTransfers(ctx context.Context, start, end *big.Int) ([]Transfer, error) {
+func (api *API) GetTransfers(ctx context.Context, start, end *hexutil.Big) ([]Transfer, error) {
 	log.Debug("call to get transfers", "start", start, "end", end)
+	if start == nil {
+		return nil, errors.New("start of the query must be provided. use 0 if you want to load all transfers")
+	}
 	if api.s.db == nil {
 		return nil, errors.New("wallet service is not initialized")
 	}
-	rst, err := api.s.db.GetTransfers(start, end)
+	rst, err := api.s.db.GetTransfers((*big.Int)(start), (*big.Int)(end))
 	if err != nil {
 		return nil, err
 	}
