70b94723a247f3c7fa032280a47ce3a92d138f3c
Dmitry <yashulyak@gmail.com>
2019-06-10 08:48:06 +0300

    Rename ConcurrentRunner to AtomicGroup

--- services/wallet/concurrent.go
+++ services/wallet/concurrent.go
@@ -11,13 +11,13 @@ import (
 
 // NewConcurrentDownloader creates ConcurrentDownloader instance.
 func NewConcurrentDownloader(ctx context.Context) *ConcurrentDownloader {
-	runner := NewConcurrentRunner(ctx)
+	runner := NewAtomicGroup(ctx)
 	result := &Result{}
 	return &ConcurrentDownloader{runner, result}
 }
 
 type ConcurrentDownloader struct {
-	*ConcurrentRunner
+	*AtomicGroup
 	*Result
 }
 
@@ -26,7 +26,7 @@ type Result struct {
 	transfers []Transfer
 }
 
-func (r *Result) Add(transfers ...Transfer) {
+func (r *Result) Push(transfers ...Transfer) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	r.transfers = append(r.transfers, transfers...)
@@ -40,78 +40,13 @@ func (r *Result) Get() []Transfer {
 	return rst
 }
 
-func NewConcurrentRunner(ctx context.Context) *ConcurrentRunner {
-	// TODO(dshulyak) rename to atomic group and keep interface consistent with regular Group.
-	ctx, cancel := context.WithCancel(ctx)
-	return &ConcurrentRunner{
-		ctx:    ctx,
-		cancel: cancel,
-	}
-}
-
-// ConcurrentRunner runs group atomically.
-type ConcurrentRunner struct {
-	ctx    context.Context
-	cancel func()
-	wg     sync.WaitGroup
-
-	mu    sync.Mutex
-	error error
-}
-
-// Go spawns function in a goroutine and stores results or errors.
-func (d *ConcurrentRunner) Go(f func(context.Context) error) {
-	d.wg.Add(1)
-	go func() {
-		defer d.wg.Done()
-		err := f(d.ctx)
-		d.mu.Lock()
-		defer d.mu.Unlock()
-		if err != nil {
-			// do not overwrite original error by context errors
-			if d.error != nil {
-				return
-			}
-			d.error = err
-			d.cancel()
-			return
-		}
-	}()
-}
-
-// Wait for all downloaders to finish.
-func (d *ConcurrentRunner) Wait() {
-	d.wg.Wait()
-	if d.Error() == nil {
-		d.mu.Lock()
-		defer d.mu.Unlock()
-		d.cancel()
-	}
-}
-
-func (d *ConcurrentRunner) WaitAsync() <-chan struct{} {
-	ch := make(chan struct{})
-	go func() {
-		d.Wait()
-		close(ch)
-	}()
-	return ch
-}
-
-// Error stores an error that was reported by any of the downloader. Should be called after Wait.
-func (d *ConcurrentRunner) Error() error {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	return d.error
-}
-
 // TransferDownloader downloads transfers from single block using number.
 type TransferDownloader interface {
 	GetTransfersByNumber(context.Context, *big.Int) ([]Transfer, error)
 }
 
 func downloadEthConcurrently(c *ConcurrentDownloader, client BalanceReader, downloader TransferDownloader, account common.Address, low, high *big.Int) {
-	c.Go(func(ctx context.Context) error {
+	c.Add(func(ctx context.Context) error {
 		log.Debug("eth transfers comparing blocks", "low", low, "high", high)
 		lb, err := client.BalanceAt(ctx, account, low)
 		if err != nil {
@@ -131,7 +66,7 @@ func downloadEthConcurrently(c *ConcurrentDownloader, client BalanceReader, down
 			if err != nil {
 				return err
 			}
-			c.Add(transfers...)
+			c.Push(transfers...)
 			return nil
 		}
 		mid := new(big.Int).Add(low, high)
