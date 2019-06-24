7308d004f3e40a53f2fe6dab66859a1197318825
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2019-06-24 12:01:22 +0200

    Move installations to status-go
    This commit moves installations management/storage to status-go.
    We remove the native binding and provide RPC endpoints to set the
    metadata and return a list of our own installations.

---
+++ static/chat_db_migrations/1561368210_add_installation_metadata.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE installations ADD COLUMN name TEXT DEFAULT '';
+ALTER TABLE installations ADD COLUMN fcm_token TEXT DEFAULT '';
+ALTER TABLE installations ADD COLUMN device_type TEXT DEFAULT '';
