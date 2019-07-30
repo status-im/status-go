760466cf735ea9ad1ceb6d0a8776153f9b5f3aa6
Dmitry <yashulyak@gmail.com>
2019-07-30 11:11:59 +0300

    Changes to accounts structure

--- accountsstore/migrations/sql/0001_accounts.up.sql
+++ accountsstore/migrations/sql/0001_accounts.up.sql
@@ -3,11 +3,3 @@ address VARCHAR PRIMARY KEY,
 name TEXT NOT NULL,
 loginTimestamp BIG INT
 ) WITHOUT ROWID;
-
-CREATE TABLE IF NOT EXISTS configurations (
-address VARCHAR NOT NULL,
-type VARCHAR NOT NULL,
-value BLOB,
-FOREIGN KEY(address) REFERENCES accounts(address) ON DELETE CASCADE,
-CONSTRAINT unique_config_type_per_address UNIQUE (address,type)
-)
