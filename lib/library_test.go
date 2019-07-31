086df218ceee24f50f942ce4688dc45ab0d398ec
Dmitry <yashulyak@gmail.com>
2019-07-31 10:45:59 +0300

    Refactor in progress

--- lib/library_test.go
+++ lib/library_test.go
@@ -16,10 +16,7 @@ import (
 // the actual test functions are in non-_test.go files (so that they can use cgo i.e. import "C")
 // the only intent of these wrappers is for gotest can find what tests are exposed.
 func TestExportedAPI(t *testing.T) {
-	allTestsDone := make(chan struct{}, 1)
-	go testExportedAPI(t, allTestsDone)
-
-	<-allTestsDone
+	testExportedAPI(t)
 }
 
 func TestValidateNodeConfig(t *testing.T) {
