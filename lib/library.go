f37d2918a5f1ad73144c9d35795da521a567a1f1
Dmitry <yashulyak@gmail.com>
2019-08-06 13:43:14 +0300

    Add logs to login with save and regualr login

--- lib/library.go
+++ lib/library.go
@@ -372,7 +372,6 @@ func SaveAccountAndLogin(accountData, password, configJSON *C.char) *C.char {
 	}
 	api.RunAsync(func() error {
 		err := statusBackend.StartNodeWithAccountAndConfig(account, C.GoString(password), &conf)
-		fmt.Println(err)
 		return err
 	})
 	return makeJSONResponse(nil)
