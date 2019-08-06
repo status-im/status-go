ace28dd5aed26786551699e948775b194cc4ff8d
Dmitry <yashulyak@gmail.com>
2019-08-06 13:16:14 +0300

    Pass account manager to accounts service to avoid selecting account before starting a node

--- services/accounts/multiaccounts.go
+++ services/accounts/multiaccounts.go
@@ -3,7 +3,7 @@ package accounts
 import (
 	"errors"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/status-im/status-go/account"
 	"github.com/status-im/status-go/multiaccounts"
 )
 
@@ -12,18 +12,22 @@ var (
 	ErrUpdatingWrongAccount = errors.New("failed to updating wrong account. please login with that account first")
 )
 
-func NewMultiAccountsAPI(db *multiaccounts.Database, address common.Address) *MultiAccountsAPI {
-	return &MultiAccountsAPI{db: db, login: address}
+func NewMultiAccountsAPI(db *multiaccounts.Database, manager *account.Manager) *MultiAccountsAPI {
+	return &MultiAccountsAPI{db: db, manager: manager}
 }
 
 // MultiAccountsAPI is class with methods available over RPC.
 type MultiAccountsAPI struct {
-	db    *multiaccounts.Database
-	login common.Address
+	db      *multiaccounts.Database
+	manager *account.Manager
 }
 
 func (api *MultiAccountsAPI) UpdateAccount(account multiaccounts.Account) error {
-	if account.Address != api.login {
+	expected, err := api.manager.MainAccountAddress()
+	if err != nil {
+		return err
+	}
+	if account.Address != expected {
 		return ErrUpdatingWrongAccount
 	}
 	return api.db.UpdateAccount(account)
