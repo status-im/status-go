f8552280103000d720be98fa69ea09c7ae52d88f
yenda <eric@status.im>
2019-12-16 16:23:36 +0100

    add accounts_deleteAccount method (#1738)
    * add accounts_deleteAccount method
    
    * set account created and updated at dates, order by creation date

--- appdatabase/migrations/sql/0001_app.up.sql
+++ appdatabase/migrations/sql/0001_app.up.sql
@@ -12,11 +12,14 @@ storage TEXT,
 pubkey BLOB,
 path TEXT,
 name TEXT,
-color TEXT
+color TEXT,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL
 ) WITHOUT ROWID;
 
 CREATE UNIQUE INDEX unique_wallet_address ON accounts (wallet) WHERE (wallet);
 CREATE UNIQUE INDEX unique_chat_address ON accounts (chat) WHERE (chat);
+CREATE INDEX created_at_account ON accounts (created_at) WHERE (created_at);
 
 CREATE TABLE IF NOT EXISTS browsers (
 id TEXT PRIMARY KEY,
