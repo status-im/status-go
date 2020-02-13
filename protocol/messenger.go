7a97325850a7d658d4481b2f863af0f9e4157dc7
Jakub Sokołowski <jakub@status.im>
2020-02-13 12:20:45 +0100

    add GetContactByID to Messenger
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- protocol/messenger.go
+++ protocol/messenger.go
@@ -1147,8 +1147,18 @@ func (m *Messenger) Contacts() []*Contact {
 	for _, contact := range m.allContacts {
 		contacts = append(contacts, contact)
 	}
-
 	return contacts
+}
+
+// GetContactByID assumes pubKey includes 0x prefix
+func (m *Messenger) GetContactByID(pubKey string) (*Contact, error) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	contact, ok := m.allContacts[pubKey]
+	if !ok {
+		return nil, errors.New("no contact found")
+	}
+	return contact, nil
 }
 
 // ReSendChatMessage pulls a message from the database and sends it again
