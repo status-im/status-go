0c8efb4da5f2ee85561b916e3c82d917cb6a79b1
Jakub Sokołowski <jakub@status.im>
2020-03-16 22:38:42 +0100

    fix update-fleet-config.sh to include rendezvous
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- _assets/scripts/update-fleet-config.sh
+++ _assets/scripts/update-fleet-config.sh
@@ -15,6 +15,7 @@ for fleet in ${fleets[@]}; do
     fleetJSON=$(echo $json | jq ".fleets.\"$fleet\"")
     boot=$(echo $fleetJSON | jq ".boot | map(.)" -r)
     mail=$(echo $fleetJSON | jq ".mail | map(.)" -r)
+    rendezvous=$(echo $fleetJSON | jq ".rendezvous | map(.)" -r)
 
     # Get random nodes from whisper node list
     maxStaticNodeCount=2
@@ -23,7 +24,12 @@ for fleet in ${fleets[@]}; do
     whisper=$(echo $fleetJSON | jq ".whisper | map(.) | .[$index:($index + $maxStaticNodeCount)]" -r)
 
     git checkout $DIR/fleet-$fleet.json \
-        && jq ".ClusterConfig.BootNodes = $boot | .ClusterConfig.TrustedMailServers = $mail | .ClusterConfig.StaticNodes = $whisper" $DIR/fleet-$fleet.json \
+        && jq \
+              ".ClusterConfig.BootNodes = $boot \
+             | .ClusterConfig.TrustedMailServers = $mail \
+             | .ClusterConfig.StaticNodes = $whisper \
+             | .ClusterConfig.RendezvousNodes = $rendezvous" \
+             $DIR/fleet-$fleet.json \
         | tee "$DIR/tmp.json" >/dev/null \
         && mv $DIR/tmp.json $DIR/fleet-$fleet.json
 done
