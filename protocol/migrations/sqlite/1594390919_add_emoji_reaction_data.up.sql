a899264bc7eb5801a3c815eb8a241916e32a61e0
Samuel Hawksby-Robinson <samuel@samyoul.com>
2020-07-10 15:27:18 +0100

    Added emoji reaction migrations

---
+++ protocol/migrations/sqlite/1594390919_add_emoji_reaction_data.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE user_messages ADD COLUMN emoji_reaction_target VARCHAR;
+ALTER TABLE user_messages ADD COLUMN emoji_reaction_type INT;
+ALTER TABLE user_messages ADD COLUMN emoji_reaction_retraction_target VARCHAR;
