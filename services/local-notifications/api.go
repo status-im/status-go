2b4bdc6e2de29926ab2c83e34ce8183d8651a126
Gheorghe Pinzaru <feross95@gmail.com>
2020-09-30 14:42:10 +0300

    Add api method

--- services/local-notifications/api.go
+++ services/local-notifications/api.go
@@ -1,9 +1,20 @@
 package localnotifications
 
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/log"
+)
+
 func NewAPI(s *Service) *API {
 	return &API{s}
 }
 
 type API struct {
 	s *Service
+}
+
+func (api *API) SwitchWalletNotifications(ctx context.Context, _ bool) error {
+	log.Debug("Switch Transaction Notification")
+	return nil
 }
