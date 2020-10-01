59e6602405bfbcf8446d26aca9b8087e84529f8e
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2020-10-01 10:31:23 +0200

    Force migration if dirty

--- protocol/migrations/sqlite/1599720851_add_seen_index_remove_long_messages.up.sql
+++ protocol/migrations/sqlite/1599720851_add_seen_index_remove_long_messages.up.sql
@@ -1,2 +1,2 @@
-CREATE INDEX seen_local_chat_id_idx ON user_messages(local_chat_id, seen);
+CREATE INDEX IF NOT EXISTS seen_local_chat_id_idx ON user_messages(local_chat_id, seen);
 UPDATE user_messages SET hide = 1 WHERE length(text) > 4096;
