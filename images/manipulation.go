bb1e232ffe5d684522c0886a315f8dc93ed422e9
Samuel Hawksby-Robinson <samuel@samyoul.com>
2020-10-08 21:24:41 +0100

    Improved images.Crop() and implemented testing

--- images/manipulation.go
+++ images/manipulation.go
@@ -1,20 +1,30 @@
 package images
 
 import (
+	"fmt"
 	"image"
 
 	"github.com/nfnt/resize"
 	"github.com/oliamb/cutter"
 )
 
-func ResizeSquare(size uint, img image.Image) image.Image {
-	return resize.Resize(size, 0, img, resize.Bilinear)
+func Resize(width, height uint, img image.Image) image.Image {
+	return resize.Resize(width, height, img, resize.Bilinear)
 }
 
-func CropSquare(img image.Image, size int, anchor image.Point) (image.Image, error) {
+func Crop(img image.Image, rect image.Rectangle) (image.Image, error) {
+
+	if img.Bounds().Max.X < rect.Max.X || img.Bounds().Max.Y < rect.Max.Y{
+		return nil, fmt.Errorf(
+			"crop dimensions out of bounds of image, image width '%dpx' & height '%dpx'; crop bottom right coordinate at X '%dpx' Y '%dpx'",
+			img.Bounds().Max.X, img.Bounds().Max.Y,
+			rect.Max.X, rect.Max.Y,
+		)
+	}
+
 	return cutter.Crop(img, cutter.Config{
-		Width:  size,
-		Height: size,
-		Anchor: anchor,
+		Width:  rect.Dx(),
+		Height: rect.Dy(),
+		Anchor: rect.Min,
 	})
 }
