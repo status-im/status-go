90000aacedf57ce721faa00c9961bd143d075fbf
Samuel Hawksby-Robinson <samuel@samyoul.com>
2020-10-22 16:59:01 +0100

    Added TestEncodeToBestSize()

--- images/meta.go
+++ images/meta.go
@@ -16,11 +16,11 @@ const (
 )
 
 var (
-	ResizeDimensions   = []uint{80, 240}
+	ResizeDimensions   = []ResizeDimension{80, 240}
 
 	// DimensionSizeLimit the size limits imposed on each resize dimension
 	// Figures are based on the following sample data https://github.com/status-im/status-react/issues/11047#issuecomment-694970473
-	DimensionSizeLimit = map[uint]DimensionSize{
+	DimensionSizeLimit = map[ResizeDimension]DimensionLimits{
 		80: {
 			Ideal: 2560, // Base on the largest sample image at quality 60% (2,554 bytes ∴ 1024 * 2.5)
 			Max:   5632, // Base on the largest sample image at quality 80% + 50% margin (3,683 bytes * 1.5 ≈ 5500 ∴ 1024 * 5.5)
@@ -32,9 +32,10 @@ var (
 	}
 )
 
-type DimensionSize struct {
+type DimensionLimits struct {
 	Ideal int
 	Max   int
 }
 
 type FileType uint
+type ResizeDimension uint
