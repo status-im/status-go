cd3e9979ec7ff9bde12ab070628f5eeed33f0d59
Samuel Hawksby-Robinson <samuel@samyoul.com>
2020-10-26 13:02:42 +0000

    Fix to identity_images migration sql

--- appdatabase/migrations/sql/0013_identity_images.up.sql
+++ appdatabase/migrations/sql/0013_identity_images.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS identity_images(
     type VARCHAR,
     image_payload BLOB NOT NULL,
     width int,
-    height int
-    filesize int,
-    resize_target int,
+    height int,
+    file_size int,
+    resize_target int
 ) WITHOUT ROWID;
 
