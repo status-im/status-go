e6b60ff65f367292b4035c17151652082c806be6
Samuel Hawksby-Robinson <samuel@samyoul.com>
2020-12-01 12:55:18 +0000

    Update to migration to handle change of photoPath to identicon

--- multiaccounts/migrations/sql/1606224181_drop_photo_path_from_accounts.down.sql
+++ multiaccounts/migrations/sql/1606224181_drop_photo_path_from_accounts.down.sql
@@ -1,2 +1,23 @@
-ALTER TABLE accounts
-ADD COLUMN photoPath TEXT;
+/* Copy the accounts table into a temp table, EXCLUDE the `identicon` column and INCLUDE the `photoPath` column */
+CREATE TEMPORARY TABLE accounts_backup(
+    keyUid VARCHAR PRIMARY KEY,
+    name TEXT NOT NULL,
+    loginTimestamp BIG INT,
+    photoPath TEXT,
+    keycardPairing TEXT
+) WITHOUT ROWID;
+INSERT INTO accounts_backup SELECT keyUid, name, loginTimestamp, identicon, keycardPairing FROM accounts;
+
+/* Drop the old accounts table and recreate with all columns EXCLUDING `identicon` and INCLUDING `photoPath` */
+DROP TABLE accounts;
+CREATE TABLE IF NOT EXISTS accounts (
+    keyUid VARCHAR PRIMARY KEY,
+    name TEXT NOT NULL,
+    loginTimestamp BIG INT,
+    photoPath TEXT,
+    keycardPairing TEXT
+) WITHOUT ROWID;
+INSERT INTO accounts SELECT keyUid, name, loginTimestamp, photoPath, keycardPairing FROM accounts_backup;
+
+/* Tidy up, drop the temp table */
+DROP TABLE accounts_backup;
--- multiaccounts/migrations/sql/1606224181_drop_photo_path_from_accounts.up.sql
+++ multiaccounts/migrations/sql/1606224181_drop_photo_path_from_accounts.up.sql
@@ -1,16 +1,23 @@
-/* Copy the accounts table into a temp table, EXCLUDE the `photoPath` column */
+/* Copy the accounts table into a temp table, EXCLUDE the `photoPath` and INCLUDE `identicon` column */
 CREATE TEMPORARY TABLE accounts_backup(
     keyUid VARCHAR PRIMARY KEY,
     name TEXT NOT NULL,
     loginTimestamp BIG INT,
+    identicon TEXT,
     keycardPairing TEXT
 ) WITHOUT ROWID;
-INSERT INTO accounts_backup SELECT keyUid, name, loginTimestamp, keycardPairing FROM accounts;
+INSERT INTO accounts_backup SELECT keyUid, name, loginTimestamp, photoPath, keycardPairing FROM accounts;
 
-/* Drop the old accounts table and recreate with all columns EXCLUDING `photoPath` */
+/* Drop the old accounts table and recreate with all columns EXCLUDING `photoPath` and INCLUDING `identicon`*/
 DROP TABLE accounts;
-CREATE TABLE accounts(keyUid, name, loginTimestamp, keycardPairing);
-INSERT INTO accounts SELECT keyUid, name, loginTimestamp, keycardPairing FROM accounts_backup;
+CREATE TABLE accounts(
+    keyUid VARCHAR PRIMARY KEY,
+    name TEXT NOT NULL,
+    loginTimestamp BIG INT,
+    identicon TEXT,
+    keycardPairing TEXT
+ ) WITHOUT ROWID;
+INSERT INTO accounts SELECT keyUid, name, loginTimestamp, identicon, keycardPairing FROM accounts_backup;
 
 /* Tidy up, drop the temp table */
 DROP TABLE accounts_backup;
