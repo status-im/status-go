1f58ba87d88b4a47226afcca375f29e6b405b49a
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-12-18 12:11:44 +0200

    Resolve incorrect envelope sending

--- protocol/transport/waku/envelopes_test.go
+++ protocol/transport/waku/envelopes_test.go
@@ -30,19 +30,23 @@ func TestEnvelopesMonitorSuite(t *testing.T) {
 }
 
 func (s *EnvelopesMonitorSuite) SetupTest() {
+
+	logger, _ := zap.NewDevelopment()
+
 	s.monitor = NewEnvelopesMonitor(
 		nil,
 		transport.EnvelopesMonitorConfig{
-			EnvelopeEventsHandler:          nil,
-			MaxAttempts:                    0,
-			MailserverConfirmationsEnabled: false,
-			IsMailserver:                   func(types.EnodeID) bool { return false },
-			Logger:                         zap.NewNop(),
+			EnvelopeEventsHandler:            nil,
+			MaxAttempts:                      0,
+			AwaitOnlyMailServerConfirmations: false,
+			IsMailserver:                     func(types.EnodeID) bool { return false },
+			Logger:                           logger, //zap.NewNop(),
 		},
 	)
 }
 
-func (s *EnvelopesMonitorSuite) TestConfirmed() {
+func (s *EnvelopesMonitorSuite) TestNotSentWithoutAcknowledge() {
+
 	s.monitor.Add(testIDs, testHash, types.NewMessage{})
 	s.Contains(s.monitor.envelopes, testHash)
 	s.Equal(EnvelopePosted, s.monitor.envelopes[testHash])
@@ -51,7 +55,7 @@ func (s *EnvelopesMonitorSuite) TestConfirmed() {
 		Hash:  testHash,
 	})
 	s.Contains(s.monitor.envelopes, testHash)
-	s.Equal(EnvelopeSent, s.monitor.envelopes[testHash])
+	s.Equal(EnvelopePosted, s.monitor.envelopes[testHash])
 }
 
 func (s *EnvelopesMonitorSuite) TestConfirmedWithAcknowledge() {
@@ -97,7 +101,7 @@ func (s *EnvelopesMonitorSuite) TestRemoved() {
 
 func (s *EnvelopesMonitorSuite) TestIgnoreNotFromMailserver() {
 	// enables filter in the tracker to drop confirmations from non-mailserver peers
-	s.monitor.mailServerConfirmation = true
+	s.monitor.awaitOnlyMailServerConfirmations = true
 	s.monitor.Add(testIDs, testHash, types.NewMessage{})
 	s.monitor.handleEvent(types.EnvelopeEvent{
 		Event: types.EventEnvelopeSent,
--- services/ext/service.go
+++ services/ext/service.go
@@ -139,8 +139,8 @@ func (s *Service) InitProtocol(identity *ecdsa.PrivateKey, db *sql.DB, multiAcco
 	}
 
 	envelopesMonitorConfig := &transport.EnvelopesMonitorConfig{
-		MaxAttempts:                    s.config.MaxMessageDeliveryAttempts,
-		MailserverConfirmationsEnabled: s.config.MailServerConfirmations,
+		MaxAttempts:                      s.config.MaxMessageDeliveryAttempts,
+		AwaitOnlyMailServerConfirmations: s.config.MailServerConfirmations,
 		IsMailserver: func(peer types.EnodeID) bool {
 			return s.peerStore.Exist(peer)
 		},
