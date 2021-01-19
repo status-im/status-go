f7a9b8a53851393fb8f90f7c878028d1631c0837
Shivek Khurana <khuranashivek@outlook.com>
2021-01-19 21:23:27 +0530

    Giphy oembed in place

--- protocol/urls/urls_test.go
+++ protocol/urls/urls_test.go
@@ -30,3 +30,21 @@ func TestGetLinkPreviewData(t *testing.T) {
 	require.Error(t, err)
 
 }
+
+func TestGetGiphyPreviewData(t *testing.T) {
+	validGiphyLink := "https://giphy.com/gifs/FullMag-robot-boston-dynamics-dance-lcG3qwtTKSNI2i5vst"
+	previewData, err := GetGiphyPreviewData(validGiphyLink)
+	bostonDynamicsEthGifData := LinkPreviewData{
+		Site:         "GIPHY",
+		Title:        "Boston Dynamics Yes GIF by FullMag - Find & Share on GIPHY",
+		ThumbnailURL: "https://media1.giphy.com/media/lcG3qwtTKSNI2i5vst/giphy.gif",
+	}
+	require.NoError(t, err)
+	require.Equal(t, bostonDynamicsEthGifData.Site, previewData.Site)
+	require.Equal(t, bostonDynamicsEthGifData.Title, previewData.Title)
+	require.Equal(t, bostonDynamicsEthGifData.ThumbnailURL, previewData.ThumbnailURL)
+
+	invalidGiphyLink := "https://giphy.com/gifs/this-gif-does-not-exist-44444"
+	_, err = GetGiphyPreviewData(invalidGiphyLink)
+	require.Error(t, err)
+}
