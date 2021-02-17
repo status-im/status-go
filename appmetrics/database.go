c31fd4ff4eb3bbd49921601ed5837b70d83b844d
Shivek Khurana <khuranashivek@outlook.com>
2021-02-17 17:47:21 +0530

    Database and Database Test package in place, added functions to get and store app metrics

---
+++ appmetrics/database.go
@@ -0,0 +1,88 @@
+package appmetrics
+
+import (
+	"database/sql"
+)
+
+type AppMetricEventType string
+
+type AppMetric struct {
+	Event      AppMetricEventType `json:"event"`
+	Value      string             `json:"value"`
+	AppVersion string             `json:"app_version"`
+	OS         string             `json:"os"`
+}
+
+const (
+	TestEvent1 AppMetricEventType = "go/test1"
+	TestEvent2 AppMetricEventType = "go/test2"
+	AppRouter  AppMetricEventType = "app/router"
+)
+
+func NewDB(db *sql.DB) *Database {
+	return &Database{db: db}
+}
+
+// Database sql wrapper for operations with browser objects.
+type Database struct {
+	db *sql.DB
+}
+
+// Close closes database.
+func (db Database) Close() error {
+	return db.db.Close()
+}
+
+func (db *Database) SaveAppMetrics(appMetrics []AppMetric) (err error) {
+	var (
+		tx     *sql.Tx
+		insert *sql.Stmt
+	)
+
+	tx, err = db.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+			return
+		}
+		_ = tx.Rollback()
+	}()
+
+	insert, err = tx.Prepare("INSERT INTO app_metrics (event, value, app_version, operating_system)")
+	if err != nil {
+		return err
+	}
+
+	for i := range appMetrics {
+		metric := &appMetrics[i]
+		_, err = insert.Exec(metric.Event, metric.Value, metric.AppVersion, metric.OS)
+
+		if err != nil {
+			return err
+		}
+	}
+	return
+}
+
+func (db *Database) GetAppMetrics(limit int, offset int) (appMetrics []AppMetric, err error) {
+	rows, err := db.db.Query("SELECT * FROM app_metrics LIMIT $1 OFFSET $2", limit, offset)
+
+	if err != nil {
+		return appMetrics, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		metric := AppMetric{}
+		err := rows.Scan(&metric.Event, &metric.Value, &metric.AppVersion, &metric.OS)
+
+		if err != nil {
+			return appMetrics, err
+		}
+		appMetrics = append(appMetrics, metric)
+	}
+	return appMetrics, nil
+}
---
+++ appmetrics/database_test.go
@@ -0,0 +1,45 @@
+package appmetrics
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/status-im/status-go/appdatabase"
+	"github.com/stretchr/testify/require"
+)
+
+func setupTestDB(t *testing.T) (*Database, func()) {
+	tmpfile, err := ioutil.TempFile("", "appmetrics-tests-")
+	require.NoError(t, err)
+	db, err := appdatabase.InitializeDB(tmpfile.Name(), "appmetrics-tests")
+	require.NoError(t, err)
+
+	return NewDB(db), func() {
+		require.NoError(t, db.Close())
+		require.NoError(t, os.Remove(tmpfile.Name()))
+	}
+}
+
+func TestOneIsOne(t *testing.T) {
+	_, stop := setupTestDB(t)
+	defer stop()
+
+	require.Equal(t, 1, 1)
+}
+
+func TestSaveAppMetrics(t *testing.T) {
+	db, stop := setupTestDB(t)
+	defer stop()
+
+	appMetrics := []AppMetric{
+		{Event: TestEvent1, Value: "1", OS: "android", AppVersion: "1.11"},
+		{Event: TestEvent2, Value: "2", OS: "ios", AppVersion: "1.10"},
+	}
+
+	db.SaveAppMetrics(appMetrics)
+
+	res, err := db.GetAppMetrics(10, 10)
+	require.NoError(t, err)
+	t.Log(res)
+}
