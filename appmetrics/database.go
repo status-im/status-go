35fbc40916c132e19bd95904dfe7a723e1027791
Shivek Khurana <khuranashivek@outlook.com>
2021-04-12 18:59:33 +0530

    Expose sessionId and created at

--- appmetrics/database.go
+++ appmetrics/database.go
@@ -19,6 +19,8 @@ type AppMetric struct {
 	Value      json.RawMessage    `json:"value"`
 	AppVersion string             `json:"app_version"`
 	OS         string             `json:"os"`
+	SessionID  string             `json:"session_id"`
+	CreatedAt  string             `json:"created_at"`
 }
 
 type AppMetricValidationError struct {
@@ -139,7 +141,7 @@ func (db *Database) SaveAppMetrics(appMetrics []AppMetric, sessionID string) (er
 }
 
 func (db *Database) GetAppMetrics(limit int, offset int) (appMetrics []AppMetric, err error) {
-	rows, err := db.db.Query("SELECT event, value, app_version, operating_system FROM app_metrics LIMIT ? OFFSET ?", limit, offset)
+	rows, err := db.db.Query("SELECT event, value, app_version, operating_system, session_id, created_at FROM app_metrics LIMIT ? OFFSET ?", limit, offset)
 
 	if err != nil {
 		return nil, err
@@ -147,8 +149,11 @@ func (db *Database) GetAppMetrics(limit int, offset int) (appMetrics []AppMetric
 	defer rows.Close()
 	for rows.Next() {
 		metric := AppMetric{}
-		err := rows.Scan(&metric.Event, &metric.Value, &metric.AppVersion, &metric.OS)
-
+		err := rows.Scan(
+			&metric.Event, &metric.Value,
+			&metric.AppVersion, &metric.OS,
+			&metric.SessionID, &metric.CreatedAt,
+		)
 		if err != nil {
 			return nil, err
 		}
