3d8a808111042ea73c2f2238ee8d8afe65f8595c
Samuel Hawksby-Robinson <samuel@samyoul.com>
2021-04-16 13:01:26 +0100

    Added id column to app metrics table

---
+++ appdatabase/migrations/sql/1618571271_add_id_to_app_metrics.up.sql
@@ -0,0 +1,13 @@
+ALTER TABLE app_metrics RENAME TO temp_app_metrics;
+CREATE TABLE app_metrics (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event VARCHAR NOT NULL,
+    value TEXT NOT NULL,
+    app_version VARCHAR NOT NULL,
+    operating_system VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    session_id VARCHAR,
+    processed BOOLEAN DEFAULT FALSE);
+INSERT INTO app_metrics(event, value, app_version, operating_system, created_at, session_id, processed)
+SELECT event, value, app_version, operating_system, created_at, session_id, processed
+FROM temp_app_metrics;
