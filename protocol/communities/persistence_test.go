3731eba989fcc7b5cb69d3350c3e442a0763f51a
Samuel Hawksby-Robinson <samuel@samyoul.com>
2021-06-22 11:32:20 +0100

    Finshed persistence tests for new db funcs

--- protocol/communities/persistence_test.go
+++ protocol/communities/persistence_test.go
@@ -1,13 +1,14 @@
 package communities
 
 import (
-	"github.com/status-im/status-go/protocol/protobuf"
 	"io/ioutil"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/suite"
 
+	"github.com/status-im/status-go/eth-node/crypto"
+	"github.com/status-im/status-go/protocol/protobuf"
 	"github.com/status-im/status-go/protocol/sqlite"
 )
 
@@ -30,17 +31,17 @@ func (s *PersistenceSuite) SetupTest() {
 	db, err := sqlite.Open(dbPath.Name(), "")
 	s.NoError(err, "creating sqlite db instance")
 
-	s.db = &Persistence{ db: db }
+	s.db = &Persistence{db: db}
 }
 
 func (s *PersistenceSuite) TestShouldHandleSyncCommunity() {
 	sc := &protobuf.SyncCommunity{
-		Id:                   []byte("0x123456"),
-		PrivateKey:           []byte("0xfedcba"),
-		Description:          []byte("this is a description"),
-		Joined:               true,
-		Verified:             true,
-		Clock:                uint64(time.Now().Unix()),
+		Id:          []byte("0x123456"),
+		PrivateKey:  []byte("0xfedcba"),
+		Description: []byte("this is a description"),
+		Joined:      true,
+		Verified:    true,
+		Clock:       uint64(time.Now().Unix()),
 	}
 
 	// check an empty db to see if a community should be synced
@@ -68,4 +69,82 @@ func (s *PersistenceSuite) TestShouldHandleSyncCommunity() {
 	should, err = s.db.ShouldHandleSyncCommunity(sc)
 	s.NoError(err, "SaveSyncCommunity")
 	s.True(should)
+}
+
+func (s *PersistenceSuite) TestSetSyncClock() {
+	sc := &protobuf.SyncCommunity{
+		Id:          []byte("0x123456"),
+		PrivateKey:  []byte("0xfedcba"),
+		Description: []byte("this is a description"),
+		Joined:      true,
+		Verified:    true,
+	}
+
+	// add a new community to the db
+	err := s.db.saveRawCommunityRow(fromSyncCommunityProtobuf(sc))
+	s.NoError(err, "saveRawCommunityRow")
+
+	// retrieve row from db synced_at must be zero
+	rcr, err := s.db.getRawCommunityRow(sc.Id)
+	s.NoError(err, "getRawCommunityRow")
+	s.Zero(rcr.SyncedAt, "synced_at must be zero value")
+
+	// Set the synced_at value
+	clock := uint64(time.Now().Unix())
+	err = s.db.SetSyncClock(sc.Id, clock)
+	s.NoError(err, "SetSyncClock")
+
+	// Retrieve row from db and check clock matches synced_at value
+	rcr, err = s.db.getRawCommunityRow(sc.Id)
+	s.NoError(err, "getRawCommunityRow")
+	s.Equal(clock, rcr.SyncedAt, "synced_at must equal the value of the clock")
+
+	// Set Synced At with an older clock value
+	olderClock := clock - uint64(256)
+	err = s.db.SetSyncClock(sc.Id, olderClock)
+	s.NoError(err, "SetSyncClock")
+
+	// Retrieve row from db and check olderClock matches synced_at value
+	rcr, err = s.db.getRawCommunityRow(sc.Id)
+	s.NoError(err, "getRawCommunityRow")
+	s.NotEqual(olderClock, rcr.SyncedAt, "synced_at must not equal the value of the olderClock value")
+
+	// Set Synced At with a newer clock value
+	newerClock := clock + uint64(512)
+	err = s.db.SetSyncClock(sc.Id, newerClock)
+	s.NoError(err, "SetSyncClock")
+
+	// Retrieve row from db and check olderClock matches synced_at value
+	rcr, err = s.db.getRawCommunityRow(sc.Id)
+	s.NoError(err, "getRawCommunityRow")
+	s.Equal(newerClock, rcr.SyncedAt, "synced_at must equal the value of the newerClock value")
+}
+
+func (s *PersistenceSuite) TestSetPrivateKey() {
+	sc := &protobuf.SyncCommunity{
+		Id:          []byte("0x123456"),
+		Description: []byte("this is a description"),
+		Joined:      true,
+		Verified:    true,
+	}
+
+	// add a new community to the db with no private key
+	err := s.db.saveRawCommunityRow(fromSyncCommunityProtobuf(sc))
+	s.NoError(err, "saveRawCommunityRow")
+
+	// retrieve row from db, private key must be zero
+	rcr, err := s.db.getRawCommunityRow(sc.Id)
+	s.NoError(err, "getRawCommunityRow")
+	s.Zero(rcr.PrivateKey, "private key must be zero value")
+
+	// Set private key
+	pk, err := crypto.GenerateKey()
+	s.NoError(err, "crypto.GenerateKey")
+	err = s.db.SetPrivateKey(sc.Id, pk)
+	s.NoError(err, "SetPrivateKey")
+
+	// retrieve row from db again, private key must match the given key
+	rcr, err = s.db.getRawCommunityRow(sc.Id)
+	s.NoError(err, "getRawCommunityRow")
+	s.Equal(crypto.FromECDSA(pk), rcr.PrivateKey, "private key must match given key")
 }
