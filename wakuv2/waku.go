b7c8229af6b3c85eda5b296b804805e7ea259444
Vitaliy Vlasov <siphiuel@gmail.com>
2021-07-21 13:08:08 +0300

    Pass connection status chan to go-waku

--- wakuv2/waku.go
+++ wakuv2/waku.go
@@ -155,7 +155,6 @@ func New(nodeKey string, cfg *Config, logger *zap.Logger) (*Waku, error) {
 
 	connStatusChan := make(chan node.ConnStatus)
 
-	keepAliveInt := 1
 	waku.node, err = node.New(context.Background(),
 		node.WithLibP2POptions(
 			libp2p.BandwidthReporter(waku.bandwidthCounter),
