d35fa5b434197cec6455c2060df2713c0f39d9a5
Richard Ramos <info@richardramos.me>
2021-07-22 10:39:02 -0400

    fix: code review

--- appdatabase/migrations/sql/1625872445_user_status.up.sql
+++ appdatabase/migrations/sql/1625872445_user_status.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE settings ADD COLUMN current_user_status BLOB;
 ALTER TABLE settings ADD COLUMN send_status_updates BOOLEAN DEFAULT TRUE;
-
+UPDATE settings SET send_status_updates = TRUE;
 CREATE TABLE status_updates (
   public_key TEXT PRIMARY KEY ON CONFLICT REPLACE,
   status_type INT NOT NULL DEFAULT 0,
