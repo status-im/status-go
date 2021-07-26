0c0e308ef1c8e43ab78fcff8ce166604d63b2e42
Jonathan Rainville <rainville.jonathan@gmail.com>
2021-07-26 14:00:10 -0400

    feat: add api and handling to delete messages

--- protocol/messenger_messages.go
+++ protocol/messenger_messages.go
@@ -9,7 +9,7 @@ import (
 	"github.com/status-im/status-go/protocol/requests"
 )
 
-var ErrInvalidEditAuthor = errors.New("sender is not the author of the message")
+var ErrInvalidEditOrDeleteAuthor = errors.New("sender is not the author of the message")
 var ErrInvalidEditContentType = errors.New("only text messages can be replaced")
 
 func (m *Messenger) EditMessage(ctx context.Context, request *requests.EditMessage) (*MessengerResponse, error) {
@@ -23,7 +23,7 @@ func (m *Messenger) EditMessage(ctx context.Context, request *requests.EditMessa
 	}
 
 	if message.From != common.PubkeyToHex(&m.identity.PublicKey) {
-		return nil, ErrInvalidEditAuthor
+		return nil, ErrInvalidEditOrDeleteAuthor
 	}
 
 	if message.ContentType != protobuf.ChatMessage_TEXT_PLAIN {
@@ -82,6 +82,71 @@ func (m *Messenger) EditMessage(ctx context.Context, request *requests.EditMessa
 	return response, nil
 }
 
+func (m *Messenger) DeleteMessageAndSend(ctx context.Context, messageID string) (*MessengerResponse, error) {
+	message, err := m.persistence.MessageByID(messageID)
+	if err != nil {
+		return nil, err
+	}
+
+	if message.From != common.PubkeyToHex(&m.identity.PublicKey) {
+		return nil, ErrInvalidEditOrDeleteAuthor
+	}
+
+	// A valid added chat is required.
+	chat, ok := m.allChats.Load(message.ChatId)
+	if !ok {
+		return nil, errors.New("Chat not found")
+	}
+
+	clock, _ := chat.NextClockAndTimestamp(m.getTimesource())
+
+	deleteMessage := &DeleteMessage{}
+
+	deleteMessage.ChatId = message.ChatId
+	deleteMessage.MessageId = messageID
+	deleteMessage.Clock = clock
+
+	encodedMessage, err := m.encodeChatEntity(chat, deleteMessage)
+
+	if err != nil {
+		return nil, err
+	}
+
+	rawMessage := common.RawMessage{
+		LocalChatID:          chat.ID,
+		Payload:              encodedMessage,
+		MessageType:          protobuf.ApplicationMetadataMessage_DELETE_MESSAGE,
+		SkipGroupMessageWrap: true,
+		ResendAutomatically:  true,
+	}
+	_, err = m.dispatchMessage(ctx, rawMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	message.Deleted = true
+	m.persistence.SaveMessages([]*common.Message{message})
+
+	err = m.persistence.HideMessage(messageID)
+	if err != nil {
+		return nil, err
+	}
+
+	if chat.LastMessage != nil && chat.LastMessage.ID == message.ID {
+		chat.LastMessage = message
+		err := m.saveChat(chat)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	response := &MessengerResponse{}
+	response.AddMessage(message)
+	response.AddChat(chat)
+
+	return response, nil
+}
+
 func (m *Messenger) applyEditMessage(editMessage *protobuf.EditMessage, message *common.Message) error {
 	if err := ValidateText(editMessage.Text); err != nil {
 		return err
@@ -109,4 +174,24 @@ func (m *Messenger) applyEditMessage(editMessage *protobuf.EditMessage, message 
 	}
 
 	return m.persistence.SaveMessages([]*common.Message{message})
+}
+
+func (m *Messenger) applyDeleteMessage(messageDeletes []*DeleteMessage, message *common.Message) error {
+	if messageDeletes[0].From != message.From {
+		return ErrInvalidEditOrDeleteAuthor
+	}
+
+	message.Deleted = true
+
+	err := message.PrepareContent(common.PubkeyToHex(&m.identity.PublicKey))
+	if err != nil {
+		return err
+	}
+
+	err = m.persistence.SaveMessages([]*common.Message{message})
+	if err != nil {
+		return err
+	}
+
+	return m.persistence.HideMessage(message.ID)
 }
