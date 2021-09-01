f5f232847747a76c1b3bc4ab9cd345183be77852
Richard Ramos <info@richardramos.me>
2021-09-01 17:25:31 -0400

    fix: waku2 history query

--- wakuv2/waku.go
+++ wakuv2/waku.go
@@ -556,10 +556,10 @@ func (w *Waku) Send(msg *pb.WakuMessage) ([]byte, error) {
 	return w.node.Publish(context.Background(), msg, nil)
 }
 
-func (w *Waku) Query(topics []types.TopicType, from uint64, to uint64, opts []store.HistoryRequestOption) (cursor *pb.Index, err error) {
+func (w *Waku) Query(topics []common.TopicType, from uint64, to uint64, opts []store.HistoryRequestOption) (cursor *pb.Index, err error) {
 	strTopics := make([]string, len(topics))
 	for i, t := range topics {
-		strTopics[i] = t.String()
+		strTopics[i] = t.ContentTopic()
 	}
 
 	result, err := w.node.Query(context.Background(), strTopics, float64(from), float64(to), opts...)
