06d4163de873df1a135044ba570741017880a8d0
Eric Mastro <ericmastro@status.im>
2021-09-11 04:08:22 +1000

    feat: Add Saved Addresses CRUD (#2356)

---
+++ services/wallet/saved_addresses.go
@@ -0,0 +1,54 @@
+package wallet
+
+import (
+	"database/sql"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type SavedAddress struct {
+	Address common.Address `json:"address"`
+	// TODO: Add Emoji and Networks
+	// Emoji    string         `json:"emoji"`
+	Name    string `json:"name"`
+	ChainID uint64 `json:"chainId"`
+}
+
+type SavedAddressesManager struct {
+	db *sql.DB
+}
+
+func (sam *SavedAddressesManager) GetSavedAddresses(chainID uint64) ([]*SavedAddress, error) {
+	rows, err := sam.db.Query("SELECT address, name, network_id FROM saved_addresses WHERE network_id = ?", chainID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var rst []*SavedAddress
+	for rows.Next() {
+		sa := &SavedAddress{}
+		err := rows.Scan(&sa.Address, &sa.Name, &sa.ChainID)
+		if err != nil {
+			return nil, err
+		}
+
+		rst = append(rst, sa)
+	}
+
+	return rst, nil
+}
+
+func (sam *SavedAddressesManager) AddSavedAddress(sa SavedAddress) error {
+	insert, err := sam.db.Prepare("INSERT OR REPLACE INTO saved_addresses (network_id, address, name) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = insert.Exec(sa.ChainID, sa.Address, sa.Name)
+	return err
+}
+
+func (sam *SavedAddressesManager) DeleteSavedAddress(chainID uint64, address common.Address) error {
+	_, err := sam.db.Exec(`DELETE FROM saved_addresses WHERE address = ? AND network_id = ?`, address, chainID)
+	return err
+}
---
+++ services/wallet/saved_addresses_test.go
@@ -0,0 +1,54 @@
+package wallet
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/status-im/status-go/appdatabase"
+)
+
+func setupTestSavedAddressesDB(t *testing.T) (*SavedAddressesManager, func()) {
+	tmpfile, err := ioutil.TempFile("", "wallet-saved_addresses-tests-")
+	require.NoError(t, err)
+	db, err := appdatabase.InitializeDB(tmpfile.Name(), "wallet-saved_addresses-tests")
+	require.NoError(t, err)
+	return &SavedAddressesManager{db}, func() {
+		require.NoError(t, db.Close())
+		require.NoError(t, os.Remove(tmpfile.Name()))
+	}
+}
+
+func TestSavedAddresses(t *testing.T) {
+	manager, stop := setupTestSavedAddressesDB(t)
+	defer stop()
+
+	rst, err := manager.GetSavedAddresses(777)
+	require.NoError(t, err)
+	require.Nil(t, rst)
+
+	sa := SavedAddress{
+		Address: common.Address{1},
+		Name:    "Zilliqa",
+		ChainID: 777,
+	}
+
+	err = manager.AddSavedAddress(sa)
+	require.NoError(t, err)
+
+	rst, err = manager.GetSavedAddresses(777)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(rst))
+	require.Equal(t, sa, *rst[0])
+
+	err = manager.DeleteSavedAddress(777, sa.Address)
+	require.NoError(t, err)
+
+	rst, err = manager.GetSavedAddresses(777)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(rst))
+}
