5512e19d83c9693bbb043a8aa6b3386164213c46
Anthony Laibe <491074+alaibe@users.noreply.github.com>
2022-01-03 10:07:33 +0100

    feat: ens API use hex return (#2471)

--- services/ens/contracts.go
+++ services/ens/contracts.go
@@ -13,6 +13,7 @@ var errorNotAvailableOnChainID = errors.New("not available for chainID")
 
 var resolversByChainID = map[uint64]common.Address{
 	1: common.HexToAddress("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"), // mainnet
+	3: common.HexToAddress("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"), // ropsten
 }
 
 var usernameRegistrarsByChainID = map[uint64]common.Address{
