3f9ef1da1ffcc364d225f084e4adc8112c5cea84
Richard Ramos <info@richardramos.me>
2022-03-04 14:31:34 -0400

    fix: vendor

--- services/updates/api.go
+++ services/updates/api.go
@@ -8,10 +8,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/hashicorp/go-version"
+	"go.uber.org/zap"
+
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/status-im/status-go/services/ens"
 	"github.com/status-im/status-go/signal"
-	"go.uber.org/zap"
 )
 
 func NewAPI(ensService *ens.Service) *API {
@@ -31,9 +33,16 @@ func (api *API) Check(ctx context.Context, chainID uint64, ens string, currentVe
 		ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
 		defer cancel()
 
+		current, err := version.NewVersion(currentVersion)
+		if err != nil {
+			log.Error("invalid current version", "err", err)
+			return
+		}
+
 		uri, err := api.ensService.API().ResourceURL(ctx, chainID, ens)
 		if err != nil || uri.Host == "" {
 			log.Error("can't get obtain the updates content hash url", "ens", ens)
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
@@ -42,18 +51,21 @@ func (api *API) Check(ctx context.Context, chainID uint64, ens string, currentVe
 		response, err := api.httpClient.Get(versionURL)
 		if err != nil {
 			log.Error("can't get content", zap.String("any", versionURL))
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
 		defer response.Body.Close()
 		if response.StatusCode != http.StatusOK {
 			log.Error(fmt.Sprintf("version verification response status error: %v", response.StatusCode))
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
 		data, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			log.Error("version verification body err", "err", err)
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
@@ -61,11 +73,7 @@ func (api *API) Check(ctx context.Context, chainID uint64, ens string, currentVe
 		err = json.Unmarshal(data, &c)
 		if err != nil {
 			log.Error("invalid json", "err", err)
-		}
-
-		current, err := version.NewVersion(currentVersion)
-		if err != nil {
-			log.Error("invalid current version", "err", err)
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
@@ -75,12 +83,14 @@ func (api *API) Check(ctx context.Context, chainID uint64, ens string, currentVe
 			latestStr = c["version"].(string)
 		default:
 			log.Error("invalid latest version", "val", c["version"])
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
 		latest, err := version.NewVersion(latestStr)
 		if err != nil {
 			log.Error("invalid latest version", "err", err)
+			signal.SendUpdateAvailable(false, "", "")
 			return
 		}
 
