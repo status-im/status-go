c3dbe89fb9bda1a15181da82a045a9c1aebe36ac
andrey <motor4ik@gmail.com>
2022-03-30 15:57:01 +0200

    BuyPrepareTx

--- services/stickers/transactions.go
+++ services/stickers/transactions.go
@@ -2,6 +2,7 @@ package stickers
 
 import (
 	"context"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"math/big"
 	"strings"
 
@@ -84,63 +85,99 @@ func (api *API) Buy(ctx context.Context, chainID uint64, txArgs transactions.Sen
 	return tx.Hash().String(), nil
 }
 
-func (api *API) BuyEstimate(ctx context.Context, chainID uint64, from types.Address, packID *bigint.BigInt) (uint64, error) {
+func (api *API) BuyPrepareTxCallMsg(chainID uint64, from types.Address, packID *bigint.BigInt) (ethereum.CallMsg, error) {
 	callOpts := &bind.CallOpts{Context: api.ctx, Pending: false}
 
 	stickerType, err := api.contractMaker.NewStickerType(chainID)
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
 	packInfo, err := stickerType.GetPackData(callOpts, packID.Int)
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
 	stickerMarketABI, err := abi.JSON(strings.NewReader(stickers.StickerMarketABI))
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
 	extraData, err := stickerMarketABI.Pack("buyToken", packID.Int, from, packInfo.Price)
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
 	sntABI, err := abi.JSON(strings.NewReader(snt.SNTABI))
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
 	stickerMarketAddress, err := stickers.StickerMarketContractAddress(chainID)
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
 	data, err := sntABI.Pack("approveAndCall", stickerMarketAddress, packInfo.Price, extraData)
 	if err != nil {
-		return 0, err
+		return ethereum.CallMsg{}, err
 	}
 
+	sntAddress, err := snt.ContractAddress(chainID)
+	if err != nil {
+		return ethereum.CallMsg{}, err
+	}
+
+	return ethereum.CallMsg{
+		From:  common.Address(from),
+		To:    &sntAddress,
+		Value: big.NewInt(0),
+		Data:  data,
+	}, nil
+}
+
+func (api *API) BuyPrepareTx(ctx context.Context, chainID uint64, from types.Address, packID *bigint.BigInt) (interface{}, error) {
+	callMsg, err := api.BuyPrepareTxCallMsg(chainID, from, packID)
+	if err != nil {
+		return nil, err
+	}
+
+	return toCallArg(callMsg), nil
+}
+
+func (api *API) BuyEstimate(ctx context.Context, chainID uint64, from types.Address, packID *bigint.BigInt) (uint64, error) {
+	callMsg, err := api.BuyPrepareTxCallMsg(chainID, from, packID)
+	if err != nil {
+		return 0, err
+	}
 	ethClient, err := api.contractMaker.RPCClient.EthClient(chainID)
 	if err != nil {
 		return 0, err
 	}
 
-	sntAddress, err := snt.ContractAddress(chainID)
-	if err != nil {
-		return 0, err
-	}
-
-	return ethClient.EstimateGas(ctx, ethereum.CallMsg{
-		From:  common.Address(from),
-		To:    &sntAddress,
-		Value: big.NewInt(0),
-		Data:  data,
-	})
-
+	return ethClient.EstimateGas(ctx, callMsg)
 }
 
 func (api *API) StickerMarketAddress(ctx context.Context, chainID uint64) (common.Address, error) {
 	return stickers.StickerMarketContractAddress(chainID)
+}
+
+func toCallArg(msg ethereum.CallMsg) interface{} {
+	arg := map[string]interface{}{
+		"from": msg.From,
+		"to":   msg.To,
+	}
+	if len(msg.Data) > 0 {
+		arg["data"] = hexutil.Bytes(msg.Data)
+	}
+	if msg.Value != nil {
+		arg["value"] = (*hexutil.Big)(msg.Value)
+	}
+	if msg.Gas != 0 {
+		arg["gas"] = hexutil.Uint64(msg.Gas)
+	}
+	if msg.GasPrice != nil {
+		arg["gasPrice"] = (*hexutil.Big)(msg.GasPrice)
+	}
+	return arg
 }
