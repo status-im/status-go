570034c864e95b1ff819384e99cff1e691f69e8a
siddarthkay <siddarthkay@gmail.com>
2022-07-12 17:25:44 +0530

    created migrations to store wallet connect session information

---
+++ appdatabase/migrations/sql/1657625611_wallet_connect_sessions.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS wallet_connect_sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    peer_id TEXT,
+    connector_info TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+);
