2f15730003cd4380582a593c30b528ae3e84c233
Richard Ramos <info@richardramos.me>
2022-09-05 10:12:11 -0400

    chore: change address of community portal testnet contract (#2843)

--- contracts/directory/address.go
+++ contracts/directory/address.go
@@ -9,7 +9,7 @@ import (
 var errorNotAvailableOnChainID = errors.New("not available for chainID")
 
 var contractAddressByChainID = map[uint64]common.Address{
-	69: common.HexToAddress("0x33534cc18D50ab082324A98eE69A7cCe47b75C49"), // optimism kovan testnet
+	69: common.HexToAddress("0x4BbCCa869E9931280Cb46AE0DfF18881Be581a4d"), // optimism kovan testnet
 }
 
 func ContractAddress(chainID uint64) (common.Address, error) {
