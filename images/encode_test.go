850d0d7727f02c774e85c90f76fc1e5a3e0465d3
Samuel Hawksby-Robinson <samuel@samyoul.com>
2022-09-09 13:41:21 +0100

    Added some preliminary work for aac compression

--- images/encode_test.go
+++ images/encode_test.go
@@ -2,9 +2,13 @@ package images
 
 import (
 	"bytes"
+	"compress/gzip"
 	"errors"
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 )
 
@@ -99,4 +103,56 @@ func TestCompressToFileLimits(t *testing.T) {
 	err = CompressToFileLimits(bb, img, FileSizeLimits{50000, 350000})
 	require.NoError(t, err)
 	require.Equal(t, 291645, bb.Len())
+}
+
+func Test(t *testing.T) {
+	// Test AAC file compression with gzip
+	f, err := ioutil.ReadFile(path + "sample3.aac")
+	require.NoError(t, err)
+	spew.Dump(len(f)) // (int) 1758426
+
+	var b bytes.Buffer
+	w := gzip.NewWriter(&b)
+	w.Write(f)
+	w.Close()
+	spew.Dump(b.Len()) // (int) 1733347
+}
+
+func TestWavToAAC(t *testing.T) {
+	file, err := os.Open(path + "BabyElephantWalk60.wav")
+	if err != nil {
+		panic(err)
+	}
+
+	wreader := wav.NewReader(file)
+	f, err := wreader.Format()
+	if err != nil {
+		panic(err)
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0))
+
+	opts := &aac.Options{}
+	opts.SampleRate = int(f.SampleRate)
+	opts.NumChannels = int(f.NumChannels)
+
+	enc, err := aac.NewEncoder(buf, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	err = enc.Encode(wreader)
+	if err != nil {
+		panic(err)
+	}
+
+	err = enc.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("test.aac", buf.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
 }
