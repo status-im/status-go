35a6135193569a4d95dba2cda16a856f4a90b2b5
Jonathan Rainville <rainville.jonathan@gmail.com>
2023-01-11 16:09:40 -0500

    feat(QuotedMessage): pass Deleted to QuotedMessage
    This is to let the front end determine a new message if the quoted message is deleted instead of just unknown

--- protocol/persistence_test.go
+++ protocol/persistence_test.go
@@ -555,9 +555,9 @@ func TestMessageReplies(t *testing.T) {
 	require.Equal(t, "", retrievedMessages[4].ResponseTo)
 	require.Nil(t, retrievedMessages[4].QuotedMessage)
 
-	// We have a ResponseTo, but no QuotedMessage, since the message was deleted
+	// We have a ResponseTo, but no QuotedMessage only gives the ID and Deleted
 	require.Equal(t, "id-4", retrievedMessages[0].ResponseTo)
-	require.Nil(t, retrievedMessages[0].QuotedMessage)
+	require.Equal(t, &common.QuotedMessage{ID: "id-4", Deleted: true}, retrievedMessages[0].QuotedMessage)
 }
 
 func TestMessageByChatIDWithTheSameClocks(t *testing.T) {
