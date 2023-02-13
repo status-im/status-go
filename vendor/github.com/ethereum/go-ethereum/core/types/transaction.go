51f99a26311f7f328a306817ab24a58ea23ff2ef
Anthony Laibe <anthony@laibe.cc>
2023-02-13 11:02:28 +0100

    chore: update go-ethereum

--- vendor/github.com/ethereum/go-ethereum/core/types/transaction.go
+++ vendor/github.com/ethereum/go-ethereum/core/types/transaction.go
@@ -52,6 +52,7 @@ const (
 	ArbitrumSubmitRetryableTxType = 105
 	ArbitrumInternalTxType        = 106
 	ArbitrumLegacyTxType          = 120
+	OptimismDepositTxType         = 126
 )
 
 // Transaction is an Ethereum transaction.
