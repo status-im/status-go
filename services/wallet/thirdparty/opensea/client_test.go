a3f1a84d291749d32408f340d331877555cee5c1
Khushboo-dev-cpp <60327365+Khushboo-dev-cpp@users.noreply.github.com>
2023-03-20 16:53:39 +0100

    chore: Improve error logging for OpenSea errors / generic json unmarshalling errors (#3298)
    fixes #8798

--- services/wallet/thirdparty/opensea/client_test.go
+++ services/wallet/thirdparty/opensea/client_test.go
@@ -2,6 +2,7 @@ package opensea
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/big"
 	"net/http"
 	"net/http/httptest"
@@ -12,6 +13,11 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	ExpiredKeyError         = "Expired API key"
+	ExpectedExpiredKeyError = "invalid json: Expired API key"
 )
 
 func TestFetchAllCollectionsByOwner(t *testing.T) {
@@ -40,6 +46,25 @@ func TestFetchAllCollectionsByOwner(t *testing.T) {
 	res, err := opensea.FetchAllCollectionsByOwner(common.Address{1})
 	assert.Equal(t, expected, res)
 	assert.Nil(t, err)
+}
+
+func TestFetchAllCollectionsByOwnerWithInValidJson(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte(ExpiredKeyError))
+		if err != nil {
+			return
+		}
+	}))
+	defer srv.Close()
+
+	opensea := &Client{
+		client: srv.Client(),
+		url:    srv.URL,
+	}
+	res, err := opensea.FetchAllCollectionsByOwner(common.Address{1})
+	assert.Nil(t, res)
+	assert.Equal(t, err, fmt.Errorf(ExpectedExpiredKeyError))
 }
 
 func TestFetchAllAssetsByOwnerAndCollection(t *testing.T) {
@@ -74,4 +99,23 @@ func TestFetchAllAssetsByOwnerAndCollection(t *testing.T) {
 	res, err := opensea.FetchAllAssetsByOwnerAndCollection(common.Address{1}, "rocky", "", 200)
 	assert.Nil(t, err)
 	assert.Equal(t, expected, *res)
+}
+
+func TestFetchAllAssetsByOwnerAndCollectionInvalidJson(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte(ExpiredKeyError))
+		if err != nil {
+			return
+		}
+	}))
+	defer srv.Close()
+
+	opensea := &Client{
+		client: srv.Client(),
+		url:    srv.URL,
+	}
+	res, err := opensea.FetchAllAssetsByOwnerAndCollection(common.Address{1}, "rocky", "", 200)
+	assert.Nil(t, res)
+	assert.Equal(t, fmt.Errorf(ExpectedExpiredKeyError), err)
 }
