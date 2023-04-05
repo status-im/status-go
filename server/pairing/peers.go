2f5b23e94a099052898c602085e7ed5724b0fc28
Samuel Hawksby-Robinson <samuel@samyoul.com>
2023-04-05 09:39:34 +0100

    Refined local pairing peer discovery architecture

--- server/pairing/peers.go
+++ server/pairing/peers.go
@@ -1,13 +1,101 @@
 package pairing
 
 import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
 
-	"github.com/schollz/peerdiscovery"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/golang/protobuf/proto"
+	udpp2p "github.com/schollz/peerdiscovery"
+
+	"github.com/status-im/status-go/protocol/protobuf"
+	"github.com/status-im/status-go/server"
 )
 
+var (
+	pk = []byte{0xbf, 0x3b, 0x37, 0x04, 0x30, 0x04, 0x32, 0x15, 0x72, 0xb0, 0x7f, 0x56, 0x72, 0x30, 0xae, 0x5b, 0x41, 0xf4, 0x4b, 0x42, 0x4a, 0xa2, 0x33, 0x53, 0x76, 0xed, 0x7a, 0xb9, 0x2d, 0x40, 0x37, 0x73}
+	k  = &ecdsa.PrivateKey{}
+)
+
+func init() {
+	k = server.ToECDSA(pk)
+}
+
+func sign(h *protobuf.LocalPairingPeerHello, k *ecdsa.PrivateKey) error {
+	dHash := sha256.Sum256(append(h.PeerId, []byte(h.DeviceName+h.DeviceType)...))
+	s, err := ecdsa.SignASN1(rand.Reader, k, dHash[:])
+	if err != nil {
+		return err
+	}
+
+	h.Signature = s
+	return nil
+}
+
+func verify(h *protobuf.LocalPairingPeerHello, k *ecdsa.PrivateKey) bool {
+	dHash := sha256.Sum256(append(h.PeerId, []byte(h.DeviceName+h.DeviceType)...))
+	return ecdsa.VerifyASN1(&k.PublicKey, dHash[:], h.Signature)
+}
+
+func makeDevicePayload(name, deviceType string, k *ecdsa.PrivateKey) (*protobuf.LocalPairingPeerHello, error) {
+	h := new(protobuf.LocalPairingPeerHello)
+
+	randId := make([]byte, 32)
+	_, err := rand.Read(randId)
+	if err != nil {
+		return nil, err
+	}
+
+	h.PeerId = randId
+	h.DeviceName = name
+	h.DeviceType = deviceType
+
+	err = sign(h, k)
+	if err != nil {
+		return nil, err
+	}
+
+	return h, nil
+}
+
+type UDPNotifier struct {
+	notifyOutput func(*protobuf.LocalPairingPeerHello)
+}
+
+func (u *UDPNotifier) notify(d udpp2p.Discovered) {
+	h := new(protobuf.LocalPairingPeerHello)
+	err := proto.Unmarshal(d.Payload, h)
+	if err != nil {
+		// TODO add logging rather than dumping
+		spew.Dump(err)
+	}
+
+	ok := verify(h, k)
+	if !ok {
+		return
+	}
+
+	u.notifyOutput(h)
+}
+
+func (u *UDPNotifier) makeUDPP2PSettings(h *protobuf.LocalPairingPeerHello) (*udpp2p.Settings, error) {
+	mh, err := proto.Marshal(h)
+	if err != nil {
+		return nil, err
+	}
+
+	return &udpp2p.Settings{
+		Limit:     4,
+		AllowSelf: true,
+		Notify:    u.notify,
+		Payload:   mh,
+	}, nil
+}
+
 func Search() {
-	discoveries, _ := peerdiscovery.Discover(peerdiscovery.Settings{Limit: 1, AllowSelf: true})
+	discoveries, _ := udpp2p.Discover(udpp2p.Settings{Limit: 1, AllowSelf: true})
 	for _, d := range discoveries {
 		fmt.Printf("discovered '%s'\n", d.Address)
 	}
--- server/pairing/peers_test.go
+++ server/pairing/peers_test.go
@@ -1,126 +1,69 @@
 package pairing
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/sha256"
 	"fmt"
-	"github.com/golang/protobuf/proto"
-	"github.com/status-im/status-go/protocol/protobuf"
-	"github.com/status-im/status-go/server"
-	"math/big"
+	"runtime"
 	"sync"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/schollz/peerdiscovery"
+	udpp2p "github.com/schollz/peerdiscovery"
+
+	"github.com/status-im/status-go/protocol/protobuf"
+	"github.com/status-im/status-go/server"
 )
 
-var (
-	pk = []byte{0xbf, 0x3b, 0x37, 0x04, 0x30, 0x04, 0x32, 0x15, 0x72, 0xb0, 0x7f, 0x56, 0x72, 0x30, 0xae, 0x5b, 0x41, 0xf4, 0x4b, 0x42, 0x4a, 0xa2, 0x33, 0x53, 0x76, 0xed, 0x7a, 0xb9, 0x2d, 0x40, 0x37, 0x73}
-	k  = &ecdsa.PrivateKey{}
-)
-
-func init() {
-	k = buildECKey()
-}
-
-func notify(d peerdiscovery.Discovered) {
-	h := new(protobuf.LocalPairingPeerHello)
-	err := proto.Unmarshal(d.Payload, h)
-	if err != nil {
-		spew.Dump(err)
-	}
-
-	fmt.Printf("payload - %+v\n", h)
-
-	dHash := sha256.Sum256([]byte(h.Name))
-	ok := ecdsa.VerifyASN1(&k.PublicKey, dHash[:], h.Signature)
-	spew.Dump("verified", ok)
-}
-
-func buildECKey() *ecdsa.PrivateKey {
-	k := big.NewInt(0).SetBytes(pk)
-
-	priv := new(ecdsa.PrivateKey)
-	priv.PublicKey.Curve = elliptic.P256()
-	priv.D = k
-	priv.PublicKey.X, priv.PublicKey.Y = elliptic.P256().ScalarBaseMult(k.Bytes())
-	return priv
-}
-
-func makeDevicePayload(name string, k *ecdsa.PrivateKey) (*protobuf.LocalPairingPeerHello, error) {
-	d := new(protobuf.LocalPairingPeerHello)
-	dHash := sha256.Sum256([]byte(name))
-
-	s, err := ecdsa.SignASN1(rand.Reader, k, dHash[:])
-	if err != nil {
-		return nil, err
-	}
-
-	d.Name = name
-	d.Signature = s
-
-	return d, nil
+func testSignal(h *protobuf.LocalPairingPeerHello) {
+	fmt.Printf("peer message - %s : %s\n", h.DeviceName, h.DeviceType)
 }
 
 func Test(t *testing.T) {
 	n, err := server.GetDeviceName()
 	if err != nil {
-		spew.Dump(err)
+		t.Error(err)
 	}
 
-	d1, err := makeDevicePayload(n+" - device 1", k)
+	d1, err := makeDevicePayload(n+" - device 1", runtime.GOOS, k)
 	if err != nil {
-		spew.Dump(err)
+		t.Error(err)
 	}
 
-	d2, err := makeDevicePayload(n+" - device 2", k)
+	d2, err := makeDevicePayload(n+" - device 2", runtime.GOOS, k)
 	if err != nil {
-		spew.Dump(err)
+		t.Error(err)
 	}
 
-	md1, err := proto.Marshal(d1)
-	if err != nil {
-		spew.Dump(err)
-	}
-
-	md2, err := proto.Marshal(d2)
-	if err != nil {
-		spew.Dump(err)
-	}
+	u := new(UDPNotifier)
+	u.notifyOutput = testSignal
 
 	wg := sync.WaitGroup{}
 
 	wg.Add(1)
 	go func() {
-		_, err := peerdiscovery.Discover(peerdiscovery.Settings{
-			Limit:     4,
-			AllowSelf: true,
-			Notify:    notify,
-			Payload:   md1,
-		})
+		s, err := u.makeUDPP2PSettings(d1)
 		if err != nil {
-			spew.Dump("error 1", err)
+			t.Error("1 -", err)
+		}
+
+		_, err = udpp2p.Discover(*s)
+		if err != nil {
+			t.Error("1 -", err)
 		}
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		_, err := peerdiscovery.Discover(peerdiscovery.Settings{
-			Limit:     4,
-			AllowSelf: true,
-			Notify:    notify,
-			Payload:   md2,
-		})
+		s, err := u.makeUDPP2PSettings(d2)
 		if err != nil {
-			spew.Dump("error 2", err)
+			t.Error("2 -", err)
+		}
+
+		_, err = udpp2p.Discover(*s)
+		if err != nil {
+			t.Error("2 -", err)
 		}
 		wg.Done()
 	}()
 
 	wg.Wait()
-	spew.Dump("done")
 }
