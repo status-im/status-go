a8eb0983783e9958fdd55aee1efaf3043abaa8fe
Michal Iskierko <michaliskierko@gmail.com>
2023-05-05 13:03:06 +0200

    feat(RemoteBurn): Add remote burn collectibles functionality
    * Updating smart contract (new burn function)
    * remoteBurn() and estimateRemoteBurn() functions

--- services/wallet/transfer/transaction.go
+++ services/wallet/transfer/transaction.go
@@ -71,13 +71,14 @@ type MultiTransactionResult struct {
 type PendingTrxType string
 
 const (
-	RegisterENS           PendingTrxType = "RegisterENS"
-	ReleaseENS            PendingTrxType = "ReleaseENS"
-	SetPubKey             PendingTrxType = "SetPubKey"
-	BuyStickerPack        PendingTrxType = "BuyStickerPack"
-	WalletTransfer        PendingTrxType = "WalletTransfer"
-	CollectibleDeployment PendingTrxType = "CollectibleDeployment"
-	CollectibleAirdrop    PendingTrxType = "CollectibleAirdrop"
+	RegisterENS                   PendingTrxType = "RegisterENS"
+	ReleaseENS                    PendingTrxType = "ReleaseENS"
+	SetPubKey                     PendingTrxType = "SetPubKey"
+	BuyStickerPack                PendingTrxType = "BuyStickerPack"
+	WalletTransfer                PendingTrxType = "WalletTransfer"
+	CollectibleDeployment         PendingTrxType = "CollectibleDeployment"
+	CollectibleAirdrop            PendingTrxType = "CollectibleAirdrop"
+	CollectibleRemoteSelfDestruct PendingTrxType = "CollectibleRemoteSelfDestruct"
 )
 
 type PendingTransaction struct {
