c6ed592a261dd21c1edfc8cfe008ead0ff1e2747
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-06-19 12:05:14 +0530

    chore: install deps script error made more user friendly for a system without package manager

--- _assets/scripts/install_deps.sh
+++ _assets/scripts/install_deps.sh
@@ -10,5 +10,12 @@ elif [[ -x $(command -v nix-env) ]]; then
   nix-env -iA nixos.protobuf3_17
 else
   echo "ERROR: No known package manager found!" >&2
+  case "$OSTYPE" in
+  darwin*)  echo "OSX detected: Install either brew or nix package manager and rerun the script" ;; 
+  linux*)   echo "LINUX detected: install either pacman or nix and rerun the script" ;;
+  bsd*)     echo "BSD detected: Install either pacman or nix package manager and rerun the script" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
   exit 1
 fi
