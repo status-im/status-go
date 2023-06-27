fc353d5dff90c5927000fa77bc7ec9ded9107f82
Ibrahem Khalil <vampirekid017@gmail.com>
2023-06-27 18:19:21 +0300

    Only send PNs to chats that are unmuted (#3673)

--- protocol/messenger.go
+++ protocol/messenger.go
@@ -5850,6 +5850,7 @@ func (m *Messenger) pushNotificationOptions() *pushnotificationclient.Registrati
 	m.allChats.Range(func(chatID string, chat *Chat) (shouldContinue bool) {
 		if chat.Muted {
 			mutedChatIDs = append(mutedChatIDs, chat.ID)
+			return true
 		}
 		if chat.Active && (chat.Public() || chat.CommunityChat()) {
 			publicChatIDs = append(publicChatIDs, chat.ID)
