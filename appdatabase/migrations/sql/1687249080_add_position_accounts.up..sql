259bc7453ffe86f227cb2f7fb80ed717e2c9455f
frank <lovefree103@gmail.com>
2023-06-28 20:53:05 +0800

    fix: ROW_NUMBER() not work on ios for some reason

--- appdatabase/migrations/sql/1687249080_add_position_accounts.up..sql
+++ appdatabase/migrations/sql/1687249080_add_position_accounts.up..sql
@@ -1,5 +1,76 @@
-ALTER TABLE keypairs_accounts ADD COLUMN position INT NOT NULL DEFAULT 0;
+CREATE TABLE keypairs_accounts_t1 (
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    address    VARCHAR,
+    key_uid    VARCHAR,
+    pubkey     VARCHAR,
+    path       VARCHAR  NOT NULL DEFAULT "",
+    name       VARCHAR  NOT NULL DEFAULT "",
+    color      VARCHAR  NOT NULL DEFAULT "",
+    emoji      VARCHAR  NOT NULL DEFAULT "",
+    wallet     BOOL     NOT NULL DEFAULT FALSE,
+    chat       BOOL     NOT NULL DEFAULT FALSE,
+    hidden     BOOL     NOT NULL DEFAULT FALSE,
+    operable   VARCHAR  NOT NULL DEFAULT "no",
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    clock      INT      NOT NULL DEFAULT 0
+);
 
-UPDATE keypairs_accounts AS ka SET position = ka2.rowNumber FROM (
-  SELECT created_at, ROW_NUMBER() OVER (ORDER BY created_at) AS rowNumber FROM keypairs_accounts) AS ka2
-WHERE ka.created_at = ka2.created_at;
+INSERT INTO keypairs_accounts_t1(address,key_uid,pubkey,path,name,color,emoji,wallet,chat,hidden,operable,created_at,updated_at,clock) SELECT
+   address,
+   key_uid,
+   pubkey,
+   path,
+   name,
+   color,
+   emoji,
+   wallet,
+   chat,
+   hidden,
+   operable,
+   created_at,
+   updated_at,
+   clock
+FROM keypairs_accounts ORDER BY created_at;
+
+CREATE TABLE keypairs_accounts_t2 (
+   address    VARCHAR PRIMARY KEY,
+   key_uid    VARCHAR,
+   pubkey     VARCHAR,
+   path       VARCHAR  NOT NULL DEFAULT "",
+   name       VARCHAR  NOT NULL DEFAULT "",
+   color      VARCHAR  NOT NULL DEFAULT "",
+   emoji      VARCHAR  NOT NULL DEFAULT "",
+   wallet     BOOL     NOT NULL DEFAULT FALSE,
+   chat       BOOL     NOT NULL DEFAULT FALSE,
+   hidden     BOOL     NOT NULL DEFAULT FALSE,
+   operable   VARCHAR  NOT NULL DEFAULT "no",
+   created_at DATETIME NOT NULL,
+   updated_at DATETIME NOT NULL,
+   clock      INT      NOT NULL DEFAULT 0,
+   position   INT      NOT NULL DEFAULT 0,
+   FOREIGN KEY (key_uid) REFERENCES keypairs (key_uid) ON DELETE CASCADE
+);
+
+INSERT INTO keypairs_accounts_t2 SELECT
+   address,
+   key_uid,
+   pubkey,
+   path,
+   name,
+   color,
+   emoji,
+   wallet,
+   chat,
+   hidden,
+   operable,
+   created_at,
+   updated_at,
+   clock,
+   id
+FROM keypairs_accounts_t1;
+
+DROP TABLE keypairs_accounts;
+DROP TABLE keypairs_accounts_t1;
+
+ALTER TABLE keypairs_accounts_t2 RENAME TO keypairs_accounts;
