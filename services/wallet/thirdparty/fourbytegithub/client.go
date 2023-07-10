bc92df79d551a9b211ab50de384475508079b060
Anthony Laibe <anthony@laibe.cc>
2023-07-10 11:35:11 +0200

    fix: fourbyte integration no arg

--- services/wallet/thirdparty/fourbytegithub/client.go
+++ services/wallet/thirdparty/fourbytegithub/client.go
@@ -69,29 +69,31 @@ func (c *Client) Run(data string) (*thirdparty.DataParsed, error) {
 	id := fmt.Sprintf("0x%s", methodSigData)
 	name := strings.Split(signature, "(")[0]
 	rs := rgx.FindStringSubmatch(signature)
-
-	inputs := make([]string, 0)
-	rawInputs := strings.Split(rs[1], ",")
-	for index, typ := range rawInputs {
-		if index == len(rawInputs)-1 && typ == "bytes" {
-			continue
-		}
-		inputs = append(inputs, fmt.Sprintf("{\"name\":\"%d\",\"type\":\"%s\"}", index, typ))
-	}
-
-	functionABI := fmt.Sprintf("[{\"constant\":true,\"inputs\":[%s],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\", \"name\": \"%s\"}], ", strings.Join(inputs, ","), name)
-	contractABI, err := abi.JSON(strings.NewReader(functionABI))
-	if err != nil {
-		return nil, err
-	}
-	method := contractABI.Methods[name]
-	inputsMap := make(map[string]interface{})
-	if err := method.Inputs.UnpackIntoMap(inputsMap, []byte(data[10:])); err != nil {
-		return nil, err
-	}
 	inputsMapString := make(map[string]string)
-	for key, value := range inputsMap {
-		inputsMapString[key] = fmt.Sprintf("%v", value)
+
+	if len(rs[1]) > 0 {
+		inputs := make([]string, 0)
+		rawInputs := strings.Split(rs[1], ",")
+		for index, typ := range rawInputs {
+			if index == len(rawInputs)-1 && typ == "bytes" {
+				continue
+			}
+			inputs = append(inputs, fmt.Sprintf("{\"name\":\"%d\",\"type\":\"%s\"}", index, typ))
+		}
+
+		functionABI := fmt.Sprintf("[{\"constant\":true,\"inputs\":[%s],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\", \"name\": \"%s\"}], ", strings.Join(inputs, ","), name)
+		contractABI, err := abi.JSON(strings.NewReader(functionABI))
+		if err != nil {
+			return nil, err
+		}
+		method := contractABI.Methods[name]
+		inputsMap := make(map[string]interface{})
+		if err := method.Inputs.UnpackIntoMap(inputsMap, []byte(data[10:])); err != nil {
+			return nil, err
+		}
+		for key, value := range inputsMap {
+			inputsMapString[key] = fmt.Sprintf("%v", value)
+		}
 	}
 
 	return &thirdparty.DataParsed{
