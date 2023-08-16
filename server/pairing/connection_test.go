1587283b11017d77a168a76d09951cc43e1be286
Igor Sirotin <sirotin@status.im>
2023-08-16 19:33:15 +0300

    one more real test

--- server/pairing/connection_test.go
+++ server/pairing/connection_test.go
@@ -1,6 +1,8 @@
 package pairing
 
 import (
+	"fmt"
+	"go.uber.org/zap"
 	"net"
 	"testing"
 
@@ -185,4 +187,133 @@ func (s *ConnectionParamsSuite) TestConnectionParams_FindReachableAddresses() {
 	localNets = []net.IPNet{}
 	ips = FindReachableAddresses(remoteIps, localNets)
 	s.Require().Len(ips, 0)
+}
+
+func (s *ConnectionParamsSuite) TestConnectionParams_RealNetworksTest() {
+
+	//  This test is intended to be run manually.
+	//  1. set `printDetails` to true
+	//  2. run Part 1 on 2 devices
+	//  3. copy printed results to Part 2
+	//  4. update expected results in Part 3
+	//  5. run Part 3
+
+	// printing is disabled by default to avoid showing sensitive information
+	const printDetails = false
+
+	printLocalAddresses := func(in [][]net.IP) {
+		fmt.Println("{")
+		for _, a := range in {
+			fmt.Println("    {")
+			for _, v := range a {
+				fmt.Println("        net.ParseIP(\"", v.String(), "\"),")
+
+			}
+			fmt.Println("    },")
+		}
+		fmt.Println("}")
+	}
+
+	printNets := func(in []net.IPNet) {
+		fmt.Println("{")
+		for _, n := range in {
+			fmt.Println("    \"", n.String(), "\",")
+		}
+		fmt.Println("}")
+	}
+
+	parseNets := func(in []string) []net.IPNet {
+		var out []net.IPNet
+		for _, v := range in {
+			_, network, err := net.ParseCIDR(v)
+			s.Require().NoError(err)
+			out = append(out, *network)
+		}
+		return out
+	}
+
+	//	Part 1:
+	//	print needed stuff. Run on both machines.
+
+	addrs, err := GetLocalAddresses()
+	s.Require().NoError(err)
+	s.Logger.Info("MacOS:", zap.Any("addrs", addrs))
+
+	if printDetails {
+		printLocalAddresses(addrs)
+	}
+
+	nets, err := GetAllAvailableNetworks()
+	s.Require().NoError(err)
+	s.Logger.Info("MacOS:", zap.Any("nets", nets))
+
+	if printDetails {
+		printNets(nets)
+	}
+
+	//	Part 2:
+	//	Input all printed devices details below
+
+	macNIs := [][]net.IP{
+		{
+			net.ParseIP("127.0.0.1"),
+			net.ParseIP("::1"),
+			net.ParseIP("fe80::1"),
+		},
+		{
+			net.ParseIP("fe80::c1f:ee0d:1476:dd9a"),
+			net.ParseIP("192.168.1.36"),
+		},
+		{
+			net.ParseIP("172.16.9.1"),
+		},
+	}
+
+	macNets := parseNets([]string{
+		"127.0.0.1/8",
+		"::1/128",
+		"fe80::1/64",
+		"fe80::c1f:ee0d:1476:dd9a/64",
+		"192.168.1.36/24",
+		"172.16.9.1/24",
+	})
+
+	winNIs := [][]net.IP{
+		{
+			net.ParseIP("fe80::6fd7:5ce4:554f:165a"),
+			net.ParseIP("192.168.1.33"),
+		},
+		{
+			net.ParseIP("fe80::ffa5:98e1:285c:42eb"),
+			net.ParseIP("10.0.85.2"),
+		},
+		{
+			net.ParseIP("::1"),
+			net.ParseIP("127.0.0.1"),
+		},
+	}
+
+	winNets := parseNets([]string{
+		"fe80::6fd7:5ce4:554f:165a/64",
+		"192.168.1.33/24",
+		"fe80::ffa5:98e1:285c:42eb/64",
+		"10.0.85.2/32",
+		"::1/128",
+		"127.0.0.1/8",
+	})
+
+	//	Part 3:
+	//	The test itself
+
+	// Windows as server, Mac as client
+	winIPs := FilterAddressesForPairingServer(winNIs)
+	winReachableIps := FindReachableAddresses(winIPs, macNets)
+	s.Require().Len(winReachableIps, 1)
+	s.Require().Equal(winReachableIps[0].String(), "192.168.1.33")
+
+	// Windows as server, Mac as client
+	macIPs := FilterAddressesForPairingServer(macNIs)
+	macReachableIps := FindReachableAddresses(macIPs, winNets)
+	s.Require().Len(macReachableIps, 1)
+	s.Require().Equal(macReachableIps[0].String(), "192.168.1.36")
 }
