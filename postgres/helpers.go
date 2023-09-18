494c6707bab9ea71777768bc3713b1295566d5c6
Jakub Sokołowski <jakub@status.im>
2023-09-18 17:50:26 +0200

    ci: parametrize DB port for unit tests
    Otherwise we can't run tests in parallel on the same host. Also the
    container name has to be different depending on executor.
    
    Resolves errors like:
    ```
    docker: Error response from daemon: Conflict.
        The container name "/status-go-test-db" is already in use by container "123...".
        You have to remove (or rename) that container to be able to reuse that name.
    ```
    Resolves: https://github.com/status-im/status-go/issues/4040
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- postgres/helpers.go
+++ postgres/helpers.go
@@ -2,14 +2,27 @@ package postgres
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+
 	// Import postgres driver
 	_ "github.com/lib/pq"
 )
 
-const (
-	DefaultTestURI = "postgres://postgres@127.0.0.1:5432/postgres?sslmode=disable"
-	DropTableURI   = "postgres://postgres@127.0.0.1:5432/template1?sslmode=disable"
+var (
+	DefaultTestDBHost = GetEnvDefault("TEST_POSTGRES_HOST", "localhost")
+	DefaultTestDBPort = GetEnvDefault("TEST_POSTGRES_PORT", "5432")
+	DefaultTestURI    = fmt.Sprintf("postgres://postgres@%s:%s/postgres?sslmode=disable", DefaultTestDBHost, DefaultTestDBPort)
+	DropTableURI      = fmt.Sprintf("postgres://postgres@%s:%s/template1?sslmode=disable", DefaultTestDBHost, DefaultTestDBPort)
 )
+
+func GetEnvDefault(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
 
 func ResetDefaultTestPostgresDB() error {
 	db, err := sql.Open("postgres", DropTableURI)
