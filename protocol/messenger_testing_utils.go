3394c8133a3ae95b4dbfdebb6e9471947f9bd830
Patryk Osmaczko <osmaczkopatryk@gmail.com>
2023-10-20 16:23:02 +0200

    chore: add `WaitOnSignaledMessengerResponse` test utility

--- protocol/messenger_testing_utils.go
+++ protocol/messenger_testing_utils.go
@@ -3,6 +3,7 @@ package protocol
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 
@@ -31,6 +32,61 @@ func WaitOnMessengerResponse(m *Messenger, condition func(*MessengerResponse) bo
 		return err
 	})
 	return response, err
+}
+
+type MessengerSignalsHandlerMock struct {
+	MessengerSignalsHandler
+
+	responseChan chan *MessengerResponse
+}
+
+func (m *MessengerSignalsHandlerMock) MessengerResponse(response *MessengerResponse) {
+	// Non-blocking send
+	select {
+	case m.responseChan <- response:
+	default:
+	}
+}
+
+func (m *MessengerSignalsHandlerMock) MessageDelivered(chatID string, messageID string) {}
+
+func WaitOnSignaledMessengerResponse(m *Messenger, condition func(*MessengerResponse) bool, errorMessage string) (*MessengerResponse, error) {
+	interval := 500 * time.Millisecond
+	timeoutChan := time.After(10 * time.Second)
+
+	if m.config.messengerSignalsHandler != nil {
+		return nil, errors.New("messengerSignalsHandler already provided/mocked")
+	}
+
+	responseChan := make(chan *MessengerResponse, 1)
+	m.config.messengerSignalsHandler = &MessengerSignalsHandlerMock{
+		responseChan: responseChan,
+	}
+
+	defer func() {
+		m.config.messengerSignalsHandler = nil
+	}()
+
+	for {
+		_, err := m.RetrieveAll()
+		if err != nil {
+			return nil, err
+		}
+
+		select {
+		case r := <-responseChan:
+			if condition(r) {
+				return r, nil
+			}
+			return nil, errors.New(errorMessage)
+
+		case <-timeoutChan:
+			return nil, errors.New("timed out: " + errorMessage)
+
+		default: // No immediate response, rest & loop back to retrieve again
+			time.Sleep(interval)
+		}
+	}
 }
 
 func FindFirstByContentType(messages []*common.Message, contentType protobuf.ChatMessage_ContentType) *common.Message {
