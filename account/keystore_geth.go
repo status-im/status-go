6ab3f0d128910f727b9e62dddddf809d98ef94fb
Siddarth Kumar <siddarthkay@gmail.com>
2023-10-23 15:10:07 +0530

    check if we have write permissions
    - makeAccountManager indeed is the root cause of the crash. Now attempting to return an error instead of crashing when we do not have permissions.

--- account/keystore_geth.go
+++ account/keystore_geth.go
@@ -1,7 +1,9 @@
 package account
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
@@ -10,9 +12,36 @@ import (
 	"github.com/status-im/status-go/eth-node/types"
 )
 
+func hasWritePermission(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	// Check if the current user has write permissions
+	if info.Mode().Perm()&0200 == 0 {
+		return false
+	}
+
+	return true
+}
+
 // makeAccountManager creates ethereum accounts.Manager with single disk backend and lightweight kdf.
 // If keydir is empty new temporary directory with go-ethereum-keystore will be intialized.
 func makeAccountManager(keydir string) (manager *accounts.Manager, err error) {
+
+	parentDir := "/" // Assuming root as a default parent directory
+
+	if keydir != "" {
+		parentDir = filepath.Dir(keydir)
+	} else {
+		parentDir = os.TempDir()
+	}
+
+	if !hasWritePermission(parentDir) {
+		return nil, fmt.Errorf("no write permission on the directory -> %s", parentDir)
+	}
+
 	if keydir == "" {
 		// There is no datadir.
 		keydir, err = os.MkdirTemp("", "go-ethereum-keystore")
