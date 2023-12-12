03f92b4a9a62060402c3a2d5992873efe77af92d
Michal Iskierko <michaliskierko@gmail.com>
2023-12-12 14:33:09 +0100

    feat: Add community tokens deployer addresses for mainnet and Optimism
    Issue #4447

--- contracts/community-tokens/deployer/address.go
+++ contracts/community-tokens/deployer/address.go
@@ -10,7 +10,9 @@ var errorNotAvailableOnChainID = errors.New("deployer contract not available for
 
 // addresses can be found on https://github.com/status-im/communities-contracts#deployments
 var contractAddressByChainID = map[uint64]common.Address{
+	1:        common.HexToAddress("0xB3Ef5B0825D5f665bE14394eea41E684CE96A4c5"), // Mainnet
 	5:        common.HexToAddress("0x81f4951ff8859d305F47A4574B206cF64C0d2645"), // Goerli
+	10:       common.HexToAddress("0x31463D22750324C8721FF7751584EF62F2ff93b3"), // Optimism
 	420:      common.HexToAddress("0xfFa8A255D905c909379859eA45B959D090DDC2d4"), // Optimism Goerli
 	42161:    common.HexToAddress("0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b"), // Arbitrum
 	421613:   common.HexToAddress("0x7Ff554af5b6624db2135E4364F416d1D397f43e6"), // Arbitrum Goerli
