4ad44b6c8a4b1bdfd41efbfaea9ef2ac828e6183
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2024-01-19 03:48:12 +0530

    Added tests
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

--- api/default_networks_test.go
+++ api/default_networks_test.go
@@ -21,7 +21,7 @@ func TestBuildDefaultNetworks(t *testing.T) {
 
 	actualNetworks := BuildDefaultNetworks(request)
 
-	require.Len(t, actualNetworks, 6)
+	require.Len(t, actualNetworks, 8)
 
 	require.Equal(t, mainnetChainID, actualNetworks[0].ChainID)
 
@@ -33,25 +33,35 @@ func TestBuildDefaultNetworks(t *testing.T) {
 	require.True(t, strings.Contains(actualNetworks[1].RPCURL, poktToken))
 	require.True(t, strings.Contains(actualNetworks[1].FallbackURL, infuraToken))
 
-	require.Equal(t, optimismChainID, actualNetworks[2].ChainID)
+	require.Equal(t, sepoliaChainID, actualNetworks[2].ChainID)
 
 	require.True(t, strings.Contains(actualNetworks[2].RPCURL, poktToken))
 	require.True(t, strings.Contains(actualNetworks[2].FallbackURL, infuraToken))
 
-	require.Equal(t, optimismGoerliChainID, actualNetworks[3].ChainID)
+	require.Equal(t, optimismChainID, actualNetworks[3].ChainID)
 
-	require.True(t, strings.Contains(actualNetworks[3].RPCURL, infuraToken))
-	require.Equal(t, "", actualNetworks[3].FallbackURL)
+	require.True(t, strings.Contains(actualNetworks[3].RPCURL, poktToken))
+	require.True(t, strings.Contains(actualNetworks[3].FallbackURL, infuraToken))
 
-	require.Equal(t, arbitrumChainID, actualNetworks[4].ChainID)
+	require.Equal(t, optimismGoerliChainID, actualNetworks[4].ChainID)
 
-	require.True(t, strings.Contains(actualNetworks[4].RPCURL, poktToken))
-	require.True(t, strings.Contains(actualNetworks[4].FallbackURL, infuraToken))
+	require.True(t, strings.Contains(actualNetworks[4].RPCURL, infuraToken))
+	require.Equal(t, "", actualNetworks[4].FallbackURL)
 
-	require.Equal(t, arbitrumGoerliChainID, actualNetworks[5].ChainID)
+	require.Equal(t, arbitrumChainID, actualNetworks[5].ChainID)
 
-	require.True(t, strings.Contains(actualNetworks[5].RPCURL, infuraToken))
-	require.Equal(t, "", actualNetworks[5].FallbackURL)
+	require.True(t, strings.Contains(actualNetworks[5].RPCURL, poktToken))
+	require.True(t, strings.Contains(actualNetworks[5].FallbackURL, infuraToken))
+
+	require.Equal(t, arbitrumGoerliChainID, actualNetworks[6].ChainID)
+
+	require.True(t, strings.Contains(actualNetworks[6].RPCURL, infuraToken))
+	require.Equal(t, "", actualNetworks[6].FallbackURL)
+
+	require.Equal(t, arbitrumSepoliaChainID, actualNetworks[7].ChainID)
+
+	require.True(t, strings.Contains(actualNetworks[7].RPCURL, infuraToken))
+	require.Equal(t, "", actualNetworks[7].FallbackURL)
 }
 
 func TestBuildDefaultNetworksGanache(t *testing.T) {
@@ -64,7 +74,7 @@ func TestBuildDefaultNetworksGanache(t *testing.T) {
 
 	actualNetworks := BuildDefaultNetworks(request)
 
-	require.Len(t, actualNetworks, 6)
+	require.Len(t, actualNetworks, 8)
 
 	for _, n := range actualNetworks {
 		require.True(t, strings.Contains(n.RPCURL, ganacheURL))
