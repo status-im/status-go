76315ae977d3fa5b4700ae95c10ac3b30b879185
Richard Ramos <info@richardramos.me>
2024-01-31 11:02:49 -0400

    fix: full nodes should run filter and lightpush

--- params/config.go
+++ params/config.go
@@ -212,9 +212,6 @@ type WakuV2Config struct {
 	// StoreSeconds indicates the maximum number of seconds before a message is removed from the store
 	StoreSeconds int
 
-	// EnableFilterNode indicates if WakuFilter Full Node should be enabled
-	EnableFilterFullNode bool
-
 	// UseShardAsDefaultTopic indicates whether the default shard should be used instead of the default relay topic
 	UseShardAsDefaultTopic bool
 }
