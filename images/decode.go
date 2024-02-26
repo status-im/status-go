afb9f93a6d7b284c1155de0d04ca843689ae43f5
Ibrkhalil <vampirekid017@gmail.com>
2024-02-26 17:37:43 +0200

    Use svg.go

--- images/decode.go
+++ images/decode.go
@@ -11,14 +11,44 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"time"
-
-	svg "github.com/h2non/go-is-svg"
+	"unicode/utf8"
 
 	"golang.org/x/image/webp"
 
 	"github.com/ethereum/go-ethereum/log"
 )
+
+var (
+	htmlCommentRegex = regexp.MustCompile(`(?i)<!--([\\s\\S]*?)-->`)
+	svgRegex         = regexp.MustCompile(`(?i)^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*>\s*)?<svg[^>]*>[^*]*<\/svg>\s*$`)
+)
+
+// isBinary checks if the given buffer is a binary file.
+func isBinary(buf []byte) bool {
+	if len(buf) < 24 {
+		return false
+	}
+	for i := 0; i < 24; i++ {
+		charCode, _ := utf8.DecodeRuneInString(string(buf[i]))
+		if charCode == 65533 || charCode <= 8 {
+			return true
+		}
+	}
+	return false
+}
+
+// Is returns true if the given buffer is a valid SVG image.
+func Is(buf []byte) bool {
+	return !isBinary(buf) && svgRegex.Match(htmlCommentRegex.ReplaceAll(buf, []byte{}))
+}
+
+// IsSVG returns true if the given buffer is a valid SVG image.
+// Alias to: Is()
+func IsSVG(buf []byte) bool {
+	return Is(buf)
+}
 
 func Decode(fileName string) (image.Image, error) {
 	file, err := os.Open(fileName)
@@ -168,7 +198,7 @@ func IsIco(buf []byte) bool {
 }
 
 func IsSvg(buf []byte) bool {
-	return svg.Is(buf)
+	return IsSVG(buf)
 }
 
 func GetImageDimensions(imgBytes []byte) (int, int, error) {
