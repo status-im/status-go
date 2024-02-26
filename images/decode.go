a5b932ef76e7de84dc0625eaf8f4bee7ff7cf03c
Ibrkhalil <vampirekid017@gmail.com>
2024-02-26 19:45:21 +0200

    cleanup

--- images/decode.go
+++ images/decode.go
@@ -25,29 +25,19 @@ var (
 	svgRegex         = regexp.MustCompile(`(?i)^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*>\s*)?<svg[^>]*>[^*]*<\/svg>\s*$`)
 )
 
-// isBinary checks if the given buffer is a binary file.
-func isBinary(buf []byte) bool {
+// Is returns true if the given buffer is a valid SVG image.
+func IsSVG(buf []byte) bool {
+	var isBinary bool
 	if len(buf) < 24 {
-		return false
+		isBinary = false
 	}
 	for i := 0; i < 24; i++ {
 		charCode, _ := utf8.DecodeRuneInString(string(buf[i]))
 		if charCode == 65533 || charCode <= 8 {
-			return true
+			isBinary = true
 		}
 	}
-	return false
-}
-
-// Is returns true if the given buffer is a valid SVG image.
-func Is(buf []byte) bool {
-	return !isBinary(buf) && svgRegex.Match(htmlCommentRegex.ReplaceAll(buf, []byte{}))
-}
-
-// IsSVG returns true if the given buffer is a valid SVG image.
-// Alias to: Is()
-func IsSVG(buf []byte) bool {
-	return Is(buf)
+	return !isBinary && svgRegex.Match(htmlCommentRegex.ReplaceAll(buf, []byte{}))
 }
 
 func Decode(fileName string) (image.Image, error) {
