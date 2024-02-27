5d83a51c7240d138c529f6be95983b3e8a45bdea
Ibrkhalil <vampirekid017@gmail.com>
2024-02-27 19:07:02 +0200

    lint

--- protocol/common/message_linkpreview.go
+++ protocol/common/message_linkpreview.go
@@ -321,8 +321,8 @@ func (m *Message) ConvertLinkPreviewsToProto() ([]*protobuf.UnfurledLink, error)
 	return unfurledLinks, nil
 }
 
-func (m *Message) ConvertFromProtoToLinkPreviews(makeThumbnailMediaServerUrl func(msgID string, previewURL string) string,
-	makeFaviconMediaServerUrl func(msgID string, previewURL string) string) []LinkPreview {
+func (m *Message) ConvertFromProtoToLinkPreviews(makeThumbnailMediaServerURL func(msgID string, previewURL string) string,
+	makeFaviconMediaServerURL func(msgID string, previewURL string) string) []LinkPreview {
 	var links []*protobuf.UnfurledLink
 
 	if links = m.GetUnfurledLinks(); links == nil {
@@ -350,7 +350,7 @@ func (m *Message) ConvertFromProtoToLinkPreviews(makeThumbnailMediaServerUrl fun
 		}
 		mediaURL := ""
 		if len(link.ThumbnailPayload) > 0 {
-			mediaURL = makeThumbnailMediaServerUrl(m.ID, link.Url)
+			mediaURL = makeThumbnailMediaServerURL(m.ID, link.Url)
 		}
 		if link.GetThumbnailPayload() != nil {
 			lp.Thumbnail.Width = int(link.ThumbnailWidth)
@@ -359,7 +359,7 @@ func (m *Message) ConvertFromProtoToLinkPreviews(makeThumbnailMediaServerUrl fun
 		}
 		faviconMediaURL := ""
 		if len(link.FaviconPayload) > 0 {
-			faviconMediaURL = makeFaviconMediaServerUrl(m.ID, link.Url)
+			faviconMediaURL = makeFaviconMediaServerURL(m.ID, link.Url)
 		}
 		if link.GetFaviconPayload() != nil {
 			lp.Favicon = faviconMediaURL
