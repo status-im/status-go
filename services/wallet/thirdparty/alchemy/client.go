fbd98326e0e27a2edd1dc865f0cd7dcab29ac4c3
Dario Gabriel Lipicar <dario@status.im>
2024-02-27 15:00:20 -0300

    fix: properly setup collectible providers exponential backoff

--- services/wallet/thirdparty/alchemy/client.go
+++ services/wallet/thirdparty/alchemy/client.go
@@ -129,14 +129,13 @@ func (o *Client) doPostWithJSON(ctx context.Context, url string, payload any) (*
 }
 
 func (o *Client) doWithRetries(req *http.Request) (*http.Response, error) {
-	b := backoff.ExponentialBackOff{
-		InitialInterval:     time.Millisecond * 1000,
-		RandomizationFactor: 0.1,
-		Multiplier:          1.5,
-		MaxInterval:         time.Second * 32,
-		MaxElapsedTime:      time.Second * 128,
-		Clock:               backoff.SystemClock,
-	}
+	b := backoff.NewExponentialBackOff()
+	b.InitialInterval = time.Millisecond * 1000
+	b.RandomizationFactor = 0.1
+	b.Multiplier = 1.5
+	b.MaxInterval = time.Second * 32
+	b.MaxElapsedTime = time.Second * 70
+
 	b.Reset()
 
 	op := func() (*http.Response, error) {
@@ -151,12 +150,13 @@ func (o *Client) doWithRetries(req *http.Request) (*http.Response, error) {
 
 		err = fmt.Errorf("unsuccessful request: %d %s", resp.StatusCode, http.StatusText(resp.StatusCode))
 		if resp.StatusCode == http.StatusTooManyRequests {
+			log.Error("doWithRetries failed with http.StatusTooManyRequests", "provider", o.ID(), "elapsed time", b.GetElapsedTime(), "next backoff", b.NextBackOff())
 			return nil, err
 		}
 		return nil, backoff.Permanent(err)
 	}
 
-	return backoff.RetryWithData(op, &b)
+	return backoff.RetryWithData(op, b)
 }
 
 func (o *Client) FetchCollectibleOwnersByContractAddress(ctx context.Context, chainID walletCommon.ChainID, contractAddress common.Address) (*thirdparty.CollectibleContractOwnership, error) {
