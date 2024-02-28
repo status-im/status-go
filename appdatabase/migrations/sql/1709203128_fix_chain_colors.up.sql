bade162fb6ddad2cbedc827f371bee28b2c29d60
Ivan Belyakov <ivan.belyakov.job@gmail.com>
2024-02-28 13:27:39 +0100

    fix: some chain colors in default networks config are wrong which were saved to DB, so fix the colors in DB as well.
    Closes https://github.com/status-im/status-desktop/issues/13762

---
+++ appdatabase/migrations/sql/1709203128_fix_chain_colors.up.sql
@@ -0,0 +1,3 @@
+UPDATE networks SET chain_color="#627EEA" WHERE chain_id IN (1, 5, 11155111);
+UPDATE networks SET chain_color="#E90101" WHERE chain_id IN (10, 420, 11155420);
+UPDATE networks SET chain_color="#51D0F0" WHERE chain_id IN (42161, 421613, 421614);
