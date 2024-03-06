091bd7bd21a982105414fdb4e28907762e990bff
Igor Sirotin <sirotin@status.im>
2024-03-06 21:09:21 +0000

    fix: right `prepareMessage` for empty album

--- protocol/persistence_quoted_message_test.go
+++ protocol/persistence_quoted_message_test.go
@@ -3,11 +3,13 @@ package protocol
 import (
 	"encoding/json"
 	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
 	"github.com/status-im/status-go/protocol/common"
 	"github.com/status-im/status-go/protocol/protobuf"
 	"github.com/status-im/status-go/server"
-	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 func TestMessengerPrepareMessage(t *testing.T) {
@@ -50,7 +52,7 @@ func (s *TestMessengerPrepareMessageSuite) generateTextMessage(ID string, From s
 	}
 }
 
-func (s *TestMessengerPrepareMessageSuite) Test_WHEN_MessageContainsImage_Then_preparedMessageAddsAlbumImageWithImageGeneratedLink() {
+func (s *TestMessengerPrepareMessageSuite) testMessageContainsImage(testAlbum bool) {
 	message1 := s.generateTextMessage("id-1", "1", 1, "")
 	message1.ContentType = protobuf.ChatMessage_IMAGE
 	message1.Payload = &protobuf.ChatMessage_Image{
@@ -63,7 +65,26 @@ func (s *TestMessengerPrepareMessageSuite) Test_WHEN_MessageContainsImage_Then_p
 	message2 := s.generateTextMessage("id-2", "2", 2, message1.ID)
 	messages := []*common.Message{message1, message2}
 
-	err := s.m.SaveMessages([]*common.Message{message1, message2})
+	var message3 *common.Message
+
+	if testAlbum {
+		albumID := RandomLettersString(5)
+		message1.GetImage().AlbumId = albumID
+
+		message3 = s.generateTextMessage("id-3", "1", 0, "")
+		message3.ContentType = protobuf.ChatMessage_IMAGE
+		message3.Payload = &protobuf.ChatMessage_Image{
+			Image: &protobuf.ImageMessage{
+				Format:  1,
+				Payload: RandomBytes(10),
+				AlbumId: albumID,
+			},
+		}
+
+		messages = append(messages, message3)
+	}
+
+	err := s.m.SaveMessages(messages)
 	s.Require().NoError(err)
 
 	err = s.p.SaveMessages(messages)
@@ -75,15 +96,55 @@ func (s *TestMessengerPrepareMessageSuite) Test_WHEN_MessageContainsImage_Then_p
 
 	retrievedMessages, _, err := s.p.MessageByChatID(s.chatID, "", 10)
 	s.Require().NoError(err)
+	if testAlbum {
+		s.Require().Len(retrievedMessages, 3)
+	} else {
+		s.Require().Len(retrievedMessages, 2)
+	}
 	s.Require().Equal(message2.ID, retrievedMessages[0].ID)
 	s.Require().Equal(message1.ID, retrievedMessages[0].ResponseTo)
 
 	err = s.m.prepareMessage(retrievedMessages[0], mediaServer)
 	s.Require().NoError(err)
 
-	expectedURL := fmt.Sprintf(`["https://Localhost:%d/messages/images?messageId=id-1"]`, mediaServer.GetPort())
+	mediaServerImageLink := func(messageID string) string {
+		return fmt.Sprintf(`https://Localhost:%d/messages/images?messageId=%s`,
+			mediaServer.GetPort(),
+			messageID)
+	}
 
-	s.Require().Equal(json.RawMessage(expectedURL), retrievedMessages[0].QuotedMessage.AlbumImages)
+	if testAlbum {
+		expectedJSON := fmt.Sprintf(`["%s","%s"]`,
+			mediaServerImageLink(message1.ID),
+			mediaServerImageLink(message3.ID),
+		)
+		s.Require().Equal(json.RawMessage(expectedJSON), retrievedMessages[0].QuotedMessage.AlbumImages)
+	} else {
+		expectedURL := mediaServerImageLink(message1.ID)
+		s.Require().Equal(expectedURL, retrievedMessages[0].QuotedMessage.ImageLocalURL)
+	}
+}
+
+func (s *TestMessengerPrepareMessageSuite) Test_WHEN_MessageContainsImage_THEN_preparedMessageAddsAlbumImageWithImageGeneratedLink() {
+	testCases := []struct {
+		name  string
+		album bool
+	}{
+		{
+			name:  "single image",
+			album: false,
+		},
+		{
+			name:  "album",
+			album: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			s.testMessageContainsImage(tc.album)
+		})
+	}
 }
 
 func (s *TestMessengerPrepareMessageSuite) Test_WHEN_NoQuotedMessage_THEN_RetrievedMessageDoesNotContainQuotedMessage() {
