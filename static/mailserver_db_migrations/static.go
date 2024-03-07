0058840aa36f587ecc44bc8c392f8455bc6c2532
Igor Sirotin <sirotin@status.im>
2024-03-07 23:32:25 +0000

    chore: go-bindata custom `-modtime`

--- static/mailserver_db_migrations/static.go
+++ static/mailserver_db_migrations/static.go
@@ -1,4 +1,4 @@
 // Package static embeds static (JS, HTML) resources right into the binaries
 package static
 
-//go:generate go-bindata -pkg migrations -o ../../mailserver/migrations/bindata.go .
+//go:generate go-bindata -modtime=1700000000 -pkg migrations -o ../../mailserver/migrations/bindata.go .
