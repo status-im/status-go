9c85c4770f64b6b9115a58bded879a42c5b8a89c
Igor Sirotin <sirotin@status.im>
2024-03-08 10:38:11 +0000

    add WaitForPeersConnected 100ms delay

--- protocol/messenger_testing_utils.go
+++ protocol/messenger_testing_utils.go
@@ -240,6 +240,8 @@ func WaitForPeersConnected(s *suite.Suite, waku *waku2.Waku, action func() []str
 		select {
 		case status := <-subscription.C:
 			if hasAllPeers(status.Peers, peerIDs) {
+				// Give some time for p2p events, otherwise might look lik peer is available, but fail to send a message.
+				<-time.After(100 * time.Millisecond)
 				return
 			}
 		case <-ctx.Done():
