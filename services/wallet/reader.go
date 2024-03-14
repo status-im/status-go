5203afd3b9b0354bae09c02defc21d229e5a3423
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2024-03-14 12:09:23 +0100

    _

--- services/wallet/reader.go
+++ services/wallet/reader.go
@@ -330,8 +330,7 @@ func (r *Reader) getWalletTokenBalances(ctx context.Context, addresses []common.
 	updateAnyway := false
 	if !updateBalances {
 		for _, address := range addresses {
-			if res, ok := cachedTokens[address]; !ok {
-				log.Info("NDBG", "res", res)
+			if res, ok := cachedTokens[address]; !ok || len(res) == 0 {
 				updateAnyway = true
 				break
 			}
