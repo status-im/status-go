34986ccd9ccb0b70f7dff3a7faa14e0c87f5db6a
Igor Sirotin <sirotin@status.im>
2024-05-06 12:47:14 +0100

    chore_: nix derivation with cc-test-reporter

---
+++ nix/pkgs/cc-test-reporter/default.nix
@@ -0,0 +1,31 @@
+{ lib, stdenv, fetchurl }:
+
+let
+  inherit (stdenv) isLinux isDarwin isWindows;
+
+  platform =
+    if isLinux then "linux" else
+    if isDarwin then "darwin" else
+    if isWindows then "windows" else
+    throw "Unsupported platform: ${stdenv.hostPlatform.system}";
+
+in stdenv.mkDerivation rec {
+  pname = "cc-test-reporter";
+  version = "0.11.1";
+
+  src = fetchurl {
+    url = "https://codeclimate.com/downloads/test-reporter/test-reporter-${version}-${platform}-amd64";
+    hash = lib.getAttr platform {
+      darwin = "sha256-uO9aRL3cJe+KCoC+uN6cBQy8xGQHim6h5Qzw36QO7EY=";
+      linux = "ne79mW3w9tHJ+3lAWzluuRp6yjWsy4lpdV/KpmjaTa0=";
+      windows = "8pn8csW9l5xMerZWAwIwWcrO7OLNWEM03yPEMMllaak=";
+    };
+   };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    install -D $src $out/bin/cc-test-reporter
+    chmod +x $out/bin/cc-test-reporter
+  '';
+}
