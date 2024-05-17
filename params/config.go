9f1c0a27b757ca97b6d3c0e3fee4860d0f9e982a
Jakub Sokołowski <jakub@status.im>
2024-05-17 15:54:35 +0200

    fix(config)_: KeycardPairingDataFile is not required
    Without this change `cmd/spiff-workflow` fails to start with:
    ```
    INFO [05-14|19:08:39.025|github.com/status-im/status-go/api/geth_backend.go:786]  failed to start node                     package=status-go/api.GethStatusBackend
    ERROR[05-14|17:00:04.279|spiff-workflow/main.go:125] failed import account                       package=status-go/cmd/statusd err="Key: 'NodeConfig.KeycardPairingDataFile' Error:Field validation for 'KeycardPairingDataFile' failed on the 'required' tag"
    ```
    The alternative is to set `KeycardPairingDataFile` to any value:
    ```
    	nodeConfig.KeycardPairingDataFile = "STUPID_BEYOND_BELIEF"
    ```
    Which is even worse.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- params/config.go
+++ params/config.go
@@ -327,7 +327,7 @@ type NodeConfig struct {
 
 	// KeycardPairingDataFile is the file where we keep keycard pairings data.
 	// note: this field won't be saved into db, it's local to the device.
-	KeycardPairingDataFile string `validate:"required"`
+	KeycardPairingDataFile string
 
 	// NodeKey is the hex-encoded node ID (private key). Should be a valid secp256k1 private key that will be used for both
 	// remote peer identification as well as network traffic encryption.
