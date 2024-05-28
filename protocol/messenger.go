523f5776c5c0d86d48e2f045920312e216d8ef55
John Ngei <johnmutuku628@gmail.com>
2024-05-28 15:40:26 +0200

    Revert "fix(login)_: Locally cache communities outside chat loop"
    This reverts commit 4f5f6647fb4857977376f2c57d7a9f8421ed04f8.

--- protocol/messenger.go
+++ protocol/messenger.go
@@ -1752,8 +1752,6 @@ func (m *Messenger) Init() error {
 	if err != nil {
 		return err
 	}
-
-	communityInfo := make(map[string]*communities.Community)
 	for _, chat := range chats {
 		if err := chat.Validate(); err != nil {
 			logger.Warn("failed to validate chat", zap.Error(err))
@@ -1770,13 +1768,20 @@ func (m *Messenger) Init() error {
 			continue
 		}
 
+		communityInfo := make(map[string]*communities.Community)
+
 		switch chat.ChatType {
 		case ChatTypePublic, ChatTypeProfile:
 			filtersToInit = append(filtersToInit, transport.FiltersToInitialize{ChatID: chat.ID})
 		case ChatTypeCommunityChat:
+			communityID, err := hexutil.Decode(chat.CommunityID)
+			if err != nil {
+				return err
+			}
+
 			community, ok := communityInfo[chat.CommunityID]
 			if !ok {
-				community, err = m.communitiesManager.GetByIDString(chat.CommunityID)
+				community, err = m.communitiesManager.GetByID(communityID)
 				if err != nil {
 					return err
 				}
