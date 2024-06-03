394d3340ae585783537b0a3cf7b402aa56d4c32e
frank <lovefree103@gmail.com>
2024-06-03 21:24:45 +0800

    fix_: failed test TestBackendStartNodeConcurrently

--- appdatabase/database.go
+++ appdatabase/database.go
@@ -89,6 +89,11 @@ func InitializeDB(path, password string, kdfIterationsNumber int) (*sql.DB, erro
 
 func fixMissingKeyUIDForAccounts(sqlTx *sql.Tx) error {
 	rows, err := sqlTx.Query(`SELECT address,pubkey FROM accounts WHERE pubkey IS NOT NULL AND type != '' AND type != 'generated'`)
+	if err == sql.ErrNoRows {
+		// we shouldn't reach here, but if we do, it probably happened from the test
+		log.Warn("Migrating accounts: no accounts found")
+		return nil
+	}
 	if err != nil {
 		log.Error("Migrating accounts: failed to query accounts", "err", err.Error())
 		return err
@@ -118,6 +123,11 @@ func fixMissingKeyUIDForAccounts(sqlTx *sql.Tx) error {
 
 	var walletRootAddress e_types.Address
 	err = sqlTx.QueryRow(`SELECT wallet_root_address FROM settings WHERE synthetic_id='id'`).Scan(&walletRootAddress)
+	if err == sql.ErrNoRows {
+		// we shouldn't reach here, but if we do, it probably happened from the test
+		log.Warn("Migrating accounts: no wallet_root_address found in settings")
+		return nil
+	}
 	if err != nil {
 		log.Error("Migrating accounts: failed to get wallet_root_address", "err", err.Error())
 		return err
