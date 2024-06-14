eb0f907137e50462a347382b584c7370618a98f6
richΛrd <info@richardramos.me>
2024-06-14 12:05:17 -0400

    fix_: use node context instead of context with timeout for discv5 (#5347)

--- wakuv2/waku.go
+++ wakuv2/waku.go
@@ -1841,7 +1841,7 @@ func (w *Waku) restartDiscV5() error {
 
 	if w.node.DiscV5().ErrOnNotRunning() != nil {
 		w.logger.Info("is not started restarting")
-		err := w.node.DiscV5().Start(ctx)
+		err := w.node.DiscV5().Start(w.ctx)
 		if err != nil {
 			w.logger.Error("Could not start DiscV5", zap.Error(err))
 		}
@@ -1855,7 +1855,7 @@ func (w *Waku) restartDiscV5() error {
 		default:
 		}
 
-		err := w.node.DiscV5().Start(ctx)
+		err := w.node.DiscV5().Start(w.ctx)
 		if err != nil {
 			w.logger.Error("Could not start DiscV5", zap.Error(err))
 		}
