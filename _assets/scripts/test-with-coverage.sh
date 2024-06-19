3f1756edb7116ced2c73d5a9020ffd3c3729dec6
Igor Sirotin <sirotin@status.im>
2024-06-19 16:58:06 +0100

    fix_: final coverage reports merging

--- _assets/scripts/test-with-coverage.sh
+++ _assets/scripts/test-with-coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-coverage_file_path="${PACKAGE_DIR}/$(mktemp coverage.out.rerun.XXXXXXXXXX)"
+coverage_file_path="$(mktemp coverage.out.rerun.XXXXXXXXXX --tmpdir="${PACKAGE_DIR}")"
 go test -json \
   -covermode=atomic \
   -coverprofile="${coverage_file_path}" \
