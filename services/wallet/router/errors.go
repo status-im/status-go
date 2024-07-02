59853fdbe2fbf8fb2ebaa51de7bca97104610f5e
Samuel Hawksby-Robinson <samuel@samyoul.com>
2024-07-02 11:25:30 +0100

    test(router_validation)_: integrated the new errors into test loigc

--- services/wallet/router/errors.go
+++ services/wallet/router/errors.go
@@ -1,47 +1,30 @@
 package router
 
 import (
-	"errors"
-
-	sErrors "github.com/status-im/status-go/errors"
+	"github.com/status-im/status-go/errors"
 )
 
 // Abbreviation `WR` for the error code stands for Wallet Router
 var (
-	ErrUsernameAndPubKeyRequiredForENSRegister   = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-001"), Details: "username and public key are required for ENSRegister"}
-	ErrOnlySTTSupportedForENSRegisterOnTestnet   = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-002"), Details: "only STT is supported for ENSRegister on testnet"}
-	ErrOnlySTTSupportedForENSReleaseOnTestnet    = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-003"), Details: "only STT is supported for ENSRelease on testnet"}
-	ErrUsernameRequiredForENSRelease             = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-004"), Details: "username is required for ENSRelease"}
-	ErrUsernameAndPubKeyRequiredForENSSetPubKey  = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-005"), Details: "username and public key are required for ENSSetPubKey"}
-	ErrPackIDRequiredForStickersBuy              = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-006"), Details: "packID is required for StickersBuy"}
-	ErrToTokenIDRequiredForSwap                  = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-007"), Details: "toTokenID is required for Swap"}
-	ErrTokenIDAndToTokenIDDifferent              = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-008"), Details: "tokenID and toTokenID must be different"}
-	ErrOnlyOneOfAmountInOrOutSet                 = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-009"), Details: "only one of amountIn or amountOut can be set"}
-	ErrAmountInMustBePositive                    = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-010"), Details: "amountIn must be positive"}
-	ErrAmountOutMustBePositive                   = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-011"), Details: "amountOut must be positive"}
-	ErrLockedAmountNotSupportedForNetwork        = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-012"), Details: "locked amount is not supported for the selected network"}
-	ErrLockedAmountMustBePositive                = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-013"), Details: "locked amount must be positive"}
-	ErrLockedAmountExceedsTotalSendAmount        = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-014"), Details: "locked amount exceeds the total amount to send"}
-	ErrLockedAmountLessThanSendAmountAllNetworks = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-015"), Details: "locked amount is less than the total amount to send, but all networks are locked"}
-	ErrNotEnoughTokenBalance                     = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-016"), Details: "not enough token balance"}
-	ErrNotEnoughNativeBalance                    = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-017"), Details: "not enough native balance"}
-	ErrNativeTokenNotFound                       = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-018"), Details: "native token not found"}
-	ErrDisabledChainFoundAmongLockedNetworks     = &sErrors.ErrorResponse{Code: sErrors.ErrorCode("WR-019"), Details: "disabled chain found among locked networks"}
-)
-
-var (
-	ErrorENSRegisterRequires                  = errors.New("username and public key are required for ENSRegister")
-	ErrorENSRegisterTestNetSTTOnly            = errors.New("only STT is supported for ENSRegister on testnet")
-	ErrorENSRegisterSNTOnly                   = errors.New("only SNT is supported for ENSRegister")
-	ErrorENSReleaseRequires                   = errors.New("username is required for ENSRelease")
-	ErrorENSSetPubKeyRequires                 = errors.New("username and public key are required for ENSSetPubKey")
-	ErrorStickersBuyRequires                  = errors.New("packID is required for StickersBuy")
-	ErrorSwapRequires                         = errors.New("toTokenID is required for Swap")
-	ErrorSwapTokenIDMustBeDifferent           = errors.New("tokenID and toTokenID must be different")
-	ErrorSwapAmountInAmountOutMustBeExclusive = errors.New("only one of amountIn or amountOut can be set")
-	ErrorSwapAmountInMustBePositive           = errors.New("amountIn must be positive")
-	ErrorSwapAmountOutMustBePositive          = errors.New("amountOut must be positive")
-	ErrorLockedAmountNotSupportedNetwork      = errors.New("locked amount is not supported for the selected network")
-	ErrorLockedAmountNotNegative              = errors.New("locked amount must not be negative")
-	ErrorLockedAmountExcludesAllSupported     = errors.New("all supported chains are excluded, routing impossible")
+	ErrENSRegisterRequiresUsernameAndPubKey      = &errors.ErrorResponse{Code: errors.ErrorCode("WR-001"), Details: "username and public key are required for ENSRegister"}
+	ErrENSRegisterTestnetSTTOnly                 = &errors.ErrorResponse{Code: errors.ErrorCode("WR-002"), Details: "only STT is supported for ENSRegister on testnet"}
+	ErrENSRegisterMainnetSNTOnly                 = &errors.ErrorResponse{Code: errors.ErrorCode("WR-003"), Details: "only SNT is supported for ENSRegister on mainnet"}
+	ErrENSReleaseRequiresUsername                = &errors.ErrorResponse{Code: errors.ErrorCode("WR-004"), Details: "username is required for ENSRelease"}
+	ErrENSSetPubKeyRequiresUsernameAndPubKey     = &errors.ErrorResponse{Code: errors.ErrorCode("WR-005"), Details: "username and public key are required for ENSSetPubKey"}
+	ErrStickersBuyRequiresPackID                 = &errors.ErrorResponse{Code: errors.ErrorCode("WR-006"), Details: "packID is required for StickersBuy"}
+	ErrSwapRequiresToTokenID                     = &errors.ErrorResponse{Code: errors.ErrorCode("WR-007"), Details: "toTokenID is required for Swap"}
+	ErrSwapTokenIDMustBeDifferent                = &errors.ErrorResponse{Code: errors.ErrorCode("WR-008"), Details: "tokenID and toTokenID must be different"}
+	ErrSwapAmountInAmountOutMustBeExclusive      = &errors.ErrorResponse{Code: errors.ErrorCode("WR-009"), Details: "only one of amountIn or amountOut can be set"}
+	ErrSwapAmountInMustBePositive                = &errors.ErrorResponse{Code: errors.ErrorCode("WR-010"), Details: "amountIn must be positive"}
+	ErrSwapAmountOutMustBePositive               = &errors.ErrorResponse{Code: errors.ErrorCode("WR-011"), Details: "amountOut must be positive"}
+	ErrLockedAmountNotSupportedForNetwork        = &errors.ErrorResponse{Code: errors.ErrorCode("WR-012"), Details: "locked amount is not supported for the selected network"}
+	ErrLockedAmountNotNegative                   = &errors.ErrorResponse{Code: errors.ErrorCode("WR-013"), Details: "locked amount must not be negative"}
+	ErrLockedAmountExceedsTotalSendAmount        = &errors.ErrorResponse{Code: errors.ErrorCode("WR-014"), Details: "locked amount exceeds the total amount to send"}
+	ErrLockedAmountLessThanSendAmountAllNetworks = &errors.ErrorResponse{Code: errors.ErrorCode("WR-015"), Details: "locked amount is less than the total amount to send, but all networks are locked"}
+	ErrNotEnoughTokenBalance                     = &errors.ErrorResponse{Code: errors.ErrorCode("WR-016"), Details: "not enough token balance"}
+	ErrNotEnoughNativeBalance                    = &errors.ErrorResponse{Code: errors.ErrorCode("WR-017"), Details: "not enough native balance"}
+	ErrNativeTokenNotFound                       = &errors.ErrorResponse{Code: errors.ErrorCode("WR-018"), Details: "native token not found"}
+	ErrDisabledChainFoundAmongLockedNetworks     = &errors.ErrorResponse{Code: errors.ErrorCode("WR-019"), Details: "disabled chain found among locked networks"}
+	ErrENSSetPubKeyInvalidUsername               = &errors.ErrorResponse{Code: errors.ErrorCode("WR-020"), Details: "a valid username, ending in '.eth', is required for ENSSetPubKey"}
+	ErrLockedAmountExcludesAllSupported          = &errors.ErrorResponse{Code: errors.ErrorCode("WR-021"), Details: "all supported chains are excluded, routing impossible"}
 )
