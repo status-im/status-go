194d9444a5b5fefa93cead554793daa84d5ae33e
Godfrain Jacques <gkounkou@gmail.com>
2024-07-12 11:12:14 -0700

    feat_: locally handled methods package base structure (#5418)

--- services/connector/api.go
+++ services/connector/api.go
@@ -1,15 +1,42 @@
 package connector
 
-func NewAPI(s *Service) *API {
-	return &API{
-		s: s,
-	}
-}
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type API struct {
 	s *Service
+	r *CommandRegistry
+}
+
+func NewAPI(s *Service) *API {
+	r := NewCommandRegistry()
+
+	return &API{
+		s: s,
+		r: r,
+	}
+}
+
+type RPCRequest struct {
+	JSONRPC string        `json:"jsonrpc"`
+	ID      int           `json:"id"`
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
 }
 
 func (api *API) CallRPC(inputJSON string) (string, error) {
+	var request RPCRequest
+
+	err := json.Unmarshal([]byte(inputJSON), &request)
+	if err != nil {
+		return "", fmt.Errorf("error unmarshalling JSON: %v", err)
+	}
+
+	if command, exists := api.r.GetCommand(request.Method); exists {
+		return command.Execute(inputJSON)
+	}
+
 	return api.s.rpcClient.CallRaw(inputJSON), nil
 }
