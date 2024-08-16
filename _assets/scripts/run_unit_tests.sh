cddff8bdb455b06879419e9231fb989348b3de31
Igor Sirotin <sirotin@status.im>
2024-08-16 22:38:00 +0100

    chore_: run tests in parallel (wip)

--- _assets/scripts/run_unit_tests.sh
+++ _assets/scripts/run_unit_tests.sh
@@ -83,7 +83,7 @@ run_test_for_package() {
   rm -f ${package_dir}/coverage.out.rerun.*
 
   PACKAGE_DIR=${package_dir} gotestsum --packages="${package}" ${gotestsum_flags} --raw-command -- \
-    ./_assets/scripts/test-with-coverage.sh \
+    ./_assets/scripts/test-with-coverage-old.sh \
     ${package} \
     -v ${GOTEST_EXTRAFLAGS} \
     -timeout "${package_timeout}" \
@@ -109,30 +109,87 @@ run_test_for_package() {
   return ${go_test_exit}
 }
 
+run_test_for_packages() {
+  local packages=$1
+  local iteration=$2
+
+  local output_file="test_${iteration}.log"
+  local coverage_file="test_${iteration}.coverage.out"
+
+  local report_file="report_${iteration}.xml"
+  local rerun_report_file="report_rerun_fails_${iteration}.txt"
+  local exit_code_file="exit_code_${iteration}.txt"
+
+  echo -e "${GRN}Testing:${RST} ${packages} Iteration:${iteration}"
+
+  gotestsum_flags="${GOTESTSUM_EXTRAFLAGS}"
+  if [[ "${CI}" == 'true' ]]; then
+    gotestsum_flags="${gotestsum_flags} --junitfile=${report_file} --rerun-fails-report=${rerun_report_file}"
+  fi
+
+  # Cleanup previous coverage reports
+  rm -f coverage.out.rerun.*
+
+  PACKAGES=${packages} \
+  gotestsum --packages="${packages}" ${gotestsum_flags} --raw-command -- \
+    ./_assets/scripts/test-with-coverage.sh \
+    -v ${GOTEST_EXTRAFLAGS} \
+    -timeout 45m \
+    -count 1 \
+    -tags "${BUILD_TAGS}" | \
+    redirect_stdout "${output_file}"
+
+  local go_test_exit=$?
+
+  # Merge package coverage results
+  go run ./cmd/test-coverage-utils/gocovmerge.go coverage.out.rerun.* > ${coverage_file}
+
+  # Cleanup coverage reports
+  rm -f coverage.out.rerun.*
+
+  echo "${go_test_exit}" > "${exit_code_file}"
+  if [[ "${go_test_exit}" -ne 0 ]]; then
+    if [[ "${CI}" == 'true' ]]; then
+      echo -e "${YLW}Failed, see the log:${RST} ${BLD}${output_file}${RST}"
+    fi
+  fi
+
+  return ${go_test_exit}
+}
+
 if [[ $UNIT_TEST_REPORT_CODECLIMATE == 'true' ]]; then
 	cc-test-reporter before-build
 fi
 
+echo -e "${GRN}UNIT_TEST_RUN_PARALLEL: ${RST}${UNIT_TEST_RUN_PARALLEL}"
 echo -e "${GRN}Testing HEAD:${RST} $(git rev-parse HEAD)"
 
 rm -rf ./**/*.coverage.out
 
-for package in ${UNIT_TEST_PACKAGES}; do
+if [[ $UNIT_TEST_RUN_PARALLEL == 'true' ]]; then
+  echo -e "${GRN}Running tests in parallel${RST}"
   for ((i=1; i<=UNIT_TEST_COUNT; i++)); do
-    if ! is_parallelizable "${package}" || [[ "$UNIT_TEST_FAILFAST" == 'true' ]]; then
-      run_test_for_package "${package}" "${i}"
-      go_test_exit=$?
-      if [[ "$UNIT_TEST_FAILFAST" == 'true' ]]; then
-        if [[ "${go_test_exit}" -ne 0 ]]; then
-          exit "${go_test_exit}"
-        fi
-      fi
-    else
-      run_test_for_package "${package}" "${i}" &
-    fi
+    run_test_for_packages "${UNIT_TEST_PACKAGES}" "${i}"
   done
-  wait # Wait for all background jobs to finish
-done
+else
+  echo -e "${GRN}Running tests sequentially${RST}"
+  for package in ${UNIT_TEST_PACKAGES}; do
+    for ((i=1; i<=UNIT_TEST_COUNT; i++)); do
+      if ! is_parallelizable "${package}" || [[ "$UNIT_TEST_FAILFAST" == 'true' ]]; then
+        run_test_for_package "${package}" "${i}"
+        go_test_exit=$?
+        if [[ "$UNIT_TEST_FAILFAST" == 'true' ]]; then
+          if [[ "${go_test_exit}" -ne 0 ]]; then
+            exit "${go_test_exit}"
+          fi
+        fi
+      else
+        run_test_for_package "${package}" "${i}" &
+      fi
+    done
+    wait # Wait for all background jobs to finish
+  done
+fi
 
 # Gather test coverage results
 rm -f c.out c-full.out
--- _assets/scripts/test-with-coverage.sh
+++ _assets/scripts/test-with-coverage.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -eu
-coverage_file_path="$(mktemp coverage.out.rerun.XXXXXXXXXX --tmpdir="${PACKAGE_DIR}")"
+
+packages=""
+coverage_file_path="$(mktemp coverage.out.rerun.XXXXXXXXXX)"
+
+# This is a hack to workaround gotestsum behaviour. When using a --raw-command,
+# gotestsum will only pass the package when rerunning a test. Otherwise we should pass the package ourselves.
+# https://github.com/gotestyourself/gotestsum/blob/03568ab6d48faabdb632013632ac42687b5f17d1/cmd/main.go#L331-L336
+if [[ "$*" != *"-test.run"* ]]; then
+  packages="${PACKAGES}"
+fi
+
 go test -json \
+  ${packages} \
   -covermode=atomic \
   -coverprofile="${coverage_file_path}" \
   -coverpkg ./... \
