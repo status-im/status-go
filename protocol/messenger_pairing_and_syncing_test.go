59f16339c12cdea04e6cfffca69658cf7380a5b3
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2024-08-27 09:45:03 +0100

    chore_: move sync and pair test to protocol

--- protocol/messenger_pairing_and_syncing_test.go
+++ protocol/messenger_pairing_and_syncing_test.go
@@ -1,13 +1,17 @@
 package protocol
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap"
 
+	"github.com/status-im/status-go/multiaccounts/accounts"
 	"github.com/status-im/status-go/protocol/encryption/multidevice"
 	"github.com/status-im/status-go/protocol/requests"
+	"github.com/status-im/status-go/protocol/tt"
 )
 
 func TestMessengerPairingTest(t *testing.T) {
@@ -85,4 +89,80 @@ func (s *MessengerPairingSuite) TestMessengerPairAfterSeedPhrase() {
 
 	s.Require().NoError(err)
 
+}
+
+func (s *MessengerPairingSuite) TestMessengerPairEnableAndSync() {
+	// assuming alice2 want to sync with alice1
+	// alice1 generated the connection string for bootstraping alice2
+	// alice2 failed to connect to alice1 and restored from seed phrase
+	// alice2 get the installationID1 from alice1 via parsing the connection string
+	alice1 := s.m
+	alice2logger := s.logger.With(zap.Namespace("alice2"))
+	alice2, err := newMessengerWithKey(s.shh, s.privateKey, alice2logger, nil)
+	s.Require().NoError(err)
+	defer TearDownMessenger(&s.Suite, alice2)
+	installationID1 := alice1.installationID
+	installationID2 := alice2.installationID
+	s.Require().NotEqual(installationID1, installationID2)
+	_, err = alice2.EnableInstallationAndPair(&requests.EnableInstallationAndPair{InstallationID: installationID1})
+	s.Require().NoError(err)
+
+	profileKp := accounts.GetProfileKeypairForTest(true, false, false)
+	profileKp.KeyUID = alice2.account.KeyUID
+	profileKp.Accounts[0].KeyUID = alice2.account.KeyUID
+
+	err = alice2.settings.SaveOrUpdateKeypair(profileKp)
+	s.Require().NoError(err)
+
+	profileKp = accounts.GetProfileKeypairForTest(true, false, false)
+	profileKp.KeyUID = alice1.account.KeyUID
+	profileKp.Accounts[0].KeyUID = alice1.account.KeyUID
+
+	err = alice1.settings.SaveOrUpdateKeypair(profileKp)
+	s.Require().NoError(err)
+
+	// alice1 should get the installationID1 from alice2
+	_, err = WaitOnMessengerResponse(
+		alice1,
+		func(r *MessengerResponse) bool {
+			for _, i := range r.Installations() {
+				if i.ID == installationID2 {
+					return true
+				}
+			}
+			return false
+		},
+		"no messages",
+	)
+
+	s.Require().NoError(err)
+
+	displayName := "new-display-name"
+	err = alice1.SetDisplayName(displayName)
+	s.Require().NoError(err)
+
+	// sync data from acc to acc2
+	err = alice1.EnableAndSyncInstallation(&requests.EnableAndSyncInstallation{InstallationID: alice2.InstallationID()})
+	s.Require().NoError(err)
+	installations := alice1.Installations()
+	s.Require().Len(installations, 2)
+
+	if installations[0].ID == alice2.InstallationID() {
+		s.Require().True(installations[0].Enabled)
+	} else {
+		s.Require().Equal(alice2.InstallationID(), installations[1].ID)
+		s.Require().True(installations[1].Enabled)
+	}
+	// ensure acc2's display name get synced
+	err = tt.RetryWithBackOff(func() error {
+		r, err := alice2.RetrieveAll()
+		s.Require().NoError(err)
+		for _, ss := range r.Settings {
+			if ss.GetDBName() == "display_name" && ss.Value == displayName {
+				return nil
+			}
+		}
+		return errors.New("display name setting not received yet")
+	})
+	s.Require().NoError(err)
 }
