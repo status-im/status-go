e6238e9288c29149c96dab3560ec67452354c985
Patryk Osmaczko <osmaczkopatryk@gmail.com>
2024-10-03 19:14:26 +0200

    fix(communities)_: ensure community ID is populated with description
    Possibly fixes: status-im/status-desktop#16226

--- protocol/communities/community.go
+++ protocol/communities/community.go
@@ -1628,6 +1628,12 @@ func (o *Community) marshaledDescription() ([]byte, error) {
 		}
 	}
 
+	// Ensure that communities created prior to the introduction of tokenized ownership
+	// propagate community ID through the description.
+	if len(clone.ID) == 0 {
+		clone.ID = o.IDString()
+	}
+
 	return proto.Marshal(clone)
 }
 
