65d7325a4a0de2626c60f45ba2f0fa997ac813dc
frank <lovefree103@gmail.com>
2024-10-14 21:34:07 +0800

    fix_: lint

--- protocol/requests/input_connection_string_for_bootstrapping.go
+++ protocol/requests/input_connection_string_for_bootstrapping.go
@@ -1,6 +1,6 @@
 package requests
 
 type InputConnectionStringForBootstrapping struct {
-    ConnectionString string `json:"connectionString"`
-    ConfigJSON       string `json:"configJSON"`
+	ConnectionString string `json:"connectionString"`
+	ConfigJSON       string `json:"configJSON"`
 }
--- protocol/requests/switch_fleet.go
+++ protocol/requests/switch_fleet.go
@@ -1,6 +1,6 @@
 package requests
 
 type SwitchFleet struct {
-    Fleet      string `json:"fleet"`
-    ConfigJSON string `json:"configJSON"`
+	Fleet      string `json:"fleet"`
+	ConfigJSON string `json:"configJSON"`
 }
