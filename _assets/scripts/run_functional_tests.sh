431c971fbbf774cff3b8d2ab9593568506ff9764
Igor Sirotin <sirotin@status.im>
2024-10-23 17:45:12 +0100

    fix_: 1

--- _assets/scripts/run_functional_tests.sh
+++ _assets/scripts/run_functional_tests.sh
@@ -6,6 +6,14 @@ GIT_ROOT=$(cd "${BASH_SOURCE%/*}" && git rev-parse --show-toplevel)
 source "${GIT_ROOT}/_assets/scripts/colors.sh"
 source "${GIT_ROOT}/_assets/scripts/codecov.sh"
 
+if [[ -z "${FUNCTIONAL_TESTS_DOCKER_UID}" ]]; then
+  FUNCTIONAL_TESTS_DOCKER_UID=$(id -u)
+fi
+
+if [[ -z "${FUNCTIONAL_TESTS_REPORT_CODECOV}" ]]; then
+  FUNCTIONAL_TESTS_REPORT_CODECOV=false
+fi
+
 echo -e "${GRN}Running functional tests${RST}"
 
 root_path="${GIT_ROOT}/tests-functional"
@@ -27,7 +35,7 @@ all_compose_files="-f ${root_path}/docker-compose.anvil.yml -f ${root_path}/dock
 
 # Run functional tests
 echo -e "${GRN}Running tests${RST}, HEAD: $(git rev-parse HEAD)"
-docker-compose ${all_compose_files} up -d --build --remove-orphans
+docker-compose ${all_compose_files} up -d --build --remove-orphans > "${root_path}/docker.log"
 
 echo -e "${GRN}Running tests-rpc${RST}" # Follow the logs, wait for them to finish
 docker-compose ${all_compose_files} logs -f tests-rpc > "${root_path}/tests-rpc.log"
@@ -42,7 +50,7 @@ docker-compose ${all_compose_files} logs status-go > "${root_path}/statusd.log"
 docker-compose ${all_compose_files} logs status-go-no-funds > "${root_path}/statusd-no-funds.log"
 
 # Retrieve exit code
-exit_code=$(docker inspect tests-functional_tests-rpc_1 -f '{{.State.ExitCode}}');
+exit_code=$(docker inspect tests-functional-tests-rpc-1 -f '{{.State.ExitCode}}');
 
 # Cleanup containers
 echo -e "${GRN}Removing docker containers${RST}"
@@ -60,5 +68,5 @@ if [[ ${FUNCTIONAL_TESTS_REPORT_CODECOV} == 'true' ]]; then
   report_to_codecov "${test_results_path}/*.xml" "${full_coverage_profile}" "functional"
 fi
 
-echo -e "${GRN}Testing finished${RST}: exit_code=$exit_code"
+echo -e "${GRN}Testing finished${RST}: exit_code=${exit_code}"
 exit $exit_code
