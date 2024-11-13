6cc2b19ea7a17a4d11504b435917ac59bb95d5da
Igor Sirotin <sirotin@status.im>
2024-11-13 23:35:33 +0700

    wip

--- _assets/scripts/upgrade_module_version.sh
+++ _assets/scripts/upgrade_module_version.sh
@@ -3,6 +3,9 @@
 version=3
 oldVersion=$((version - 1))
 
+echo "version=${version}"
+echo "oldVersion=${oldVersion}"
+
 rm extkeys/go.sum
 rm extkeys/go.mod
 rm exchanges/go.sum
@@ -10,6 +13,8 @@ rm exchanges/go.mod
 
 sed -i '' "s|github.com/status-im/status-go/v${oldVersion}|github.com/status-im/status-go/v${version}|g" cmd/generate_handlers/generate_handlers_template.txt
 sed -i '' "s|github.com/status-im/status-go/v${oldVersion}|github.com/status-im/status-go/v${version}|g" cmd/status-backend/server/parse-api/endpoints_template.txt
+
+#make generate
 
 mod upgrade -tag ${version}
 
