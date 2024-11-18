b446a516b8a230dab88dfd71335444d3d86a078d
Igor Sirotin <sirotin@status.im>
2024-11-18 07:39:44 +0000

    script comments

--- _assets/scripts/upgrade_module_version.sh
+++ _assets/scripts/upgrade_module_version.sh
@@ -3,19 +3,20 @@
 version=3
 oldVersion=$((version - 1))
 
-echo "version=${version}"
-echo "oldVersion=${oldVersion}"
+echo "Upgrading from version ${oldVersion} to ${version}"
 
+# Remove sub-modules
 rm extkeys/go.sum
 rm extkeys/go.mod
 rm exchanges/go.sum
 rm exchanges/go.mod
 
+# Update template files
 sed -i '' "s|github.com/status-im/status-go/v${oldVersion}|github.com/status-im/status-go/v${version}|g" cmd/generate_handlers/generate_handlers_template.txt
 sed -i '' "s|github.com/status-im/status-go/v${oldVersion}|github.com/status-im/status-go/v${version}|g" cmd/status-backend/server/parse-api/endpoints_template.txt
 
-#make generate
-
+# Run upgrade tool
 mod upgrade -tag ${version}
 
+# Fix "/v3/v3" bug
 find . -name "*.go" -exec sed -i '' "s/\/v${version}\/v${version}/\/v${version}/g" {} +
