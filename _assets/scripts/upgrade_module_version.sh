98685335b33b411f713b87a409ac2fbc431c27e5
Igor Sirotin <sirotin@status.im>
2024-11-18 08:19:40 +0000

    wip

--- _assets/scripts/upgrade_module_version.sh
+++ _assets/scripts/upgrade_module_version.sh
@@ -6,8 +6,8 @@ oldVersion=$((version - 1))
 echo "Upgrading from version ${oldVersion} to ${version}"
 
 # Remove sub-modules
-rm extkeys/go.sum
-rm extkeys/go.mod
+#rm extkeys/go.sum
+#rm extkeys/go.mod
 rm exchanges/go.sum
 rm exchanges/go.mod
 
@@ -20,3 +20,6 @@ mod upgrade -tag ${version}
 
 # Fix "/v3/v3" bug
 find . -name "*.go" -exec sed -i '' "s/\/v${version}\/v${version}/\/v${version}/g" {} +
+
+# Fallback "/v3/extkeys" to "/extkeys"
+sed -i '' "s|github.com/status-im/status-go/v${oldVersion}|github.com/status-im/status-go/v${version}|g"
