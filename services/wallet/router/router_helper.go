553eda713f05f172efdc673d4bddc6bf89b8287c
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2024-11-28 18:19:28 +0400

    fix_: add tests

--- services/wallet/router/router_helper.go
+++ services/wallet/router/router_helper.go
@@ -14,6 +14,7 @@ import (
 	"github.com/status-im/status-go/contracts"
 	gaspriceoracle "github.com/status-im/status-go/contracts/gas-price-oracle"
 	"github.com/status-im/status-go/params"
+	"github.com/status-im/status-go/rpc/chain"
 	"github.com/status-im/status-go/services/wallet/bigint"
 	"github.com/status-im/status-go/services/wallet/collectibles"
 	walletCommon "github.com/status-im/status-go/services/wallet/common"
@@ -88,12 +89,16 @@ func (r *Router) estimateGasForApproval(params pathprocessor.ProcessorInputParam
 }
 
 func (r *Router) calculateApprovalL1Fee(amountIn *big.Int, chainID uint64, approvalContractAddress *common.Address) (uint64, error) {
-	data, err := walletCommon.PackApprovalInputData(amountIn, approvalContractAddress)
+	ethClient, err := r.rpcClient.EthClient(chainID)
 	if err != nil {
 		return 0, err
 	}
 
-	ethClient, err := r.rpcClient.EthClient(chainID)
+	return CalculateApprovalL1Fee(amountIn, chainID, approvalContractAddress, ethClient)
+}
+
+func CalculateApprovalL1Fee(amountIn *big.Int, chainID uint64, approvalContractAddress *common.Address, ethClient chain.ClientInterface) (uint64, error) {
+	data, err := walletCommon.PackApprovalInputData(amountIn, approvalContractAddress)
 	if err != nil {
 		return 0, err
 	}
