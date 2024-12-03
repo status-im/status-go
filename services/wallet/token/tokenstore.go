6339aecb445004c5ac93ff7d829aaa76a41899ad
Brian Sztamfater <brian@status.im>
2024-12-03 00:36:39 -0300

    fix_: usdt token decimals on tokenstore for arbitrum
    Signed-off-by: Brian Sztamfater <brian@status.im>

--- services/wallet/token/tokenstore.go
+++ services/wallet/token/tokenstore.go
@@ -1638,7 +1638,7 @@ func newDefaultStore() *DefaultStore {
 				Address:     common.HexToAddress("0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9"),
 				Name:        "Tether USD",
 				Symbol:      "USDT",
-				Decimals:    18,
+				Decimals:    6,
 				ChainID:     42161,
 				TokenListID: "status",
 			},
