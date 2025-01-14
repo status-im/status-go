b18af18f8cfab7b6b087af0d3299d90aaa9548a2
Dario Gabriel Lipicar <dario@status.im>
2025-01-14 09:29:03 -0300

    fix_: add community token deployer address for base

--- contracts/community-tokens/deployer/address.go
+++ contracts/community-tokens/deployer/address.go
@@ -4,18 +4,21 @@ import (
 	"errors"
 
 	"github.com/ethereum/go-ethereum/common"
+	walletCommon "github.com/status-im/status-go/services/wallet/common"
 )
 
 var errorNotAvailableOnChainID = errors.New("deployer contract not available for chainID")
 
 // addresses can be found on https://github.com/status-im/communities-contracts#deployments
 var contractAddressByChainID = map[uint64]common.Address{
-	1:        common.HexToAddress("0xB3Ef5B0825D5f665bE14394eea41E684CE96A4c5"), // Mainnet
-	10:       common.HexToAddress("0x31463D22750324C8721FF7751584EF62F2ff93b3"), // Optimism
-	42161:    common.HexToAddress("0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b"), // Arbitrum
-	11155111: common.HexToAddress("0xCDE984e57cdb88c70b53437cc694345B646371f9"), // Sepolia
-	421614:   common.HexToAddress("0x7Ff554af5b6624db2135E4364F416d1D397f43e6"), // Arbitrum Sepolia
-	11155420: common.HexToAddress("0xcE2A896eEA2F585BC0C3753DC8116BbE2AbaE541"), // Optimism Sepolia
+	walletCommon.EthereumMainnet: common.HexToAddress("0xB3Ef5B0825D5f665bE14394eea41E684CE96A4c5"),
+	walletCommon.OptimismMainnet: common.HexToAddress("0x31463D22750324C8721FF7751584EF62F2ff93b3"),
+	walletCommon.ArbitrumMainnet: common.HexToAddress("0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b"),
+	walletCommon.BaseMainnet:     common.HexToAddress("0x898331B756EE1f29302DeF227a4471e960c50612"),
+	walletCommon.EthereumSepolia: common.HexToAddress("0xCDE984e57cdb88c70b53437cc694345B646371f9"),
+	walletCommon.ArbitrumSepolia: common.HexToAddress("0x7Ff554af5b6624db2135E4364F416d1D397f43e6"),
+	walletCommon.OptimismSepolia: common.HexToAddress("0xcE2A896eEA2F585BC0C3753DC8116BbE2AbaE541"),
+	walletCommon.BaseSepolia:     common.HexToAddress("0x7Ff554af5b6624db2135E4364F416d1D397f43e6"),
 }
 
 func ContractAddress(chainID uint64) (common.Address, error) {
