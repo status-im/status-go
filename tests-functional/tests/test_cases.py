a14348cdf5d1d90a63d0707b298248ae505a0e1a
Florin Barbu <barbu_florin_adrian@yahoo.com>
2025-01-15 11:03:18 +0200

    test_: fix low bandwidth command

--- tests-functional/tests/test_cases.py
+++ tests-functional/tests/test_cases.py
@@ -173,9 +173,9 @@ class NetworkConditionTestCase:
             node.container_exec("tc qdisc del dev eth0 root netem")
 
     @contextmanager
-    def add_low_bandwith(self, node, rate="1mbit", burst="32kbit"):
+    def add_low_bandwith(self, node, rate="1mbit", burst="32kbit", limit="12500"):
         logging.info("Entering context manager: add_low_bandwith")
-        node.container_exec(f"apk add iproute2 && tc qdisc add dev eth0 root tbf rate {rate} burst {burst}")
+        node.container_exec(f"apk add iproute2 && tc qdisc add dev eth0 root tbf rate {rate} burst {burst} limit {limit}")
         try:
             yield
         finally:
