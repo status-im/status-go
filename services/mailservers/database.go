e469624bea0adcb46d027c089d2a8756c54fbcfa
Igor Sirotin <sirotin@status.im>
2025-03-10 18:25:54 +0000

    feat: custom Mailserver UnmarshalJSON

--- services/mailservers/database.go
+++ services/mailservers/database.go
@@ -49,6 +49,45 @@ type Mailserver struct {
 	FailedRequests uint   `json:"-"`
 }
 
+// UnmarshalJSON implements the custom JSON unmarshaling logic for Mailserver.
+// It supports ENR and Addr being saved as strings.
+func (m *Mailserver) UnmarshalJSON(data []byte) error {
+	type Alias Mailserver // Create an alias type to avoid infinite recursion
+	aux := struct {
+		Alias
+		ENR  string `json:"enr"`  // Temporary field to handle ENR as a string
+		Addr string `json:"addr"` // Temporary field to handle Addr as a string
+	}{}
+
+	// Unmarshal the data into the temporary struct
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// Set the basic fields
+	*m = Mailserver(aux.Alias)
+
+	// Decode the ENR if present
+	if aux.ENR != "" {
+		decodedENR, err := enode.Parse(enode.ValidSchemes, aux.ENR)
+		if err != nil {
+			return fmt.Errorf("invalid ENR: %w", err)
+		}
+		m.ENR = decodedENR
+	}
+
+	// Decode the Multiaddr if present
+	if aux.Addr != "" {
+		decodedAddr, err := multiaddr.NewMultiaddr(aux.Addr)
+		if err != nil {
+			return fmt.Errorf("invalid Addr: %w", err)
+		}
+		m.Addr = &decodedAddr
+	}
+
+	return nil
+}
+
 func (m Mailserver) PeerInfo() (peer.AddrInfo, error) {
 	var maddrs []multiaddr.Multiaddr
 
