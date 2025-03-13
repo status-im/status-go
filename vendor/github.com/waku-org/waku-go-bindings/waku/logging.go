66217af3dc5ea31e2f4be013b0ebc6228500f779
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2025-03-13 15:34:50 +0200

    chore_: updating waku-go-bindings

--- vendor/github.com/waku-org/waku-go-bindings/waku/logging.go
+++ vendor/github.com/waku-org/waku-go-bindings/waku/logging.go
@@ -3,41 +3,46 @@ package waku
 import (
 	"sync"
 
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 )
 
 var (
-	once     sync.Once
-	instance *logrus.Logger
+	once  sync.Once
+	sugar *zap.SugaredLogger
 )
 
-// _getLogger ensures we always return the same logger instance (private function)
-func _getLogger() *logrus.Logger {
+func _getLogger() *zap.SugaredLogger {
 	once.Do(func() {
-		instance = logrus.New()
-		instance.SetFormatter(&logrus.TextFormatter{
-			FullTimestamp: true,
-		})
-		instance.SetLevel(logrus.DebugLevel) // Set default log level
+		config := zap.NewDevelopmentConfig()
+		config.DisableCaller = true
+		config.EncoderConfig.CallerKey = ""
+		l, err := config.Build()
+		if err != nil {
+			panic(err)
+		}
+		sugar = l.Sugar()
 	})
-	return instance
+	return sugar
 }
 
-// Debug logs a debug message
+func SetLogger(newLogger *zap.Logger) {
+	once.Do(func() {})
+
+	sugar = newLogger.Sugar()
+}
+
 func Debug(msg string, args ...interface{}) {
-	_getLogger().WithFields(logrus.Fields{}).Debugf(msg, args...)
+	_getLogger().Debugf(msg, args...)
 }
 
-// Info logs an info message
 func Info(msg string, args ...interface{}) {
-	_getLogger().WithFields(logrus.Fields{}).Infof(msg, args...)
-}
-
-// Error logs an error message
-func Error(msg string, args ...interface{}) {
-	_getLogger().WithFields(logrus.Fields{}).Errorf(msg, args...)
+	_getLogger().Infof(msg, args...)
 }
 
 func Warn(msg string, args ...interface{}) {
-	_getLogger().WithFields(logrus.Fields{}).Warnf(msg, args...)
+	_getLogger().Warnf(msg, args...)
+}
+
+func Error(msg string, args ...interface{}) {
+	_getLogger().Errorf(msg, args...)
 }
