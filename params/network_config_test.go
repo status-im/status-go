c70264eb20f9791a61759dee80117fa4e101a8b0
Igor Sirotin <sirotin@status.im>
2025-03-24 17:20:45 +0000

    chore_: rename params test package

--- params/network_config_test.go
+++ params/network_config_test.go
@@ -1,25 +1,23 @@
-package params_test
+package params
 
 import (
 	"testing"
-
-	"github.com/status-im/status-go/params"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRpcProvider_GetHost(t *testing.T) {
-	provider := params.RpcProvider{URL: "https://api.example.com/path"}
+	provider := RpcProvider{URL: "https://api.example.com/path"}
 	expectedHost := "api.example.com"
 	assert.Equal(t, expectedHost, provider.GetHost())
 }
 
 func TestRpcProvider_GetFullURL(t *testing.T) {
-	provider := params.RpcProvider{URL: "https://api.example.com", AuthType: params.TokenAuth, AuthToken: "mytoken"}
+	provider := RpcProvider{URL: "https://api.example.com", AuthType: TokenAuth, AuthToken: "mytoken"}
 	expectedFullURL := "https://api.example.com/mytoken"
 	assert.Equal(t, expectedFullURL, provider.GetFullURL())
 
-	provider.AuthType = params.NoAuth
+	provider.AuthType = NoAuth
 	expectedFullURL = "https://api.example.com"
 	assert.Equal(t, expectedFullURL, provider.GetFullURL())
 }
