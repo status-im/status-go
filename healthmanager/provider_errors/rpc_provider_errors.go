9098a710cee1e3be7f987d92945796b0b9787236
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2025-03-25 15:10:16 +0400

    fix(health-manager)_: handle nil wrapped in an error type
    - Add safeRPCError function to safely handle nil RPC errors
    - Add tests
    
    fixes: https://sentry.infra.status.im/organizations/sentry/issues/160/
    fixes #6457

--- healthmanager/provider_errors/rpc_provider_errors.go
+++ healthmanager/provider_errors/rpc_provider_errors.go
@@ -2,6 +2,7 @@ package provider_errors
 
 import (
 	"errors"
+	"reflect"
 	"strings"
 
 	"github.com/ethereum/go-ethereum"
@@ -20,31 +21,52 @@ const (
 	RpcErrorTypeRPCOther       RpcProviderErrorType = "rpc_other"
 )
 
+// safeRPCError safely extracts an RPC error and its error code.
+// Returns nil, 0, false if:
+// - err is nil
+// - err is not an RPC error
+// - err is a nil RPC error pointer
+func safeRPCError(err error) (rpc.Error, int, bool) {
+	var rpcErr rpc.Error
+	if !errors.As(err, &rpcErr) {
+		return nil, 0, false
+	}
+
+	// If it's a nil pointer but wrapped, we still want to handle it as an RPC error
+	if v := reflect.ValueOf(rpcErr); v.Kind() == reflect.Ptr && v.IsNil() {
+		return nil, 0, true
+	}
+
+	return rpcErr, rpcErr.ErrorCode(), true
+}
+
 // Not found should not be cancelling the requests, as that's returned
 // when we are hitting a non archival node for example, it should continue the
 // chain as the next provider might have archival support.
 func IsNotFoundError(err error) bool {
+	if err == nil {
+		return false
+	}
 	return strings.Contains(err.Error(), ethereum.NotFound.Error())
 }
 
 func IsRPCError(err error) (rpc.Error, bool) {
-	var rpcErr rpc.Error
-	if errors.As(err, &rpcErr) {
-		return rpcErr, true
-	}
-	return nil, false
+	rpcErr, _, ok := safeRPCError(err)
+	return rpcErr, ok
 }
 
 func IsMethodNotFoundError(err error) bool {
-	if rpcErr, ok := IsRPCError(err); ok {
-		return rpcErr.ErrorCode() == -32601
-	}
-	return false
+	_, code, ok := safeRPCError(err)
+	return ok && code == -32601
 }
 
 func IsVMError(err error) bool {
-	if rpcErr, ok := IsRPCError(err); ok {
-		return rpcErr.ErrorCode() == -32015 // Код ошибки VM execution error
+	_, code, ok := safeRPCError(err)
+	if ok && code == -32015 { // VM execution error code
+		return true
+	}
+	if err == nil {
+		return false
 	}
 	if strings.Contains(err.Error(), core.ErrInsufficientFunds.Error()) {
 		return true
@@ -62,14 +84,15 @@ func determineRpcErrorType(err error) RpcProviderErrorType {
 	if err == nil {
 		return RpcErrorTypeNone
 	}
-	//if IsRpsLimitError(err) {
-	//	return RpcErrorTypeRPSLimit
-	//}
+
 	if IsMethodNotFoundError(err) || IsNotFoundError(err) {
 		return RpcErrorTypeMethodNotFound
 	}
 	if IsVMError(err) {
 		return RpcErrorTypeVMError
+	}
+	if _, ok := IsRPCError(err); ok {
+		return RpcErrorTypeRPCOther
 	}
 	return RpcErrorTypeRPCOther
 }
