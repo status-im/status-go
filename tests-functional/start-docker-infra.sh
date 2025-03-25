4f554311ea1d77d0d2b7517031af36ef26a4b393
Radoslaw Kaminski <radoslaw@status.im>
2025-03-25 15:41:27 +0000

    test_: add helper scripts

---
+++ tests-functional/start-docker-infra.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Starting docker containers"
+docker compose -f docker-compose.anvil.yml -f docker-compose.test.status-go.yml -f docker-compose.status-go.local.yml up --build --remove-orphans -d
+
+echo "Deploying smart contracts"
+pytest -m contracts
