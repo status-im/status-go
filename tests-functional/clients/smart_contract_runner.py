087e07bec8780abcb21f917f1648201c2ef267e7
Radoslaw Kaminski <radoslaw@status.im>
2025-03-26 13:14:14 +0000

    test_: revert unnecessary changes

--- tests-functional/clients/smart_contract_runner.py
+++ tests-functional/clients/smart_contract_runner.py
@@ -2,6 +2,7 @@ import io
 import json
 import logging
 import tarfile
+import tempfile
 import time
 import docker
 import docker.errors
@@ -84,6 +85,7 @@ class SmartContractRunner:
 
         cmd = f"/app/clone_and_run.sh {github_org} {github_repo} {smart_contract_dir} {smart_contract_filename} {private_key} {sender_address}"
         logging.info(f"Running command: {cmd}")
+
         exec_result = self.container.exec_run(
             f"{cmd}",
             workdir="/app",
@@ -94,14 +96,14 @@ class SmartContractRunner:
             raise Exception(f"Failed to clone and run {github_repo}")
 
         container_output_path = f"/app/{github_repo}/broadcast/{smart_contract_filename}/{ANVIL_NETWORK_ID}/run-latest.json"
-        host_output_path = self._extract_data(container_output_path, github_repo)
+        host_output_path = self._extract_data(container_output_path)
         if not host_output_path:
             raise Exception(f"Failed to extract data from {container_output_path}")
         with open(host_output_path, "r") as f:
             output = json.load(f)
         return output["returns"]
 
-    def _extract_data(self, path: str, repo_name: str):
+    def _extract_data(self, path: str):
         if not self.container:
             return path
 
@@ -110,28 +112,14 @@ class SmartContractRunner:
         except docker.errors.NotFound:
             return None
 
-        output_dir = self._get_output_dir(repo_name)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
+        temp_dir = tempfile.mkdtemp()
         tar_bytes = io.BytesIO(b"".join(stream))
 
         with tarfile.open(fileobj=tar_bytes) as tar:
-            tar.extractall(path=output_dir)
+            tar.extractall(path=temp_dir)
             # If the tar contains a single file, return the path to that file
-            # Otherwise it's a directory, just return output_dir.
+            # Otherwise it's a directory, just return temp_dir.
             if len(tar.getmembers()) == 1:
-                return os.path.join(output_dir, tar.getmembers()[0].name)
+                return os.path.join(temp_dir, tar.getmembers()[0].name)
 
-        return output_dir
-
-    def _get_output_dir(self, repo_name: str):
-        working_dir = os.getcwd()
-
-        tests_functional = "tests-functional"
-        if tests_functional not in working_dir:
-            working_dir = os.path.join(working_dir, tests_functional)
-
-        output_dir = os.path.join(working_dir, "tmp", "smart-contracts-output", repo_name)
-
-        return output_dir
+        return temp_dir
