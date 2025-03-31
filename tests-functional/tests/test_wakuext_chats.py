51349284321141c09ff2e573628ab68f470cc506
Radoslaw Kaminski <radoslaw@status.im>
2025-03-31 10:37:39 +0100

    test_: improve all chats test

--- tests-functional/tests/test_wakuext_chats.py
+++ tests-functional/tests/test_wakuext_chats.py
@@ -10,15 +10,18 @@ from steps.messenger import MessengerSteps
 class TestChatActions(MessengerSteps):
 
     def test_all_chats(self):
-        sent_texts, _ = self.send_multiple_one_to_one_messages(1)
+        self.make_contacts()
+        private_group_id = self.join_private_group()
+        self.sender.wakuext_service.send_chat_message(private_group_id, "test_message")
+        self.send_multiple_one_to_one_messages(1)
 
         response = self.sender.wakuext_service.chats()
         self.sender.verify_json_schema(response, method="wakuext_chats")
 
         chats = response.get("result", [])
-        assert len(chats) == 1
+        assert len(chats) == 2
         assert chats[0].get("chatType", 0) == ChatType.ONE_TO_ONE.value
-        assert chats[0].get("lastMessage", {}).get("text", "") == sent_texts[0]
+        assert chats[1].get("chatType", 0) == ChatType.PRIVATE_GROUP_CHAT.value
 
     def test_chat_by_chat_id(self):
         sent_texts, _ = self.send_multiple_one_to_one_messages(1)
