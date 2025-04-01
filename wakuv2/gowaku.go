e2394d3d4eeb479c7f43bfcc4b0004e698e53ce9
Igor Sirotin <sirotin@status.im>
2025-04-01 12:40:48 +0100

    test(wakuv2)_: handlePeerAddress

--- wakuv2/gowaku.go
+++ wakuv2/gowaku.go
@@ -21,6 +21,10 @@
 
 package wakuv2
 
+// Generate a mock for peerAddressHandler. Keep it in same dir and package, as it's a private type.
+// Yet we name the file _test.go to keep it only available in testing environment.
+//go:generate mockgen -source=gowaku.go -destination=gowaku_mock_test.go -package=wakuv2
+
 import (
 	"context"
 	"crypto/ecdsa"
