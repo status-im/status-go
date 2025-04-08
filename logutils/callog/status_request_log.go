3221bcd425c1da586ad7dd6d115c924b48045a48
Emil Sawicki <emil.sawicki9@gmail.com>
2025-04-08 23:30:37 +0200

    feat_: Handle market data proxy config

--- logutils/callog/status_request_log.go
+++ logutils/callog/status_request_log.go
@@ -38,6 +38,8 @@ var sensitiveKeys = []string{
 	"alchemyBaseSepoliaToken",
 	"statusProxyMarketUser",
 	"statusProxyMarketPassword",
+	"marketDataProxyLogin",
+	"marketDataProxyPassword",
 	"statusProxyBlockchainUser",
 	"statusProxyBlockchainPassword",
 	"verifyENSURL",
