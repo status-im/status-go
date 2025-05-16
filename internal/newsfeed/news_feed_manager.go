a2fcb9ac780e25bbafd81cff2402615208f9bcc3
Jonathan Rainville <rainville.jonathan@gmail.com>
2025-05-16 13:21:35 -0400

    chore(feed)_: set up the prod RSS urls and enable the feature flag

--- internal/newsfeed/news_feed_manager.go
+++ internal/newsfeed/news_feed_manager.go
@@ -10,8 +10,8 @@ import (
 	gocommon "github.com/status-im/status-go/common"
 )
 
-const STATUS_DESKTOP_FEED_URL = "https://status-website-git-add-rss-status-im-web.vercel.app/desktop-news/rss?_vercel_share=IySrfv1xyphlMgEp3rAfrCnzT19yjzG9&x-vercel-protection-bypass=I4euHZRiK3TEnAE5Kw73tQxhmxjBThSS"
-const STATUS_MOBILE_FEED_URL = "https://status-website-git-add-rss-status-im-web.vercel.app/mobile-news/rss?_vercel_share=IySrfv1xyphlMgEp3rAfrCnzT19yjzG9&x-vercel-protection-bypass=I4euHZRiK3TEnAE5Kw73tQxhmxjBThSS"
+const STATUS_DESKTOP_FEED_URL = "https://status.app/desktop-news/rss"
+const STATUS_MOBILE_FEED_URL = "https://status.app/mobile-news/rss"
 
 type FeedParser interface {
 	ParseURL(url string) (*gofeed.Feed, error)
