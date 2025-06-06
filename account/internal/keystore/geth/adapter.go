f2e1aa1c78940fc9ab446d730a0a4ef20a5a9b6f
Sale Djenic <aleksandardjenic@status.im>
2025-06-06 14:57:12 +0200

    refactor(account-management)_: move one step further from go-ethereum
    - Refactored the importExtendedKey and AccountDecryptedKey methods to simplify parameters and improve clarity.
    - Changes done here partially break the status-go dependency to go-ethereum, the only remaining dependency that
    needs to be solved is in using `keystore.DecryptKey` and `keystore.EncryptKey` functions.

--- account/internal/keystore/geth/adapter.go
+++ account/internal/keystore/geth/adapter.go
@@ -2,15 +2,12 @@ package geth
 
 import (
 	"crypto/ecdsa"
-	"errors"
-	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/status-im/extkeys"
 
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/status-im/status-go/account/types"
+	"github.com/status-im/status-go/eth-node/crypto"
 	ethtypes "github.com/status-im/status-go/eth-node/types"
 )
 
@@ -24,34 +21,56 @@ func NewAdapter(ks *keystore.KeyStore) *Adapter {
 	}
 }
 
+// ImportECDSA imports an ECDSA private key
 func (a *Adapter) ImportECDSA(priv *ecdsa.PrivateKey, passphrase string) (types.Account, error) {
 	gethAccount, err := a.keystore.ImportECDSA(priv, passphrase)
 	return accountFrom(gethAccount), err
 }
 
+// ImportSingleExtendedKey imports an extended key by converting it to ECDSA private key
 func (a *Adapter) ImportSingleExtendedKey(extKey *extkeys.ExtendedKey, passphrase string) (types.Account, error) {
-	gethAccount, err := a.keystore.ImportSingleExtendedKey(extKey, passphrase)
-	return accountFrom(gethAccount), err
+	privateKey := extKey.ToECDSA()
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+	account, err := a.Find(address)
+	if err == nil {
+		return account, nil
+	}
+	return a.updateKeystoreFile(privateKey, extKey, passphrase)
 }
 
-func (a *Adapter) ImportExtendedKeyForPurpose(keyPurpose extkeys.KeyPurpose, extKey *extkeys.ExtendedKey, passphrase string) (types.Account, error) {
-	gethAccount, err := a.keystore.ImportExtendedKeyForPurpose(keyPurpose, extKey, passphrase)
-	return accountFrom(gethAccount), err
+// ImportExtendedKeyForWallet imports an extended key for a wallet purpose by deriving the appropriate child key
+func (a *Adapter) ImportExtendedKeyForWallet(extKey *extkeys.ExtendedKey, passphrase string) (types.Account, error) {
+	const keyPurpose = extkeys.KeyPurposeWallet
+	key, err := newKeyForPurposeFromExtendedKey(keyPurpose, extKey)
+	if err != nil {
+		zeroKey(key.PrivateKey)
+		return types.Account{}, err
+	}
+
+	account, err := a.Find(key.Address)
+	if err == nil {
+		return account, nil
+	}
+	return a.updateKeystoreFile(key.PrivateKey, key.ExtendedKey, passphrase)
 }
 
-func (a *Adapter) AccountDecryptedKey(account types.Account, auth string) (types.Account, *ethtypes.Key, error) {
-	gethAccount, err := gethAccountFrom(account)
+// AccountDecryptedKey gets the decrypted key for an account using standard go-ethereum functions
+func (a *Adapter) AccountDecryptedKey(address ethtypes.Address, auth string) (types.Account, *ethtypes.Key, error) {
+	gethAccount, err := a.find(address)
 	if err != nil {
 		return types.Account{}, nil, err
 	}
 
-	var gethKey *keystore.Key
-	gethAccount, gethKey, err = a.keystore.AccountDecryptedKey(gethAccount, auth)
-	return accountFrom(gethAccount), keyFrom(gethKey), err
+	ethKey, err := readKeystoreFileAndDecryptedKey(gethAccount.URL.Path, auth)
+	if err != nil {
+		return types.Account{}, nil, err
+	}
+
+	return accountFrom(gethAccount), ethKey, nil
 }
 
-func (a *Adapter) Delete(account types.Account) error {
-	gethAccount, err := gethAccountFrom(account)
+func (a *Adapter) Delete(address ethtypes.Address) error {
+	gethAccount, err := a.find(address)
 	if err != nil {
 		return err
 	}
@@ -68,47 +87,10 @@ func (a *Adapter) Accounts() []types.Account {
 	return accounts
 }
 
-// parseGethURL converts a user supplied URL into the accounts specific structure.
-func parseGethURL(url string) (accounts.URL, error) {
-	parts := strings.Split(url, "://")
-	if len(parts) != 2 || parts[0] == "" {
-		return accounts.URL{}, errors.New("protocol scheme missing")
+func (a *Adapter) Find(address ethtypes.Address) (types.Account, error) {
+	gethAccount, err := a.find(address)
+	if err != nil {
+		return types.Account{}, err
 	}
-	return accounts.URL{
-		Scheme: parts[0],
-		Path:   parts[1],
-	}, nil
-}
-
-func gethAccountFrom(account types.Account) (accounts.Account, error) {
-	var (
-		gethAccount accounts.Account
-		err         error
-	)
-	gethAccount.Address = gethcommon.Address(account.Address)
-	if account.URL != "" {
-		gethAccount.URL, err = parseGethURL(account.URL)
-	}
-	return gethAccount, err
-}
-
-func accountFrom(gethAccount accounts.Account) types.Account {
-	return types.Account{
-		Address: ethtypes.Address(gethAccount.Address),
-		URL:     gethAccount.URL.String(),
-	}
-}
-
-func keyFrom(k *keystore.Key) *ethtypes.Key {
-	if k == nil {
-		return nil
-	}
-
-	return &ethtypes.Key{
-		ID:              k.Id,
-		Address:         ethtypes.Address(k.Address),
-		PrivateKey:      k.PrivateKey,
-		ExtendedKey:     k.ExtendedKey,
-		SubAccountIndex: k.SubAccountIndex,
-	}
+	return accountFrom(gethAccount), nil
 }
