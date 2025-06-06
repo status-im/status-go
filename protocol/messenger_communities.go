8a0ef7668468bb7562eae7f569b74c26ac9975d6
Jonathan Rainville <rainville.jonathan@gmail.com>
2025-06-06 10:39:18 -0400

    fix(manager_archive)_: fix crash caused by nil filter
    Fixes https://github.com/status-im/status-desktop/issues/18087

--- protocol/messenger_communities.go
+++ protocol/messenger_communities.go
@@ -3873,7 +3873,10 @@ func (m *Messenger) InitHistoryArchiveTasks(communities []*communities.Community
 				topics = append(topics, filter.ContentTopic())
 			}
 
-			filters = append(filters, m.messaging.ChatFilterByChatID(c.UniversalChatID()))
+			filter := m.messaging.ChatFilterByChatID(c.UniversalChatID())
+			if filter != nil {
+				filters = append(filters, filter)
+			}
 
 			// First we need to know the timestamp of the latest waku message
 			// we've received for this community, so we can request messages we've
@@ -4315,6 +4318,9 @@ func (m *Messenger) pinMessagesToWakuMessages(pinMessages []*common.PinMessage, 
 	for _, msg := range pinMessages {
 
 		filter := m.messaging.ChatFilterByChatID(msg.LocalChatID)
+		if filter == nil {
+			return nil, ErrNoFiltersForChat
+		}
 		encodedPayload, err := proto.Marshal(msg.GetProtobuf())
 		if err != nil {
 			return nil, err
@@ -4345,6 +4351,9 @@ func (m *Messenger) chatMessagesToWakuMessages(chatMessages []*common.Message, c
 	for _, msg := range chatMessages {
 
 		filter := m.messaging.ChatFilterByChatID(msg.LocalChatID)
+		if filter == nil {
+			return nil, ErrNoFiltersForChat
+		}
 		encodedPayload, err := proto.Marshal(msg.GetProtobuf())
 		if err != nil {
 			return nil, err
