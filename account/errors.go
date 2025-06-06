74ba334f582a506e09e69bb182d473452c79be3d
Sale Djenic <aleksandardjenic@status.im>
2025-06-06 14:57:12 +0200

    refactor(account-management)_: move one step further from go-ethereum
    - Refactored the importExtendedKey and AccountDecryptedKey methods to simplify parameters and improve clarity.
    - Changes done here partially break the status-go dependency to go-ethereum, the only remaining dependency that
    needs to be solved is in using `keystore.DecryptKey` and `keystore.EncryptKey` functions.

--- account/errors.go
+++ account/errors.go
@@ -5,10 +5,8 @@ import (
 )
 
 var (
-	ErrAddressToAccountMappingFailure = errors.New("cannot retrieve a valid account for a given address")
-	ErrAccountToKeyMappingFailure     = errors.New("cannot retrieve a valid key for a given account")
-	ErrNoAccountSelected              = errors.New("no account has been selected, please login")
-	ErrOnboardingNotStarted           = errors.New("onboarding must be started before choosing an account")
-	ErrOnboardingAccountNotFound      = errors.New("cannot find onboarding account with the given id")
-	ErrAccountKeyStoreMissing         = errors.New("account key store is not set")
+	ErrNoAccountSelected         = errors.New("no account has been selected, please login")
+	ErrOnboardingNotStarted      = errors.New("onboarding must be started before choosing an account")
+	ErrOnboardingAccountNotFound = errors.New("cannot find onboarding account with the given id")
+	ErrAccountKeyStoreMissing    = errors.New("account key store is not set")
 )
