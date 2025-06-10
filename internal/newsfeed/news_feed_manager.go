d2e600cf56e9a0c66fb6acfd86ade0274c580e91
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2025-06-10 15:49:35 +0100

    Updated rss link to blogpost rss feed that can be used for testing

--- internal/newsfeed/news_feed_manager.go
+++ internal/newsfeed/news_feed_manager.go
@@ -11,7 +11,7 @@ import (
 )
 
 const STATUS_DESKTOP_FEED_URL = "https://status.app/desktop-news/rss"
-const STATUS_MOBILE_FEED_URL = "https://vkjr.github.io/status-mobile-rss-feed-test/rss.xml"
+const STATUS_MOBILE_FEED_URL = "https://status-rss-feed-test.blogspot.com/feeds/posts/default"
 
 type FeedParser interface {
 	ParseURL(url string) (*gofeed.Feed, error)
