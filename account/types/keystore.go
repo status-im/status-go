7f7809dff8a4dc5c13f3807924b609402437ef71
Sale Djenic <aleksandardjenic@status.im>
2025-06-12 21:21:28 +0200

    refactor(account-management)_: decouple and enhance keystore functionality
    - Removed the old GethManager and associated methods, moving to a new adapter-based approach for keystore management.
    - Previously Introduced keystore adapter, updated, improved and moved from internal.
    - Updated account management methods to utilize the new adapter, enhancing clarity and usability.
    - Refactored tests to align with the new structure and ensure coverage.
    - Cleaned up unused code and improved overall readability of the account package.

--- account/types/keystore.go
+++ account/types/keystore.go
@@ -16,22 +16,17 @@ type KeyStore interface {
 	// ImportExtendedKey is used in older version of Status where PrivateKey is set to be the BIP44 key at index 0,
 	// and ExtendedKey is the extended key of the BIP44 key at index 1.
 	ImportSingleExtendedKey(extKey *extkeys.ExtendedKey, passphrase string) (Account, error)
-	// ImportExtendedKeyForWallet stores ECDSA key (obtained from extended key) along with CKD#2 (root for sub-accounts)
-	// If key file is not found, it is created. Key is encrypted with the given passphrase.
-	// Deprecated: status-go is now using ImportSingleExtendedKey
-	ImportExtendedKeyForWallet(extKey *extkeys.ExtendedKey, passphrase string) (Account, error)
 	// AccountDecryptedKey returns decrypted key for account (provided that password is correct).
 	AccountDecryptedKey(address ethtypes.Address, passphrase string) (Account, *ethtypes.Key, error)
-	// Delete deletes the key matched by account if the passphrase is correct.
+	// Delete deletes the key matched by account.
 	// If the account contains no filename, the address must match a unique key.
 	Delete(address ethtypes.Address) error
 	// Find returns the account matched by address
 	Find(address ethtypes.Address) (Account, error)
 	// Accounts returns all accounts in the keystore
 	Accounts() []Account
-	// VerifyPassword tries to decrypt a given account key file, with a provided password.
-	// If no error is returned, then account is considered verified.
-	VerifyPassword(address ethtypes.Address, passphrase string) (*ethtypes.Key, error)
 	// ReEncryptKeyStoreDir re-encrypts all keys in the keystore directory.
-	ReEncryptKeyStoreDir(keyDirPath, oldPass, newPass string) error
+	ReEncryptKeyStoreDir(oldPass, newPass string) error
+	// MigrateKeyStoreDir migrates the keystore directory from one location to another.
+	MigrateKeyStoreDir(newDir string, addresses []string) error
 }
