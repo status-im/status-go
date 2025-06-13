7b2be55717db3da979888fa9ff110b68819ed54c
Sale Djenic <aleksandardjenic@status.im>
2025-06-13 10:48:50 +0200

    refactor(account-management)_: remove onboarding functionality and related tests
    - Deleted the onboarding-related methods and tests from the account package.
    - Cleaned up unused code.
    - Adjusted error handling by removing onboarding-specific errors.

--- account/errors.go
+++ account/errors.go
@@ -5,8 +5,6 @@ import (
 )
 
 var (
-	ErrNoAccountSelected         = errors.New("no account has been selected, please login")
-	ErrOnboardingNotStarted      = errors.New("onboarding must be started before choosing an account")
-	ErrOnboardingAccountNotFound = errors.New("cannot find onboarding account with the given id")
-	ErrAccountKeyStoreMissing    = errors.New("account key store is not set")
+	ErrNoAccountSelected      = errors.New("no account has been selected, please login")
+	ErrAccountKeyStoreMissing = errors.New("account key store is not set")
 )
