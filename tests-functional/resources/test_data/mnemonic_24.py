e944d6e107c6e6fe54a485a49753fcc12ca61782
Churikova Tetiana <tatiana@status.im>
2025-06-18 13:00:56 +0200

    test_: add missing test data

---
+++ tests-functional/resources/test_data/mnemonic_24.py
@@ -0,0 +1,39 @@
+# Account data for mnemonic with 24 words
+
+accounts = [
+    {
+        "address": "0x8a6d1f3b9f158f7274ca4be1a3f0056c86e2ccdb",
+        "public-key": (
+            "0x04c25b359fab7fc8989d6325128b06dd9734b38d207dc2ab652e130a5d59852910fd6414694e1f5ce3a9cdd5c1" "f6bec9a425a57bae10c98ff4337adba3bf8c18bb"
+        ),
+        "path": "m/43'/60'/1581'/0'/0",
+        "prodPreferredChainIds": "1:10:42161:8453",
+        "operable": "fully",
+        "position": -1,
+    },
+    {
+        "address": "0xf2d58ae5aa880f7c3f65d769296b1061c61e0955",
+        "public-key": (
+            "0x04218096ceb5420c9b4cfa9d0187a057099540edff0aa5882b0a16b76fc8f0056d1a01930db4981f8885d00137" "c535740c04eec7ebe8bae7ef9fd98338fba31e04"
+        ),
+        "path": "m/44'/60'/0'/0/0",
+        "name": "Account 1",
+        "colorId": "primary",
+        "hidden": False,
+        "prodPreferredChainIds": "1:10:42161:8453",
+        "position": 0,
+    },
+]
+
+profile_data = {
+    "address": "0xb47386b0074a9ddfd979540f134915d1df8dc3d0",
+    "dapps-address": "0xf2d58ae5aa880f7c3f65d769296b1061c61e0955",
+    "eip1581-address": "0xf203f9c33afd10e2d3888289ad2cad81c4b017c4",
+    "key-uid": "0xcf119f28496e4123dd6d5a4936c5f595ee1a873b11ead5f275098456eb8777c4",
+    "name": "Selfassured Pesky Mayfly",
+    "public-key": (
+        "0x04c25b359fab7fc8989d6325128b06dd9734b38d207dc2ab652e130a5d59852910fd6414694e1f5ce3a9cdd5c1f6be" "c9a425a57bae10c98ff4337adba3bf8c18bb"
+    ),
+    "wallet-root-address": "0x0410bd5715fdd8ccadede1d3131a9180a96e502c",
+    "emojiHash": ["👦🏻", "🕔", "🧜", "👩🏽‍🤝‍👨🏼", "👩🏿‍🔧", "🉐", "🧝🏿‍♂️", "🚣🏾‍♀️", "🫀", "🏄🏿", "🌘", "🤵🏼‍♀️", "🏄🏿‍♀️", "🎴"],
+}
