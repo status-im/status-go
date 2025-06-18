ff7c17c1a370e00bb052da025f08554ad5403587
Churikova Tetiana <tatiana@status.im>
2025-06-18 13:00:56 +0200

    test_: add missing test data

---
+++ tests-functional/resources/test_data/mnemonic_12.py
@@ -0,0 +1,61 @@
+# Account data for mnemonic with 12 words
+
+accounts = [
+    {
+        "address": "0xb01a7dbaaacc92581558a4d178289be7471ba0f4",
+        "public-key": (
+            "0x047fd6e4384b764cb7782bf747ad3fb51e8c54c2b3c2be7029c72d85d16b07f4be6a8703cf0a1d97be4c8712ad" "52cd6a519efdc723faae4935bbaba5c320dde02b"
+        ),
+        "path": "m/43'/60'/1581'/0'/0",
+        "prodPreferredChainIds": "1:10:42161:8453",
+        "operable": "fully",
+        "position": -1,
+    },
+    {
+        "address": "0xc43f4ab94ec965a3ee9815c5df07383057d261a8",
+        "public-key": (
+            "0x041d8f6ebfa662c506d3d11cd407373f8ff2eb4c9ddc61a834864150a908172efbaa37f0de7dbeeea51f02272" "74e6ccd78fa5a07de55716094dbba475ac9e9ab44"
+        ),
+        "path": "m/44'/60'/0'/0/0",
+        "name": "Account 1",
+        "colorId": "primary",
+        "hidden": False,
+        "prodPreferredChainIds": "1:10:42161:8453",
+        "position": 0,
+    },
+]
+
+profile_data = {
+    "address": "0x5f8e02f9f52709b29c82bd893d9af0f83273a6e4",
+    "currency": "usd",
+    "networks/current-network": "",
+    "dapps-address": "0xc43f4ab94ec965a3ee9815c5df07383057d261a8",
+    "eip1581-address": "0x803102f704324d4a4f2ddb1c47f6ab31339d0f70",
+    "key-uid": "0x3231d92c94548d14f097173765a50bebe28fbad8f2267c9e08cc4433a6f219a4",
+    "latest-derived-path": 0,
+    "link-preview-request-enabled": True,
+    "messages-from-contacts-only": False,
+    "mutual-contact-enabled?": False,
+    "name": "Anchored Open Wirehair",
+    "networks/networks": [],
+    "news-feed-enabled?": True,
+    "news-rss-enabled?": True,
+    "photo-path": "",
+    "preview-privacy?": False,
+    "public-key": (
+        "0x047fd6e4384b764cb7782bf747ad3fb51e8c54c2b3c2be7029c72d85d16b07f4be6a8703cf0a1d97be4c8712ad52cd" "6a519efdc723faae4935bbaba5c320dde02b"
+    ),
+    "default-sync-period": 777600,
+    "appearance": 0,
+    "profile-pictures-show-to": 2,
+    "profile-pictures-visibility": 2,
+    "use-mailservers?": True,
+    "wallet-root-address": "0x1846a7930d0ab03e5a120ebdd46eff3fe0365824",
+    "send-status-updates?": True,
+    "backup-enabled?": True,
+    "show-community-asset-when-sending-tokens?": True,
+    "display-assets-below-balance-threshold": 100000000,
+    "url-unfurling-mode": 1,
+    "compressedKey": "zQ3shoF8xQNaT44MWQxztUXK6DK9UU63PRgJhn1Zd7oYWXJ5K",
+    "emojiHash": ["🔢", "🤞🏽", "🖍️", "🙇🏽‍♀️", "🙋🏼‍♀️", "🙌", "💎", "🎞️", "😊", "🦸🏼", "😤", "👵🏿", "🧑🏿‍🔧", "🤶🏿"],
+}
