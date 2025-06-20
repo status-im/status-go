9dc0c7f78907c07b5a23179b0fc9152922446fc9
Igor Sirotin <sirotin@status.im>
2025-06-20 10:30:49 +0100

    ci_: docker mount cache id

--- _assets/scripts/run_functional_tests.sh
+++ _assets/scripts/run_functional_tests.sh
@@ -43,6 +43,7 @@ echo -e "${GRN}Building status-go${RST}"
 docker build --file ./_assets/build/Dockerfile . \
   --build-arg "build_flags=-cover" \
   --build-arg "build_tags='gowaku_no_rln,enable_private_api'" \
+  --build-arg "cache_id=${BUILD_ID}" \
   --tag "${image_name}"
 
 # Run docker
