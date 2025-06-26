04a98787f9cdd03c50a032fc516a5d15616bc917
Sale Djenic <aleksandardjenic@status.im>
2025-06-26 16:22:13 +0200

    feat(wallet)_: implement linea_estimateGas for Status Sepolia chain
    - Added support for the `linea_estimateGas` method in the wallet's gas estimation logic.
    - Updated the `EstimateGas` function to route to `linea_estimateGas` when the chain ID is for Status Sepolia chain.
    - Enhanced fee management to dynamically evaluate the account that is going to place a tx is base fee less
    and/or priority fee less for the Status Sepolia chain.

--- services/wallet/router/fees/fees_history.go
+++ services/wallet/router/fees/fees_history.go
@@ -2,10 +2,13 @@ package fees
 
 import (
 	"context"
+	"math/big"
 
+	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethCommon "github.com/ethereum/go-ethereum/common"
 	gaspriceproxy "github.com/status-im/status-go/contracts/gas-price-proxy"
-	"github.com/status-im/status-go/services/wallet/common"
+	walletCommon "github.com/status-im/status-go/services/wallet/common"
 )
 
 type FeeHistory struct {
@@ -17,8 +20,9 @@ type FeeHistory struct {
 
 func (fh *FeeHistory) isEIP1559Compatible(chainID uint64) bool {
 	// Since the Status Network is gasless chain, but EIP-1559 compatible, we should not rely on checking the BaseFeePerGas, that's why we have this special case.
-	if common.IsGaslessChainAndEIP1559Compatible(chainID) {
-		return true
+	eip1559Enabled, err := walletCommon.IsPartiallyOrFullyGaslessChainEIP1559Compatible(chainID)
+	if err == nil {
+		return eip1559Enabled
 	}
 
 	if len(fh.BaseFeePerGas) == 0 {
@@ -36,9 +40,9 @@ func (fh *FeeHistory) isEIP1559Compatible(chainID uint64) bool {
 
 func (f *FeeManager) getFeeHistory(ctx context.Context, chainID uint64, newestBlock string, rewardPercentiles []int) (*FeeHistory, error) {
 	blockCount := uint64(10) // use the last 10 blocks for L1 chains
-	if chainID != common.EthereumMainnet &&
-		chainID != common.EthereumSepolia &&
-		chainID != common.AnvilMainnet {
+	if chainID != walletCommon.EthereumMainnet &&
+		chainID != walletCommon.EthereumSepolia &&
+		chainID != walletCommon.AnvilMainnet {
 		blockCount = 50 // use the last 50 blocks for L2 chains
 	}
 
@@ -47,9 +51,44 @@ func (f *FeeManager) getFeeHistory(ctx context.Context, chainID uint64, newestBl
 	return feeHistory, err
 }
 
+func (f *FeeManager) getGaslessParamsForAccount(ctx context.Context, chainID uint64, address ethCommon.Address) (baseFee *big.Int, priorityFee *big.Int, err error) {
+	if !walletCommon.IsPartiallyOrFullyGaslessChain(chainID) {
+		return nil, nil, nil
+	}
+
+	toAddress := walletCommon.ZeroAddress()
+	msg := ethereum.CallMsg{
+		From:  address,
+		To:    &toAddress,
+		Value: walletCommon.ZeroBigIntValue(),
+	}
+
+	var result struct {
+		BaseFeePerGas     string `json:"baseFeePerGas"`
+		PriorityFeePerGas string `json:"priorityFeePerGas"`
+	}
+
+	err = f.RPCClient.CallContext(ctx, &result, chainID, "linea_estimateGas", walletCommon.ToCallArg(msg))
+	if err != nil {
+		return
+	}
+
+	baseFee, err = hexStringToBigInt(result.BaseFeePerGas)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	priorityFee, err = hexStringToBigInt(result.PriorityFeePerGas)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return
+}
+
 // GetL1Fee returns L1 fee for placing a transaction to L1 chain, appicable only for txs made from L2.
 func (f *FeeManager) GetL1Fee(ctx context.Context, chainID uint64, input []byte) (uint64, error) {
-	if chainID == common.EthereumMainnet || chainID == common.EthereumSepolia {
+	if chainID == walletCommon.EthereumMainnet || chainID == walletCommon.EthereumSepolia {
 		return 0, nil
 	}
 
