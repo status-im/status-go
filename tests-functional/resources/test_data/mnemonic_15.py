3d445349f00efb3bad90e5c2ccc1c99933320cc9
fbarbu15 <florin@status.im>
2025-08-04 14:39:27 +0300

    test_: backup mnemonic and restore account  (#6789)
    * test_: add restore account
    
    * test_: add restore account tests
    
    * test_: add restore account tests
    
    * test_: add concurrent test
    
    * test_: class rename
    
    * test_: fix assert
    
    * test_: review comments
    
    * test_: fix
    
    * test_: fix review comments

---
+++ tests-functional/resources/test_data/mnemonic_15.py
@@ -0,0 +1,39 @@
+# Account data for mnemonic with 15 words
+
+accounts = [
+    {
+        "address": "0x245b7438961de05444645898f9215e5cf0786891",
+        "public-key": (
+            "0x04c898c7763afd577f10efdd9e5d607caafd6d708e6cad8cee1b6d822d6ab148eb4e76d1c8266c8b73c8ce1d76" "699e072ac5844a9a6934abb565044bf619336302"
+        ),
+        "path": "m/43'/60'/1581'/0'/0",
+        "prodPreferredChainIds": "1:10:42161:8453",
+        "operable": "fully",
+        "position": -1,
+    },
+    {
+        "address": "0x685d7ec8e08769ca7020a6b65709887e38e68e6d",
+        "public-key": (
+            "0x0480acddfad1e73c3e70e8e50f82eb1566e3df125736e9fe9042c4df5022c825afe6234021ad8bbb43e0ab0196" "878c2d9e3c8b5a8f266aca72b0e23d1f84464c72"
+        ),
+        "path": "m/44'/60'/0'/0/0",
+        "name": "Account 1",
+        "colorId": "primary",
+        "hidden": False,
+        "prodPreferredChainIds": "1:10:42161:8453",
+        "position": 0,
+    },
+]
+
+profile_data = {
+    "address": "0x3644a8cc3860606fdee3b95c8825e17933a91647",
+    "dapps-address": "0x685d7ec8e08769ca7020a6b65709887e38e68e6d",
+    "eip1581-address": "0xe98734898ff58ac33a1a9c28f732696ec3e6b580",
+    "key-uid": "0x944c1ce03f83dd1750acee591745d6ef14da90723af86f97b2df7d7282e8dd97",
+    "name": "Overcooked Lost Grayreefshark",
+    "public-key": (
+        "0x04c898c7763afd577f10efdd9e5d607caafd6d708e6cad8cee1b6d822d6ab148eb4e76d1c8266c8b73c8ce1d76699e" "072ac5844a9a6934abb565044bf619336302"
+    ),
+    "wallet-root-address": "0xe89675c9be641ceeca9f250345dc58528c3de93b",
+    "emojiHash": ["🏄🏾", "👒", "👨🏽‍🎓", "🅰️", "👩🏾‍🍳", "🪀", "📩", "🐄", "⏳", "👸🏻", "🚣🏾‍♂️", "👩🏽‍🤝‍👩🏻", "📀", "🌒"],
+}
