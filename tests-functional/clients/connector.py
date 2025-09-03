66e43f1a20a51fd4460d8b5a72d677f8349c8385
Radoslaw Kaminski <radoslaw@status.im>
2025-09-03 14:07:41 +0100

    test_: add more connector service tests

--- tests-functional/clients/connector.py
+++ tests-functional/clients/connector.py
@@ -10,9 +10,14 @@ class ConnectorClient:
     def __init__(self, url: str):
         self.url = url
         self.ws_conn: WebSocket | None = None
-        self.request_id = 0
+        self._request_id = 0
         self.wrapped_request_id = 0
         self.name = ""
+
+    @property
+    def request_id(self) -> int:
+        self._request_id += 1
+        return self._request_id
 
     def connect(self):
         http_url = self.url.replace("ws", "http")
@@ -34,26 +39,45 @@ class ConnectorClient:
             self.ws_conn.close()
 
     def get_accounts(self):
+        self._send("eth_accounts")
+
+    def get_chain_id(self):
+        self._send("eth_chainId")
+
+    def get_block_number(self):
+        self._send("eth_blockNumber")
+
+    def revoke_permissions(self):
+        self._send("wallet_revokePermissions")
+
+    def _send(self, method, params=None):
         assert self.ws_conn is not None
 
-        self.request_id += 1
-
+        request_id = self.request_id
         request = {
             "jsonrpc": "2.0",
-            "id": self.request_id,
+            "id": request_id,
             "name": self.name,
             "url": "http://localhost/",
-            "method": "eth_accounts",
+            "method": method,
         }
+        if params is not None:
+            request["params"] = params
+
         wrapped_request = {
             "jsonrpc": "2.0",
-            "id": self.request_id,
+            "id": request_id,
             "method": "connector_callRPC",
             "params": [json.dumps(request)],
         }
 
+        logging.debug(f"Sending Connector request with data: {json.dumps(wrapped_request, sort_keys=True)}")
         self.ws_conn.send(json.dumps(wrapped_request), websocket.ABNF.OPCODE_TEXT)
 
     def receive(self):
         assert self.ws_conn is not None
-        return self.ws_conn.recv()
+
+        response = self.ws_conn.recv()
+        logging.debug(f"Got Connector response: {json.dumps(response, sort_keys=True)}")
+
+        return json.loads(response)
--- tests-functional/tests/test_status_connector.py
+++ tests-functional/tests/test_status_connector.py
@@ -1,4 +1,3 @@
-import json
 import pytest
 
 from clients.connector import ConnectorClient
@@ -30,35 +29,89 @@ class TestStatusConnector:
         yield client
         client.disconnect()
 
-    def test_get_accounts(self, backend, connector):
+    def test_get_chain_id_resolved_before_connection(self, backend, connector):
+        connector.get_chain_id()
+
+        message = connector.receive()
+        chain_id = int(message.get("result"), 16)
+        assert chain_id == backend.network_id
+
+    def test_other_methods_not_resolved_before_connection(self, connector):
+        connector.get_block_number()
+
+        message = connector.receive()
+        assert message.get("error").get("message") == "dApp is not permitted by user"
+
+    def test_connect_and_get_accounts(self, backend, connector):
         # Load actual directly from backend
-        accs = backend.accounts_service.get_accounts()["result"]
-        wallet_acc = next((acc.get("address") for acc in accs if acc.get("wallet") is True), None)
+        wallet_acc = self._get_wallet_account(backend)
         assert wallet_acc is not None
 
         # Request accounts through connector
         connector.get_accounts()
 
         # Expect SEND_REQUEST_ACCOUNTS signal, check dApp name
-        signal = backend.wait_for_signal(SignalType.CONNECTOR_SEND_REQUEST_ACCOUNTS.value)
-        event = signal.get("event")
+        event = self._get_event(backend, SignalType.CONNECTOR_SEND_REQUEST_ACCOUNTS.value)
         assert event.get("name") == connector.name
+        request_id = event.get("requestId")
 
         # Accept request
-        response = backend.connector_service.request_accounts_accepted(event.get("requestId"), wallet_acc, backend.network_id)
+        response = backend.connector_service.request_accounts_accepted(request_id, wallet_acc, backend.network_id)
         assert response.get("error") is None
 
         # Expect DAPP_PERMISSION_GRANTED signal, check dApp name, shared account and chain ID
-        signal = backend.wait_for_signal(SignalType.CONNECTOR_DAPP_PERMISSION_GRANTED.value)
-        event = signal.get("event")
+        event = self._get_event(backend, SignalType.CONNECTOR_DAPP_PERMISSION_GRANTED.value)
         assert event.get("name") == connector.name
         assert event.get("sharedAccount") == wallet_acc
         assert len(event.get("chains")) == 1 and event.get("chains")[0] == backend.network_id
 
         # Receive accounts on connector
         message = connector.receive()
-        message = json.loads(message)
         assert message.get("result") is not None
         accounts = message.get("result")
         assert len(accounts) == 1
         assert accounts[0] == wallet_acc
+
+    def test_other_methods_resolved_after_connection(self, backend, connector):
+        # Request accounts through connector and connect
+        wallet_acc = self._get_wallet_account(backend)
+        connector.get_accounts()
+        request_id = self._get_event(backend, SignalType.CONNECTOR_SEND_REQUEST_ACCOUNTS.value).get("requestId")
+        backend.connector_service.request_accounts_accepted(request_id, wallet_acc, backend.network_id)
+        message = connector.receive()
+        assert message.get("result") is not None
+
+        # Other methods are resolved after connection
+        connector.get_block_number()
+
+        message = connector.receive()
+        assert message.get("result") is not None
+
+    def test_revoke_permissions_on_disconnect(self, backend, connector):
+        # Request accounts through connector and connect
+        wallet_acc = self._get_wallet_account(backend)
+        connector.get_accounts()
+        request_id = self._get_event(backend, SignalType.CONNECTOR_SEND_REQUEST_ACCOUNTS.value).get("requestId")
+        backend.connector_service.request_accounts_accepted(request_id, wallet_acc, backend.network_id)
+        message = connector.receive()
+        assert message.get("result") is not None
+
+        # Handle Revoke Permissions before disconnect
+        connector.revoke_permissions()
+
+        message = connector.receive()
+        assert message.get("error") is None
+
+        # Expect DAPP_PERMISSION_REVOKED signal, check dApp name
+        event = self._get_event(backend, SignalType.CONNECTOR_DAPP_PERMISSION_REVOKED.value)
+        assert event.get("name") == connector.name
+
+    def _get_wallet_account(self, backend):
+        accs = backend.accounts_service.get_accounts()["result"]
+        wallet_acc = next((acc.get("address") for acc in accs if acc.get("wallet") is True), None)
+        return wallet_acc
+
+    def _get_event(self, backend, signal_type):
+        signal = backend.wait_for_signal(signal_type)
+        event = signal.get("event")
+        return event
