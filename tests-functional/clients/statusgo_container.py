e8b6cd2305c22b9358e9a688e8f2d186e975d0dc
Igor Sirotin <sirotin@status.im>
2025-09-04 13:02:00 +0100

    fix: only connect to the compose network

--- tests-functional/clients/statusgo_container.py
+++ tests-functional/clients/statusgo_container.py
@@ -68,6 +68,7 @@ class StatusGoContainer:
             "entrypoint": entrypoint,
             "ports": ports,
             "stop_signal": "SIGINT",
+            "network": self.network_name,
         }
 
         if "FUNCTIONAL_TESTS_DOCKER_UID" in os.environ:
@@ -84,9 +85,6 @@ class StatusGoContainer:
         StatusGoContainer.all_containers.append(self)
 
         logging.debug(f"Container {self.container.name} created. ID = {self.container.id}")
-
-        network = self.docker_client.networks.get(self.network_name)
-        network.connect(self.container)
 
     def __del__(self):
         self.stop()
