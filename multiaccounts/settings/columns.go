98b96ffb1fdd083e70ab4c5030394c753c2a943a
Jonathan Rainville <rainville.jonathan@gmail.com>
2025-09-11 15:04:57 -0400

    fix(settings): fix wrongly named reactFieldName for messages backup

--- multiaccounts/settings/columns.go
+++ multiaccounts/settings/columns.go
@@ -167,7 +167,7 @@ var (
 		dBColumnName:   "log_level",
 	}
 	MessagesBackupEnabled = SettingField{
-		reactFieldName: "messages-backup-enabled",
+		reactFieldName: "messages-backup-enabled?",
 		dBColumnName:   "messages_backup_enabled",
 		valueHandler:   BoolHandler,
 	}
