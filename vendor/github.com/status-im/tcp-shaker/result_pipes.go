97a48351a972e88699c1a38987f7a084787d15a7
Igor Sirotin <sirotin@status.im>
2025-09-29 00:32:53 +0100

    chore: delete vendor dir

--- vendor/github.com/status-im/tcp-shaker/result_pipes.go
+++
@@ -1,7 +0,0 @@
-package tcp
-
-type resultPipes interface {
-	popResultPipe(int) (chan error, bool)
-	deregisterResultPipe(int)
-	registerResultPipe(int, chan error)
-}
--- vendor/github.com/wealdtech/go-ens/v3/contracts/dnsregistrar/generate.go
+++
@@ -1,3 +0,0 @@
-package dnsregistrar
-
-//go:generate abigen -abi contract.abi -out contract.go -pkg dnsregistrar -type Contract
