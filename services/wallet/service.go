48c5ffed9aceb0291605d9035afecbcff3f3d70c
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2025-10-10 15:46:56 +0100

    fix: disable mercuryo on-ramp provider

--- services/wallet/service.go
+++ services/wallet/service.go
@@ -102,10 +102,6 @@ func NewService(
 
 	featureFlags := &protocolCommon.FeatureFlags{}
 
-	if config.WalletConfig.EnableMercuryoProvider {
-		featureFlags.EnableMercuryoProvider = true
-	}
-
 	savedAddressesManager := &SavedAddressesManager{db: db}
 	transactionManager := transfer.NewTransactionManager(gethManager, transactor, config, accountsDB, pendingTxManager, feed)
 	blockChainState := blockchainstate.NewBlockChainState(rpcClient)
@@ -122,15 +118,6 @@ func NewService(
 
 		cryptoOnRampProviders = []onramp.Provider{
 			onramp.NewMoonPayProvider(),
-		}
-
-		if featureFlags.EnableMercuryoProvider {
-			params := onramp.MercuryoParams{
-				SignURL:      fmt.Sprintf("https://%s.api.status.im/mercuryo/sign/", statusProxyStageName),
-				SignUser:     config.WalletConfig.StatusProxyUser,
-				SignPassword: config.WalletConfig.StatusProxyPassword,
-			}
-			cryptoOnRampProviders = append(cryptoOnRampProviders, onramp.NewMercuryoProvider(tokenManager, params))
 		}
 
 		cryptoCompare := cryptocompare.NewClient()
