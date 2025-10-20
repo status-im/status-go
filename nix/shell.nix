1a9058f8a605a9f94102c3a52285ea4981f29503
Ivan Folgueira Bande <ivansete@status.im>
2025-10-20 19:02:56 +0200

    fix: patch shellHook to force using Nim instaleld in nix

--- nix/shell.nix
+++ nix/shell.nix
@@ -24,6 +24,16 @@ in mkShell {
   ];
 
   shellHook = lib.optionalString (!isMacM1) ''
+    echo "Patching env.sh to use Nix Nim..."
+    env_sh="vendor/github.com/waku-org/sds-go-bindings/third_party/nim-sds/vendor/nimbus-build-system/scripts/env.sh"
+
+    if [ -f "$env_sh" ]; then
+      sed -i "s|/vendor/Nim/bin/nim|${pkgs.nim}/bin/nim|g" "$env_sh"
+      echo "Patched env.sh to use ${pkgs.nim}/bin/nim"
+    else
+      echo "env.sh not found at $env_sh, skipping patch"
+    fi
+
     export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${pkgs.lib-sds-pkg}/lib/
     ANDROID_HOME=${pkgs.androidPkgs.androidsdk}/libexec/android-sdk/
     ANDROID_NDK=$ANDROID_HOME/ndk-bundle
