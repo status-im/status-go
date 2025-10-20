1ec1c7bfea6aac2af9b010da56486727ad27a048
Ivan Folgueira Bande <ivansete@status.im>
2025-10-21 01:16:45 +0200

    chore: add CGO_ flags in default nix

--- nix/pkgs/status-go/library/default.nix
+++ nix/pkgs/status-go/library/default.nix
@@ -67,6 +67,8 @@ in pkgs.buildGoModule {
     export GO111MODULE=on
 
     export NIM_SDS_REPO_PATH=${pkgs.nim-sds-src}/
+    export CGO_CFLAGS="-I$NIM_SDS_REPO_PATH/library/"
+    export CGO_LDFLAGS="-L$NIM_SDS_REPO_PATH/build -lsds -Wl,-rpath,$NIM_SDS_REPO_PATH/build"
 
     # Patch env.sh now that nim-sds exists
     echo "Patching env.sh to use Nix Nim..."
