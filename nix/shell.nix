b5f1c66bc534b553d14f16efac649bce4e10097d
Ivan Folgueira Bande <ivansete@status.im>
2025-10-21 02:26:05 +0200

    fix: nix shell use sed instead

--- nix/shell.nix
+++ nix/shell.nix
@@ -25,11 +25,10 @@ in mkShell {
 
   shellHook = lib.optionalString (!isMacM1) ''
     echo "Patching env.sh to use Nix Nim..."
-    env_sh="$PWD/vendor/github.com/waku-org/sds-go-bindings/third_party/nim-sds/vendor/nimbus-build-system/scripts/env.sh"
+    env_sh="${toString ./vendor/github.com/waku-org/sds-go-bindings/third_party/nim-sds/vendor/nimbus-build-system/scripts/env.sh}"
     if [ -f "$env_sh" ]; then
       echo "Replacing nim by ${pkgs.nim}/bin/nim"
-      ${pkgs.gnused}/bin/substituteInPlace "$env_sh" \
-        --replace-warn "/vendor/Nim/bin/nim" "${pkgs.nim}/bin/nim"
+      ${pkgs.gnused}/bin/sed -i 's#/vendor/Nim/bin/nim#${pkgs.nim}/bin/nim#g' "$env_sh"
     else
       echo "Warning: $env_sh not found (PWD=$PWD)"
     fi
