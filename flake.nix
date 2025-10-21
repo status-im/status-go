67d10dc24177103b8c051733e0ea02e3abf485fa
Ivan Folgueira Bande <ivansete@status.im>
2025-10-21 02:56:47 +0200

    fix: fetch latest nim-sds with more platforms

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/0ef228213045d2cdb5a169a95d63ded38670b293";
     # We cannot do follows since the nim-unwrapped-2_0 doesn't exist in this nixpkgs version above
     nwaku.url = "git+https://github.com/waku-org/nwaku?submodules=1&rev=7e5041d5e17d717f77fb74ffd876b987a0c6bf5d";
-    nim-sds.url = "git+https://github.com/waku-org/nim-sds?submodules=1&rev=972d7862fca9c3a114e3840cc44b863cb7ca90c2";
+    nim-sds.url = "git+https://github.com/waku-org/nim-sds?submodules=1&rev=35c0f1964dbaee24f885dee8530357c3e17fdc33";
   };
 
   outputs = { self, nixpkgs, nwaku, nim-sds }:
