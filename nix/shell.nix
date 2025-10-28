0b2d340f25e2a7ecf4c85da6da2183d73788ab28
Ivan Folgueira Bande <ivansete@status.im>
2025-10-28 19:19:35 +0100

    chore: make a more appropriate LD_LIBRARY_PATH export for sds

--- nix/shell.nix
+++ nix/shell.nix
@@ -27,7 +27,7 @@ in mkShell {
   ];
 
   shellHook = ''
-    export LD_LIBRARY_PATH="${pkgs.lib-sds-pkg}/lib/"
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.lib-sds-pkg ]}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
     export USE_SYSTEM_NIM=1
     export CGO_CFLAGS="$CGO_CFLAGS $NIX_CFLAGS_COMPILE"
     export CGO_LDFLAGS="$CGO_LDFLAGS $NIX_LDFLAGS"
