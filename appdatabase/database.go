636cc2a05dd0b5e3fd65d9b8abd037c0aae94b17
Igor Sirotin <sirotin@status.im>
2025-11-08 22:16:30 +0100

    chore: remove v1 migration tests

--- appdatabase/database.go
+++ appdatabase/database.go
@@ -1,29 +1,23 @@
 package appdatabase
 
 import (
-	"crypto/sha256"
 	"database/sql"
 	"encoding/json"
 	"math/big"
 
 	"go.uber.org/zap"
 
-	d_common "github.com/status-im/status-go/common"
 	"github.com/status-im/status-go/logutils"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/status-im/status-go/appdatabase/migrations"
-	migrationsprevnodecfg "github.com/status-im/status-go/appdatabase/migrationsprevnodecfg"
-	"github.com/status-im/status-go/crypto"
+	"github.com/status-im/status-go/appdatabase/migrationsprevnodecfg"
 	"github.com/status-im/status-go/nodecfg"
 	"github.com/status-im/status-go/services/wallet/bigint"
 	w_common "github.com/status-im/status-go/services/wallet/common"
 	"github.com/status-im/status-go/sqlite"
-
-	cryptotypes "github.com/status-im/status-go/crypto/types"
 )
 
 const nodeCfgMigrationDate = 1640111208
@@ -63,13 +57,8 @@ func doMigration(db *sql.DB) error {
 		}
 	}
 
-	postSteps := []*sqlite.PostStep{
-		{Version: 1662365868, CustomMigration: FixMissingKeyUIDForAccounts},
-		{Version: 1720606449, CustomMigration: OptimizeMobileWakuV2SettingsForMobileV1},
-	}
-	postSteps = append(postSteps, customSteps...)
 	// Run all the new migrations
-	err = migrations.Migrate(db, postSteps)
+	err = migrations.Migrate(db, customSteps)
 	if err != nil {
 		return err
 	}
@@ -90,72 +79,6 @@ func InitializeDB(path, password string, kdfIterationsNumber int) (*sql.DB, erro
 	}
 
 	return db, nil
-}
-
-func OptimizeMobileWakuV2SettingsForMobileV1(sqlTx *sql.Tx) error {
-	if d_common.IsMobilePlatform() {
-		_, err := sqlTx.Exec(`UPDATE wakuv2_config SET light_client = ?, enable_store_confirmation_for_messages_sent = ?`, true, false)
-		if err != nil {
-			logutils.ZapLogger().Error("failed to enable light client and disable store confirmation for mobile v1", zap.Error(err))
-			return err
-		}
-	}
-	return nil
-}
-
-func FixMissingKeyUIDForAccounts(sqlTx *sql.Tx) error {
-	rows, err := sqlTx.Query(`SELECT address,pubkey FROM accounts WHERE pubkey IS NOT NULL AND type != '' AND type != 'generated'`)
-	if err != nil {
-		logutils.ZapLogger().Error("Migrating accounts: failed to query accounts", zap.Error(err))
-		return err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var address cryptotypes.Address
-		var pubkey cryptotypes.HexBytes
-		err = rows.Scan(&address, &pubkey)
-		if err != nil {
-			logutils.ZapLogger().Error("Migrating accounts: failed to scan records", zap.Error(err))
-			return err
-		}
-		pk, err := crypto.UnmarshalPubkey(pubkey)
-		if err != nil {
-			logutils.ZapLogger().Error("Migrating accounts: failed to unmarshal pubkey", zap.String("pubkey", d_common.TruncateWithDot(string(pubkey))), zap.Error(err))
-			return err
-		}
-		pkBytes := sha256.Sum256(crypto.FromECDSAPub(pk))
-		keyUIDHex := hexutil.Encode(pkBytes[:])
-		_, err = sqlTx.Exec(`UPDATE accounts SET key_uid = ? WHERE address = ?`, keyUIDHex, address)
-		if err != nil {
-			logutils.ZapLogger().Error("Migrating accounts: failed to update key_uid for imported accounts", zap.Error(err))
-			return err
-		}
-	}
-
-	var walletRootAddress cryptotypes.Address
-	err = sqlTx.QueryRow(`SELECT wallet_root_address FROM settings WHERE synthetic_id='id'`).Scan(&walletRootAddress)
-	if err == sql.ErrNoRows {
-		// we shouldn't reach here, but if we do, it probably happened from the test
-		logutils.ZapLogger().Warn("Migrating accounts: no wallet_root_address found in settings")
-		return nil
-	}
-	if err != nil {
-		logutils.ZapLogger().Error("Migrating accounts: failed to get wallet_root_address", zap.Error(err))
-		return err
-	}
-	_, err = sqlTx.Exec(`UPDATE accounts SET key_uid = ?, derived_from = ? WHERE type = '' OR type = 'generated'`, CurrentAppDBKeyUID, walletRootAddress.Hex())
-	if err != nil {
-		logutils.ZapLogger().Error("Migrating accounts: failed to update key_uid/derived_from", zap.Error(err))
-		return err
-	}
-	// fix the default wallet account color issue https://github.com/status-im/status-mobile/issues/20476
-	// we don't care the other type of account's color
-	_, err = sqlTx.Exec(`UPDATE accounts SET color = 'blue',emoji='🐳' WHERE wallet = 1`)
-	if err != nil {
-		logutils.ZapLogger().Error("Migrating accounts: failed to update default wallet account's color to blue", zap.Error(err))
-		return err
-	}
-	return nil
 }
 
 func migrateEnsUsernames(sqlTx *sql.Tx) error {
--- static/test-mobile-release-1.20.x-aa6e4b2-account/README.md
+++
@@ -1,2 +0,0 @@
-The account data files in this directory are generated with this draft PR(https://github.com/status-im/status-mobile/pull/20123) build.
-Only used to determine if we can log in with older status-go accounts in the latest version of status-go.
