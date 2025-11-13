d8237046e5bef6796dbbd324a35916a56db81aee
Igor Sirotin <sirotin@status.im>
2025-11-13 23:15:26 +0000

    feat: example app create and list accounts

---
+++ examples/backend-create-account/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/status-im/status-go/logutils"
+	"github.com/status-im/status-go/pkg/backend"
+	"github.com/status-im/status-go/pkg/services/root/requests"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Error getting current directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	logger := logutils.ZapLogger()
+	rootDataDir := path.Join(dir, "data")
+
+	fmt.Printf("Root data dir: %s\n", rootDataDir)
+
+	b, err := backend.NewStatusBackend(
+		rootDataDir,
+		backend.WithLogger(logger),
+	)
+
+	if err != nil {
+		fmt.Printf("Error creating b: %v\n", err)
+		os.Exit(1)
+	}
+
+	accs, err := b.RootService().ListAccounts()
+	if err != nil {
+		fmt.Printf("Error listing accounts: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Accounts: %+v\n", accs)
+
+	request := &requests.CreateAccount{
+		Password:          "",
+		KdfIterations:     0,
+		DeviceName:        "test-device",
+		DisplayName:       "test-user",
+		WakuV2LightClient: false,
+	}
+	acc, err := b.RootService().CreateAccount(context.Background(), request, nil)
+	if err != nil {
+		fmt.Printf("Error creating account: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Account created: %+v\n", acc)
+
+	accs, err = b.RootService().ListAccounts()
+	if err != nil {
+		fmt.Printf("Error listing accounts: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Accounts: %+v\n", accs)
+}
