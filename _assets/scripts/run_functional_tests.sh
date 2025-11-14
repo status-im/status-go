5d6b462f9404b6139dc35843bf7524678e6ed686
Ivan Folgueira Bande <ivansete@status.im>
2025-11-14 11:53:02 +0100

    chore: properly enforce simple aproach to force which func tests to run

--- _assets/scripts/run_functional_tests.sh
+++ _assets/scripts/run_functional_tests.sh
@@ -76,7 +76,8 @@ pytest --reruns 2 -m rpc -c "${root_path}/pytest.ini" -n 12 \
   --docker-image=${image_name} \
   --codecov_dir="${binary_coverage_reports_path}" \
   --logs-dir="${logs_path}" \
-  --junitxml="${test_results_path}/report.xml"
+  --junitxml="${test_results_path}/report.xml" \
+  ./tests-functional
 exit_code=$?
 
 # Stop containers
