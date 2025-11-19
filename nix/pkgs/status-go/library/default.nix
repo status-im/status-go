8dc2f76085442d0e42961548e0b53acc18a60a55
Igor Sirotin <sirotin@status.im>
2025-11-19 22:46:19 +0000

    chore: upgrade go-ens to v3.6.0

--- nix/pkgs/status-go/library/default.nix
+++ nix/pkgs/status-go/library/default.nix
@@ -11,7 +11,7 @@ let
 in pkgs.buildGoModule {
   pname = "status-go";
   src = builtins.path { path = ./../../../..; name = "status-go-library"; };
-  vendorHash = "sha256-tyKqhYhVyOKSTyzCl9okwUo7+VavGpmJbK0iSJxfibc=";
+  vendorHash = "sha256-QaB0LSOAcpcSA3Cv36UcJGKA1OlBcQHchuURxdDLBGE=";
 
   inherit meta version;
 
