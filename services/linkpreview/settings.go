b1e855707b4b1e9e3466efe1fad607e04170fc46
Igor Sirotin <sirotin@status.im>
2025-11-19 23:36:20 +0000

    chore: cleanup

--- services/linkpreview/settings.go
+++ services/linkpreview/settings.go
@@ -6,39 +6,6 @@ import (
 
 //go:generate go tool mockgen -package=mock_settings -source=settings.go -destination=./mock/settings.go
 
-//
-// Option 1
-//
-
 type Settings interface {
 	GetUnfurlingMode() (settings.URLUnfurlingModeType, error)
 }
-
-////
-//// Option 2
-////
-//
-//type Settings1 interface {
-//	GetUnfurlingMode() (settings.URLUnfurlingModeType, error)
-//	SaveUnfurlingMode(modeType settings.URLUnfurlingModeType) error
-//}
-//
-////
-//// Option 3
-////
-//
-//type Settings2 interface {
-//	GetUnfurlingMode() <-chan settings.URLUnfurlingModeType
-//}
-//
-////
-//// Option 4
-////
-//
-//type Service2 struct {
-//
-//}
-//
-//func (s *Service2) SetUnfurlingMode(mode settings.URLUnfurlingModeType) {
-//
-//}
