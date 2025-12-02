d4facf4449358cca76582ed3186c43f8db2f1a7c
Roman <zajic@zajic.net>
2025-12-02 15:51:44 +0800

    fix: remove skip

--- tests-functional/tests/test_wakuext_community_token_permissions.py
+++ tests-functional/tests/test_wakuext_community_token_permissions.py
@@ -231,7 +231,6 @@ class TestCommunityTokenPermissions(MessengerSteps):
         assert owner_key in owner_community.get("members", {})
         assert CommunityRoles.ROLE_OWNER.value in owner_community["members"][owner_key].get("roles", [])
 
-    # @pytest.mark.skip(reason="Pending on issue https://github.com/status-im/status-go/issues/7161")
     def test_owner_edits_visible_before_and_after_minting_owner_token(self):
         """Test that owner edits are visible before and after minting the owner token"""
         # Owner creates a community
