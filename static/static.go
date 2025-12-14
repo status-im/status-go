379fa9c69a9e477f9c0b528fa529ba71951b5e9d
Igor Sirotin <sirotin@status.im>
2025-12-14 17:07:57 +0000

    refactor: move emojis.txt to protocol/identity/emojihash

--- static/static.go
+++ static/static.go
@@ -1,4 +1,4 @@
 // Package static embeds static (JS, HTML) resources right into the binaries
 package static
 
-//go:generate go tool go-bindata -modtime=1700000000 -pkg static -o bindata.go emojis.txt ./keys
+//go:generate go tool go-bindata -modtime=1700000000 -pkg static -o bindata.go ./keys
