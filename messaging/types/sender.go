9f5c64afbdad7802ef5548c9a0f42b3af2cc3de6
Igor Sirotin <sirotin@status.im>
2025-12-16 16:28:08 +0000

    Revert "feat: Add SDS unwrap process for received messages (#7149)"
    This reverts commit fb57d648487e7de7881b08a4670245cbf6224360.

--- messaging/types/sender.go
+++ messaging/types/sender.go
@@ -12,7 +12,6 @@ type SendPublicParams struct {
 	Priority            *MessagePriority
 	HashRatchet         *SendPublicHashRatchetParams
 	CommunityPublicKey  *ecdsa.PublicKey
-	CommunityID         []byte
 }
 
 type SendPublicHashRatchetParams struct {
