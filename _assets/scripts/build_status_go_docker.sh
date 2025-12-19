3b92fc5a4b175214033436e0186ec75f9caff3a2
Marcin Czenko <marcin.czenko@pm.me>
2025-12-19 10:37:57 +0100

    build: adds convenience scripts for running tests

---
+++ _assets/scripts/build_status_go_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o nounset
+
+GIT_ROOT=$(cd "${BASH_SOURCE%/*}" && git rev-parse --show-toplevel)
+source "${GIT_ROOT}/_assets/scripts/colors.sh"
+source "${GIT_ROOT}/_assets/scripts/codecov.sh"
+
+echo -e "${GRN}Building status-go docker image...${RST}"
+
+identifier=${BUILD_TAG:-"status-go-func-tests-$(git rev-parse --short HEAD)"}
+image_name="statusgo-${identifier,,}"
+
+# Build statusgo image
+echo -e "${GRN}Building status-go${RST}"
+docker build . \
+  --build-arg "build_tags='gowaku_no_rln'" \
+  --build-arg "enable_go_cache=false" \
+  --tag "${image_name}"
+
+echo -e "${GRN}Building status-go docker image DONE!${RST}"
---
+++ _assets/scripts/run_functional_tests_dev.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GIT_ROOT=$(cd "${BASH_SOURCE%/*}" && git rev-parse --show-toplevel)
+source "${GIT_ROOT}/_assets/scripts/colors.sh"
+source "${GIT_ROOT}/_assets/scripts/codecov.sh"
+
+: "${FUNCTIONAL_TESTS_LOG_LEVEL:=INFO}"
+
+root_path="${GIT_ROOT}/tests-functional"
+logs_path="${root_path}/logs"
+
+all_compose_files="-f ${root_path}/docker-compose.anvil.yml -f ${root_path}/docker-compose.waku.yml"
+identifier=${BUILD_TAG:-"status-go-func-tests-$(git rev-parse --short HEAD)"}
+project_name="${identifier,,}"
+image_name="statusgo-${identifier,,}"
+
+source "${GIT_ROOT}/_assets/scripts/functional_tests_commons.sh"
+
+test_pattern="${1-}"
+
+set_pyenv
+
+list_tests_and_confirm "${test_pattern}"
+
+remove_old_logs
+
+remove_old_containers
+
+start_services
+
+wait_for_waku_suite_scanner
+
+wait_for_services 60
+
+run_tests "${test_pattern}"
+
+# Cleanup will be handled automatically by the trap
+echo -e "${GRN}Testing finished${RST}"
+exit $exit_code
