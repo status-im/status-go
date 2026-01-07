05c4aa40032c382595f7235f5e9fd27a510e4cec
Siddarth Kumar <siddarthkay@gmail.com>
2026-01-07 20:48:44 +0400

    chore: bump libc to fix flatpak builds
    needed to fix issues with running flatpak in status-desktop linux
    builds. Otherwise they panic like this :
    ```
    goroutine 1 [running]:
    modernc.org/libc/honnef.co/go/netdb.init.0()
    /home/jenkins/go/pkg/mod/modernc.org/libc@v1.11.82/honnef.co/go/netdb/netdb.go:716
    +0x6cf
    SIGABRT: Abnormal termination.
    
    ```

--- nix/pkgs/status-go/library/default.nix
+++ nix/pkgs/status-go/library/default.nix
@@ -11,7 +11,7 @@ let
 in pkgs.buildGoModule {
   pname = "status-go";
   src = builtins.path { path = ./../../../..; name = "status-go-library"; };
-  vendorHash = "sha256-GgP0/qUR5DJUzQRZvDpDHqdQna2H0q1hXTRHykVT1g8=";
+  vendorHash = "sha256-NyiEvHeu9lfkFPPlwtAy7/I4Dj5a0dxBfUj9QOzuKdM=";
 
   inherit meta version;
 
