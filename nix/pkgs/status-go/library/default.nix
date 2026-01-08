a45da4d5b071d8fe9dd4d7573bed09b92e0fc60d
saledjenic <86303051+saledjenic@users.noreply.github.com>
2026-01-08 11:53:02 +0100

    chore: update go-wallet-sdk dependency and add skipped token keys to token manager (#7265)
    - Updated go-wallet-sdk version in go.mod and go.sum
    - Integrated SkippedTokenKeys into token manager

--- nix/pkgs/status-go/library/default.nix
+++ nix/pkgs/status-go/library/default.nix
@@ -11,7 +11,7 @@ let
 in pkgs.buildGoModule {
   pname = "status-go";
   src = builtins.path { path = ./../../../..; name = "status-go-library"; };
-  vendorHash = "sha256-owwYkQ/3AqTC3EZqxuD+k0aNxmTeUUZ6CWZ3v9dx2MQ=";
+  vendorHash = "sha256-O2hS/NtjbU4QVPjsu3xh/BScRY+qqydMUdl+uo++OZs=";
 
   inherit meta version;
 
