8146f6ae5eccc70f751b597717aab505ab7064b1
Marcin Czenko <marcin.czenko@pm.me>
2026-04-13 17:03:52 +0200

    build: removes printing libstorage version on nix env

--- nix/shell.nix
+++ nix/shell.nix
@@ -32,12 +32,6 @@ in mkShell {
     export NIM_SDS_LIB_DIR="${pkgs.libsds}/lib"
 
     export LD_LIBRARY_PATH="${lib.makeLibraryPath (with pkgs; [libwaku libsds libstorage])}"
-    if [ -f "${pkgs.libstorage}/lib/libstorage.so" ]; then
-      storage_revision_line=$(${pkgs.binutils}/bin/strings "${pkgs.libstorage}/lib/libstorage.so" | ${pkgs.ripgrep}/bin/rg -m1 "Storage revision:" || true)
-      if [ -n "$storage_revision_line" ]; then
-        echo "libstorage revision: $storage_revision_line"
-      fi
-    fi
   '' + lib.optionalString (!isDarwin && isAarch64) ''
     export ANDROID_HOME=${pkgs.androidPkgs.androidsdk}/libexec/android-sdk/
     export ANDROID_NDK=\$ANDROID_HOME/ndk-bundle
