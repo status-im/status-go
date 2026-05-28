b1c522e6ccf2ae2aaccfe1c0ddd22ce26a3fde66
Siddarth Kumar <siddarthkay@gmail.com>
2026-05-28 16:59:35 +0530

    fix: use circom-compat-ffi from PR that fixes fetching of dep

--- flake.nix
+++ flake.nix
@@ -23,6 +23,8 @@
     logos-storage-nim = {
       url = "git+https://github.com/logos-storage/logos-storage-nim?submodules=1&rev=3c09f008bb5266a669fd19f18368f9e8b861b664";
       inputs.nixpkgs.follows = "nixpkgs";
+      # https://github.com/logos-storage/circom-compat-ffi/pull/11
+      inputs.circom-compat.url = "github:logos-storage/circom-compat-ffi/3cca4e91054a4d2bb5335feb19138362ce0fe417";
     };
     # We cannot do follows since the nim-unwrapped-2_0 doesn't exist in this nixpkgs version above
     nim-sds.url = "git+https://github.com/logos-messaging/nim-sds?submodules=1&rev=fb8039c5a56086ec7fb3e5e1a5a593bb3756ccb6";
