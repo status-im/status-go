18fbc24cf4eef3181d3e3133dea8ee759f5a1139
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2026-06-15 14:27:19 +0400

    chore: make vendor

--- nix/pkgs/status-go/library/default.nix
+++ nix/pkgs/status-go/library/default.nix
@@ -11,7 +11,7 @@ let
 in pkgs.buildGoModule {
   pname = "status-go";
   src = builtins.path { path = ./../../../..; name = "status-go-library"; };
-  vendorHash = "sha256-itcTnM/Jm4shXuW38g6YlgNyLAHFs0CBlysqDwh4xXk=";
+  vendorHash = "sha256-bblnIanlRFp7MXO6yyjU0HY0tRgiyA6oLwmmoABXrqU=";
 
   inherit meta version;
 
